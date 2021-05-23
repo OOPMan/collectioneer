b031fc1501a5a763bc94153908ab155a4c6525bf
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2021-05-22 21:56:22 -0400

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,101 @@
+lazy val scala213Version      = "2.13.5"
+lazy val scala30Version       = "3.0.0"
+
+lazy val commonSettings = Seq(
+  version := "0.1.0",
+  scalaVersion := scala30Version,
+  crossScalaVersions := Seq(scala213Version, scala30Version),
+  libraryDependencies ++= Seq(
+    "com.novocode"  % "junit-interface" % "0.11" % "test",
+    "com.lihaoyi" % "ammonite" % "2.3.8-124-2da846d2" % "test" cross CrossVersion.for3Use2_13With("", ".5")
+  ),
+  Test / sourceGenerators += Def.task {
+    val file = (Test / sourceManaged).value / "amm.scala"
+    IO.write(file,
+      """object amm {
+        |   def main(args: Array[String]) = ammonite.Main.main(args)
+        |}""".stripMargin
+    )
+    Seq(file)
+  }.taskValue,
+  Test / fullClasspath ++= {
+    (Test / updateClassifiers).value
+      .configurations
+      .find(_.configuration.name == Test.name)
+      .get
+      .modules
+      .flatMap(_.artifacts)
+      .collect{case (a, f) if a.classifier == Some("sources") => f}
+  }
+)
+
+lazy val collectioneer = project
+  .in(file("."))
+  .settings(commonSettings)
+  .aggregate(
+      core,
+      cli,
+      gui,
+      repl
+  )
+
+lazy val core = project
+  .in(file("modules/core"))
+  .settings(commonSettings)
+  .settings(
+    name := "Collectioneer Core",
+    libraryDependencies ++= Seq(
+      "org.flywaydb"            % "flyway-core"       % "6.3.0",
+      "com.h2database"          % "h2"                % "1.4.200"  % "test",
+      "org.tpolecat"            %% "doobie-core"      % "0.13.3",
+      "org.tpolecat"            %% "doobie-h2"        % "0.13.3",          // H2 extensions support
+      "org.tpolecat"            %% "doobie-hikari"    % "0.13.3",          // HikariCP transactor.
+    )
+  )
+
+lazy val cli = project
+  .in(file("modules/cli"))
+  .settings(commonSettings)
+  .settings(
+    name := "Collectioneer CLI",
+    libraryDependencies ++= Seq(
+      "com.github.scopt"        %% "scopt"            % "4.0.1",
+    )
+  )
+  .dependsOn(core)
+
+lazy val repl = project
+  .in(file("modules/repl"))
+  .settings(commonSettings)
+  .settings(
+    name:= "Collectioneer REPL",
+    libraryDependencies ++= Seq(
+      "com.lihaoyi" %% "ammonite-terminal" % "2.3.8-124-2da846d2" cross CrossVersion.for3Use2_13
+    )
+  )
+
+// Determine OS version of JavaFX binaries
+lazy val osName = System.getProperty("os.name") match {
+  case n if n.startsWith("Linux") => "linux"
+  case n if n.startsWith("Mac") => "mac"
+  case n if n.startsWith("Windows") => "win"
+  case _ => throw new Exception("Unknown platform!")
+}
+// Add JavaFX dependencies
+lazy val javaFXModules = Seq("base", "controls", "fxml", "graphics", "media", "swing", "web")
+lazy val gui = project
+  .in(file("modules/gui"))
+  .settings(commonSettings)
+  .settings(
+    name := "Collectioneer GUI",
+    scalacOptions ++= Seq("-unchecked", "-deprecation", "-encoding", "utf8", "-feature"),
+    fork := true,
+    mainClass := Some("ScalaFXHelloWorld"),
+    libraryDependencies ++= Seq(
+      "org.scalafx"             %% "scalafx"          % "16.0.0-R24",
+    ),
+    libraryDependencies ++= javaFXModules.map(m =>
+      "org.openjfx" % s"javafx-$m" % "16" classifier osName
+    )
+  )
+  .dependsOn(core)
---
+++ modules/core/src/main/resources/migrations/V1__initial.sql
@@ -0,0 +1,94 @@
+create table collections
+(
+    id identity not null,
+    name varchar not null,
+    description clob(1048576) null,
+    virtual boolean not null default false,
+    deleted boolean not null default false,
+    created timestamp with time zone not null default CURRENT_TIMESTAMP(),
+    modified timestamp with time zone not null default CURRENT_TIMESTAMP(),
+    constraint collections_pk
+        primary key (id)
+);
+
+create table collections_source_collections_assn
+(
+    collection_id bigint not null,
+    source_collection_id bigint not null,
+    index int not null default 0,
+    source_type enum('ITEMS', 'ITEM_TYPES') not null default 'ITEMS',
+    constraint collections_source_collections_assn_pk
+        primary key (collection_id, source_collection_id),
+    constraint collections_source_collections_assn_collection_id_fk
+        foreign key (collection_id) references collections(id),
+    constraint collections_source_collections_assn_source_collection_id_fk
+        foreign key (source_collection_id) references collections(id)
+);
+
+create table items
+(
+    id identity not null,
+    name varchar not null,
+    description clob(1048576) null,
+    virtual boolean not null default false,
+    deleted boolean not null default false,
+    created timestamp with time zone not null default CURRENT_TIMESTAMP(),
+    modified timestamp with time zone not null default CURRENT_TIMESTAMP(),
+    constraint items_pk
+        primary key (id)
+);
+
+create table items_collections_assn
+(
+    item_id bigint not null,
+    collection_id bigint not null,
+    quantity bigint not null default 1,
+    assn_type enum('COLLECTION_OF', 'INSTANCE_OF') not null default 'COLLECTION_OF',
+    constraint items_collections_assn_pk
+        primary key (item_id, collection_id),
+    constraint items_collections_assn_item_id_fk
+        foreign key (item_id) references items(id),
+    constraint items_collections_assn_collection_id_fk
+        foreign key (collection_id) references collections(id)
+);
+
+create table items_item_types_assn
+(
+    item_id bigint not null,
+    item_type_id bigint not null,
+    index int not null default 0,
+    constraint item_item_types_assn_pk
+        primary key (item_id, item_type_id),
+    constraint item_item_types_assn_item_id_fk
+        foreign key (item_id) references items(id),
+    constraint item_item_types_assn_item_type_id_fk
+        foreign key (item_type_id) references items(id)
+);
+
+create table properties
+(
+    id identity not null,
+    item_id bigint not null,
+    name varchar not null,
+    deleted boolean not null default false,
+    created timestamp with time zone not null default CURRENT_TIMESTAMP(),
+    modified timestamp with time zone not null default CURRENT_TIMESTAMP(),
+    type enum('string', 'bigint', 'double', 'bool', 'timestamp', 'clob', 'blob', 'uuid', 'array', 'json') not null,
+    string_value varchar null,
+    bigint_value bigint null,
+    double_value double null,
+    bool_value boolean null,
+    clob_value clob null,
+    blob_value blob null,
+    uuid_value uuid null,
+    array_value array null,
+    array_type enum('string', 'bigint', 'double', 'bool', 'timestamp') null,
+    json_value json null,
+    timestamp_value timestamp with time zone null,
+    constraint properties_pk
+        primary key (id),
+    constraint properties_item_id_fk
+        foreign key (item_id) references items(id),
+    constraint properties_item_id_name_uk
+        unique (item_id, name)
+);
