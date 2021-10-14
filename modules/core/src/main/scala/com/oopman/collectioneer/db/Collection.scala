0815b06dac7d652e1554ec90c3f0412b957b9744
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2021-10-14 10:53:13 -0400

    Added some explicit type annotations and additional imports in Collection.scala

--- modules/core/src/main/scala/com/oopman/collectioneer/db/Collection.scala
+++ modules/core/src/main/scala/com/oopman/collectioneer/db/Collection.scala
@@ -1,12 +1,16 @@
 package com.oopman.collectioneer.db
 
 import doobie._
-import doobie.util.fragments
 import doobie.implicits._
-import doobie.implicits.javatimedrivernative._
-import doobie.h2.implicits._
+import doobie.util.fragments
+import doobie.util.ExecutionContexts
+import cats._
+import cats.data._
+import cats.effect._
+import cats.implicits._
+import doobie.implicits.javatimedrivernative.*
+import doobie.h2.implicits.*
 import org.h2.api.TimestampWithTimeZone
-
 import java.time.ZonedDateTime
 
 
@@ -21,32 +25,31 @@ case class Collection
   modified: ZonedDateTime = ZonedDateTime.now(),
 )
 
-
-def retrieveCollectionById(id: Long) =
+def retrieveCollectionById(id: Long): Query0[Collection] =
   sql"""SELECT id, name, description, virtual, deleted, created, modified
        |FROM collections
        |WHERE id = $id""".stripMargin.query[Collection]
 
-def retrieveCollectionsByName(name: String) =
+def retrieveCollectionsByName(name: String): Query0[Collection] =
   sql"""SELECT id, name, description, virtual, deleted, created, modified
        |FROM collections
        |WHERE name = $name""".stripMargin.query[Collection]
 
-def retrieveCollectionsByVirtual(virtual: Boolean) =
+def retrieveCollectionsByVirtual(virtual: Boolean): Query0[Collection] =
   sql"""SELECT id, name, description, virtual, deleted, created, modified
        |FROM collections
        |WHERE virtual = $virtual""".stripMargin.query[Collection]
 
-def retrieveCollectionsByDeleted(deleted: Boolean) =
+def retrieveCollectionsByDeleted(deleted: Boolean): Query0[Collection] =
   sql"""SELECT id, name, description, virtual, deleted, created, modified
        |FROM collections
        |WHERE deleted = $deleted""".stripMargin.query[Collection]
 
-def createCollection(name: String, description: Option[String] = None, virtual: Boolean = false) =
+def createCollection(name: String, description: Option[String] = None, virtual: Boolean = false): Update0 =
   sql"""INSERT INTO collections(name, description, virtual)
        |VALUES ($name, $description, $virtual)""".stripMargin.update
 
-def updateCollection(id: Long, name: Option[String], description: Option[String], virtual: Option[Boolean]) =
+def updateCollection(id: Long, name: Option[String], description: Option[String], virtual: Option[Boolean]): Update0 =
   val updates = List(
     name.map(value => fr"name = $value"),
     description.map(value => fr"description = $value"),
@@ -56,7 +59,16 @@ def updateCollection(id: Long, name: Option[String], description: Option[String]
   val setFragment = fragments.setOpt(updates: _*)
   (fr"UPDATE collections" ++ setFragment ++ fr"WHERE id = $id").update
 
-def deleteCollection(id: Long) =
+def deleteCollection(id: Long): Update0 =
   sql"""UPDATE collections
        |SET deleted = true, modified = CURRENT_TIMESTAMP()
        |WHERE id = $id""".stripMargin.update
+
+
+def test() = {
+  import cats.effect.unsafe.implicits.global
+  val xa = Transactor.fromDriverManager[IO](
+    "org.postgresql.Driver", "jdbc:postgresql:world", "postgres", ""
+  )
+  retrieveCollectionsByDeleted(true).option.transact(xa).unsafeRunSync()
+}
