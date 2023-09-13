7c9c26fc3540a45b1b916e2a5c5d1bd909a4821f
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2023-09-12 21:15:28 -0400

    Updated V2__initial_data migration to use new PropertyTypes enum

--- modules/core/src/main/scala/com/oopman/collectioneer/db/migrations/h2/V2__initial_data.scala
+++ modules/core/src/main/scala/com/oopman/collectioneer/db/migrations/h2/V2__initial_data.scala
@@ -1,18 +1,16 @@
 package com.oopman.collectioneer.db.migrations.h2
 
-import com.oopman.collectioneer.CoreProperties
+import com.oopman.collectioneer.{CoreProperties, PropertyTypes}
 import org.flywaydb.core.api.migration.{BaseJavaMigration, Context}
 
-val allTypes = List(
-  "varchar", "varbinary", "tinyint", "smallint", "int", "bigint", "numeric", "float", "double", "boolean", "date",
-  "time", "timestamp", "clob", "blob", "uuid", "json"
-)
 val properties = List(
-  (CoreProperties.name, List("varchar")),
-  (CoreProperties.description, List("varchar", "clob")),
-  (CoreProperties.default_value, allTypes),
-  (CoreProperties.min_values, List("int")),
-  (CoreProperties.max_values, List("int"))
+  (CoreProperties.name, List(PropertyTypes.varchar)),
+  (CoreProperties.description, List(PropertyTypes.varchar, PropertyTypes.clob)),
+  (CoreProperties.default_value, PropertyTypes.values.toList),
+  (CoreProperties.min_value, PropertyTypes.values.toList),
+  (CoreProperties.max_value, PropertyTypes.values.toList),
+  (CoreProperties.min_values, List(PropertyTypes.int)),
+  (CoreProperties.max_values, List(PropertyTypes.int))
 )
 /**
  *
@@ -24,14 +22,13 @@ class V2__initial_data extends BaseJavaMigration {
       "INSERT INTO properties(pk, property_name, property_type) VALUES (?, ?, ?)"
     )
     // Insert Property rows
-    for ((property, property_type) <- properties) {
+    for ((property, propertyTypes) <- properties) {
+      val propertyTypesArray = connection.createArrayOf("VARCHAR", propertyTypes.map(_.toString).toArray)
       insertProperties.setString(1, property.uuid.toString)
       insertProperties.setString(2, property.toString)
-      insertProperties.setArray(3, connection.createArrayOf("VARCHAR", property_type.toArray))
+      insertProperties.setArray(3, propertyTypesArray)
       insertProperties.addBatch()
     }
     insertProperties.executeBatch()
-
-
   }
 }
