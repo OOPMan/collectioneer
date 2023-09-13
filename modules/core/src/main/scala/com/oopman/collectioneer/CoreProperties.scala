4b16f0c4010a365e354118a75a9b2ff7c3f64d9e
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2023-09-13 13:52:30 -0400

    Updated CoreProperties enumeration to include PropertyType information for each Property

--- modules/core/src/main/scala/com/oopman/collectioneer/CoreProperties.scala
+++ modules/core/src/main/scala/com/oopman/collectioneer/CoreProperties.scala
@@ -3,14 +3,35 @@ package com.oopman.collectioneer
 import java.util.UUID
 
 /**
- * This enumeration contains the UUIDs associated with the core properties in the application
+ * This enumeration contains the UUIDs and PropertyTypes associated with the core properties in the application
  */
-enum CoreProperties(val uuid: UUID) {
-  case name extends CoreProperties(UUID.fromString("f65d1d21-3542-48f9-a9d5-f96921e4ba15"))
-  case description extends CoreProperties(UUID.fromString("ea3c3226-2a04-45f0-a50e-1db9c4aa0072"))
-  case default_value extends CoreProperties(UUID.fromString("7b7b9577-da92-41d2-9ff8-a174e191b030"))
-  case min_value extends CoreProperties(UUID.fromString("4d3460bd-2cbf-45f9-9059-59edea9c1a17"))
-  case max_value extends CoreProperties(UUID.fromString("9eeca250-53c8-4a35-a636-6954d0718b93"))
-  case min_values extends CoreProperties(UUID.fromString("df39b389-5055-46f7-82f3-53c736fb6950"))
-  case max_values extends CoreProperties(UUID.fromString("158a2358-b5d1-4e73-a6bf-c93d0587590d"))
+enum CoreProperties(val uuid: UUID, val propertyTypes: List[PropertyTypes]) {
+  case name extends CoreProperties(
+    UUID.fromString("f65d1d21-3542-48f9-a9d5-f96921e4ba15"),
+    List(PropertyTypes.varchar)
+  )
+  case description extends CoreProperties(
+    UUID.fromString("ea3c3226-2a04-45f0-a50e-1db9c4aa0072"),
+    List(PropertyTypes.varchar, PropertyTypes.clob)
+  )
+  case default_value extends CoreProperties(
+    UUID.fromString("7b7b9577-da92-41d2-9ff8-a174e191b030"),
+    PropertyTypes.values.toList
+  )
+  case min_value extends CoreProperties(
+    UUID.fromString("4d3460bd-2cbf-45f9-9059-59edea9c1a17"),
+    PropertyTypes.values.toList
+  )
+  case max_value extends CoreProperties(
+    UUID.fromString("9eeca250-53c8-4a35-a636-6954d0718b93"),
+    PropertyTypes.values.toList
+  )
+  case min_values extends CoreProperties(
+    UUID.fromString("df39b389-5055-46f7-82f3-53c736fb6950"),
+    List(PropertyTypes.int)
+  )
+  case max_values extends CoreProperties(
+    UUID.fromString("158a2358-b5d1-4e73-a6bf-c93d0587590d"),
+    List(PropertyTypes.int)
+  )
 }
--- modules/core/src/main/scala/com/oopman/collectioneer/db/migrations/h2/V2__initial_data.scala
+++ modules/core/src/main/scala/com/oopman/collectioneer/db/migrations/h2/V2__initial_data.scala
@@ -3,15 +3,6 @@ package com.oopman.collectioneer.db.migrations.h2
 import com.oopman.collectioneer.{CoreProperties, PropertyTypes}
 import org.flywaydb.core.api.migration.{BaseJavaMigration, Context}
 
-val properties = List(
-  (CoreProperties.name, List(PropertyTypes.varchar)),
-  (CoreProperties.description, List(PropertyTypes.varchar, PropertyTypes.clob)),
-  (CoreProperties.default_value, PropertyTypes.values.toList),
-  (CoreProperties.min_value, PropertyTypes.values.toList),
-  (CoreProperties.max_value, PropertyTypes.values.toList),
-  (CoreProperties.min_values, List(PropertyTypes.int)),
-  (CoreProperties.max_values, List(PropertyTypes.int))
-)
 /**
  *
  */
@@ -22,10 +13,12 @@ class V2__initial_data extends BaseJavaMigration {
       "INSERT INTO properties(pk, property_name, property_type) VALUES (?, ?, ?)"
     )
     // Insert Property rows
-    for ((property, propertyTypes) <- properties) {
-      val propertyTypesArray = connection.createArrayOf("VARCHAR", propertyTypes.map(_.toString).toArray)
-      insertProperties.setString(1, property.uuid.toString)
-      insertProperties.setString(2, property.toString)
+    for (coreProperty <- CoreProperties.values) {
+      val uuid = coreProperty.uuid.toString
+      val propertyName = coreProperty.toString
+      val propertyTypesArray = connection.createArrayOf("VARCHAR", coreProperty.propertyTypes.map(_.toString).toArray)
+      insertProperties.setString(1, uuid)
+      insertProperties.setString(2, propertyName)
       insertProperties.setArray(3, propertyTypesArray)
       insertProperties.addBatch()
     }
