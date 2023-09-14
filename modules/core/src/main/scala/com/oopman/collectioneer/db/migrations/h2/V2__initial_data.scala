04367cf5491cfbbfb63f6c09611178a4331d5ddd
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2023-09-13 21:25:40 -0400

    Updated V2__initial_data migration to save additional required rows to property_value_sets and properties__collections tables

--- modules/core/src/main/scala/com/oopman/collectioneer/db/migrations/h2/V2__initial_data.scala
+++ modules/core/src/main/scala/com/oopman/collectioneer/db/migrations/h2/V2__initial_data.scala
@@ -37,11 +37,18 @@ class V2__initial_data extends BaseJavaMigration {
       insertCollections.setString(1, uuid)
       insertCollections.addBatch()
     }
+    for {
+      coreCollection <- CoreCollections.values
+      (_, uuid) <- coreCollection.propertyPropertyValueSets
+    } {
+      insertPropertyValueSets.setString(1, uuid)
+      insertPropertyValueSets.addBatch()
+    }
     insertPropertyValueSets.executeBatch()
     insertCollections.executeBatch()
     // Associate Properties with Collections
     val insertPropertiesCollections = connection.prepareStatement(
-      "INSERT INTO properties__collections(property_pk, collection_pk, property_value_set_pk) VALUES (?, ?, ?)"
+      "INSERT INTO properties__collections(property_pk, collection_pk, property_value_set_pk, relationship) VALUES (?, ?, ?, ?)"
     )
     for {
       coreCollection <- CoreCollections.values
@@ -52,8 +59,23 @@ class V2__initial_data extends BaseJavaMigration {
       insertPropertiesCollections.setString(1, propertyUUID)
       insertPropertiesCollections.setString(2, collectionUUID)
       insertPropertiesCollections.setString(3, collectionUUID)
+      insertPropertiesCollections.setString(4, "property_of_collection")
       insertPropertiesCollections.addBatch()
     }
+    // Associate CommonPropertiesOfProperties with Properties of CommonProperties
+    // name
+    insertPropertiesCollections.setString(1, CoreProperties.name.uuid.toString)
+    insertPropertiesCollections.setString(2, CoreCollections.CommonPropertiesOfProperties.uuid.toString)
+    insertPropertiesCollections.setString(3, CoreCollections.CommonProperties.propertyPropertyValueSets(CoreProperties.name))
+    insertPropertiesCollections.setString(4, "collection_of_properties_of_property")
+    insertPropertiesCollections.addBatch()
+    // description
+    insertPropertiesCollections.setString(1, CoreProperties.description.uuid.toString)
+    insertPropertiesCollections.setString(2, CoreCollections.CommonPropertiesOfProperties.uuid.toString)
+    insertPropertiesCollections.setString(3, CoreCollections.CommonProperties.propertyPropertyValueSets(CoreProperties.description))
+    insertPropertiesCollections.setString(4, "collection_of_properties_of_property")
+    insertPropertiesCollections.addBatch()
     insertPropertiesCollections.executeBatch()
+    // TODO: Insert PropertyValues for the properties of name and description to set in place sensible defaults
   }
 }
