41edd6100fc5f6b67f80b6ae774b471e8f3ee3e4
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2023-09-13 21:24:38 -0400

    Updated CoreCollections to include a mapping of CoreProperty -> PropertyValueSet UUID to support storing default data for CommonPropertiesOfProperties of the Properties of the CommonProperties Collection. To be more specific, this is to support storing the default values and constraints of the name and description Properties which are common to all Collections

--- modules/core/src/main/scala/com/oopman/collectioneer/CoreCollections.scala
+++ modules/core/src/main/scala/com/oopman/collectioneer/CoreCollections.scala
@@ -2,7 +2,12 @@ package com.oopman.collectioneer
 
 import java.util.UUID
 
-enum CoreCollections(val uuid: UUID, val properties: List[CoreProperties]) {
+enum CoreCollections
+(
+  val uuid: UUID,
+  val properties: List[CoreProperties],
+  val propertyPropertyValueSets: Map[CoreProperties, String]
+) {
   /**
    * CommonProperties encapsulates those Properties that are always common to all Collections:
    *
@@ -11,7 +16,11 @@ enum CoreCollections(val uuid: UUID, val properties: List[CoreProperties]) {
    */
   case CommonProperties extends CoreCollections(
     UUID.fromString("709113ad-6ca0-4d08-b9f4-a526d81da549"),
-    List(CoreProperties.name, CoreProperties.description)
+    List(CoreProperties.name, CoreProperties.description),
+    Map(
+      CoreProperties.name -> "7a235eff-99d1-4412-accd-23066d5298a1",
+      CoreProperties.description -> "6030f26b-88b5-43a2-b5ec-db3349777808"
+    )
   )
   /**
    * CommonPropertiesOfProperties encapsulates those Properties that are always common to all Properties:
@@ -25,6 +34,7 @@ enum CoreCollections(val uuid: UUID, val properties: List[CoreProperties]) {
    */
   case CommonPropertiesOfProperties extends CoreCollections(
     UUID.fromString("4a3dfbf0-3d68-4604-a471-5c4451a3ee5a"),
-    List(CoreProperties.default_value, CoreProperties.min_values, CoreProperties.max_values)
+    List(CoreProperties.default_value, CoreProperties.min_values, CoreProperties.max_values),
+    Map()
   )
 }
