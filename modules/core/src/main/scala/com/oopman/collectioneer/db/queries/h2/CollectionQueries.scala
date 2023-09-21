d3426f1168abffb510d98e54845d2655465b36f6
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2023-09-20 22:52:50 -0400

    Tweaked CollectionQueries structure

--- modules/core/src/main/scala/com/oopman/collectioneer/db/queries/h2/CollectionQueries.scala
+++ modules/core/src/main/scala/com/oopman/collectioneer/db/queries/h2/CollectionQueries.scala
@@ -3,9 +3,10 @@ package com.oopman.collectioneer.db.queries.h2
 import com.oopman.collectioneer.db.entity.{Collections, c}
 import scalikejdbc.*
 
-def all =
-  sql"""
-       SELECT ${c.result.*}
-       FROM ${Collections.as(c)}
-  """
+object CollectionQueries:
+  def all =
+    sql"""
+         SELECT ${c.result.*}
+         FROM ${Collections.as(c)}
+    """
 
