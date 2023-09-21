c5c6bd13a8e9b44abb2c05a5768477c5e748a1c2
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2023-09-20 22:49:55 -0400

    Added Collections syntax SQLSyntaxSupport syntax instance

--- modules/core/src/main/scala/com/oopman/collectioneer/db/entity/Collections.scala
+++ modules/core/src/main/scala/com/oopman/collectioneer/db/entity/Collections.scala
@@ -3,6 +3,8 @@ package com.oopman.collectioneer.db.entity
 import java.time.ZonedDateTime
 import java.util.UUID
 
+import scalikejdbc._
+
 case class Collections
 (
   pk: UUID = UUID.randomUUID(),
@@ -11,3 +13,18 @@ case class Collections
   created: ZonedDateTime = ZonedDateTime.now(),
   modified: ZonedDateTime = ZonedDateTime.now(),
 )
+
+object Collections extends SQLSyntaxSupport[Collections]:
+  override val schemaName = Some("public")
+  override val tableName = "collections"
+
+  def apply(c: ResultName[Collections])(rs: WrappedResultSet) =
+    new Collections(
+      pk = UUID.fromString(rs.string(c.pk)),
+      virtual = rs.boolean(c.virtual),
+      deleted = rs.boolean(c.deleted),
+      created = rs.zonedDateTime(c.created),
+      modified = rs.zonedDateTime(c.modified)
+    )
+
+val c = Collections.syntax("c")
