070812b76e5f4a429e84f0cb2329ef776910a7c2
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2023-09-23 17:52:04 -0400

    Continued implementing CollectionsDAO

--- modules/core/src/main/scala/com/oopman/collectioneer/db/dao/CollectionsDAO.scala
+++ modules/core/src/main/scala/com/oopman/collectioneer/db/dao/CollectionsDAO.scala
@@ -1,11 +1,17 @@
 package com.oopman.collectioneer.db.dao
 
-import com.oopman.collectioneer.db.entity.{Collections, c}
+import scalikejdbc._
+import com.oopman.collectioneer.db.entity.{Collections, c1}
 import com.oopman.collectioneer.db.queries.h2.CollectionQueries
 
 
 object CollectionsDAO {
-  def getAll() =
-    CollectionQueries.all.map(Collections(c.resultName)).list
+  def getAll()(implicit session: DBSession = AutoSession): List[Collections] =
+    CollectionQueries.all.map(Collections(c1.resultName)).list.apply()
 
 }
+
+class CollectionsDAO(val connectionPoolName: String):
+
+  def getAll(): List[Collections] = NamedDB(connectionPoolName) readOnly { implicit session => CollectionsDAO.getAll() }
+
