6bf6b7e5501feae4751a86c4475fc2fbf05c830b
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2023-09-29 23:24:31 -0400

    Began adding Query and DAO methods to support querying for all Collections with matching PKs

--- modules/core/src/main/scala/com/oopman/collectioneer/db/dao/CollectionsDAO.scala
+++ modules/core/src/main/scala/com/oopman/collectioneer/db/dao/CollectionsDAO.scala
@@ -1,15 +1,23 @@
 package com.oopman.collectioneer.db.dao
 
-import scalikejdbc._
+import scalikejdbc.*
 import com.oopman.collectioneer.db.entity.{Collections, c1}
 import com.oopman.collectioneer.db.queries.h2.CollectionQueries
 
+import java.util.UUID
+
 
 object CollectionsDAO:
-  def getAll()(implicit session: DBSession = AutoSession): List[Collections] =
+  def getAll(implicit session: DBSession = AutoSession): List[Collections] =
     CollectionQueries.all.map(Collections(c1.resultName)).list.apply()
+
+  def getAllMatchingPKs(collectionPKs: Seq[UUID])(implicit session: DBSession = AutoSession): List[Collections] =
+    CollectionQueries.allMatchingPKs(collectionPKs).map(Collections(c1.resultName)).list.apply()
 
 
 class CollectionsDAO(val connectionPoolName: String):
 
-  def getAll(): List[Collections] = NamedDB(connectionPoolName) readOnly { implicit session => CollectionsDAO.getAll() }
+  def getAll: List[Collections] = NamedDB(connectionPoolName) readOnly { implicit session => CollectionsDAO.getAll }
+
+  def getAllMatchingPKs(collectionPKs: Seq[UUID]): List[Collections] =
+    NamedDB(connectionPoolName) readOnly { implicit session => CollectionsDAO.getAllMatchingPKs(collectionPKs) }
