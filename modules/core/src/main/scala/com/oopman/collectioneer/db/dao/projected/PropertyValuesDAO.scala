9362210e033102e09c85047fb85010079ad6c557
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2023-10-02 12:08:54 -0400

    Cleaned up PropertyValuesDAO and PropertyValues entity

--- modules/core/src/main/scala/com/oopman/collectioneer/db/dao/projected/PropertyValuesDAO.scala
+++ modules/core/src/main/scala/com/oopman/collectioneer/db/dao/projected/PropertyValuesDAO.scala
@@ -1,14 +1,18 @@
 package com.oopman.collectioneer.db.dao.projected
 
-import com.oopman.collectioneer.db.entity.projected.PropertyValues
+import com.oopman.collectioneer.db.entity.projected.{PropertyValues, generatePropertyValuesFromWrappedResultSet}
 import com.oopman.collectioneer.db.queries.h2.projected.PropertyValueQueries
 
 import java.util.UUID
 import scalikejdbc.*
 
 object PropertyValuesDAO:
-  def getPropertyValuesByPropertyValueSets(pvsUUIDs: Seq[UUID])(implicit session: DBSession = AutoSession) =
-    PropertyValueQueries.propertyValuesByPropertyValueSets(pvsUUIDs).map(PropertyValues.apply).list.apply()
+  def getPropertyValuesByPropertyValueSets(pvsUUIDs: Seq[UUID])(implicit session: DBSession = AutoSession): List[PropertyValues] =
+    PropertyValueQueries
+      .propertyValuesByPropertyValueSets(pvsUUIDs)
+      .map(generatePropertyValuesFromWrappedResultSet)
+      .list
+      .apply()
 
 
 class PropertyValuesDAO(val connectionPoolName: String):
--- modules/core/src/main/scala/com/oopman/collectioneer/db/entity/projected/PropertyValues.scala
+++ modules/core/src/main/scala/com/oopman/collectioneer/db/entity/projected/PropertyValues.scala
@@ -5,47 +5,6 @@ import scalikejdbc.*
 
 import java.util.UUID
 
-/*
-case class PropertyValueVarchars
-(
-  propertyValueSetPk: UUID,
-  propertyPk: UUID,
-  pvcValues: Option[List[String]],
-  pviValues: Option[List[Int]]
-)
-
-case class PropertyValueInts
-(
-  propertyValueSetPk: UUID,
-  propertyPk: UUID,
-  pvcValues: Option[List[String]],
-  pviValues: Option[List[Int]]
-)
-*
- */
-
-case class PropertyValueInner
-(
-  propertyValueSetPk: UUID,
-  propertyPk: UUID,
-  pvcValues: Option[List[String]],
-  pviValues: Option[List[Int]]
-)
-
-class PropertyValueInnerTable(val propertyType: PropertyTypes) extends SQLSyntaxSupport[PropertyValueInner]:
-  override val schemaName = Some("public")
-  override val tableName = s"property_value_${propertyType.toString}s"
-
-  def apply(pv: ResultName[PropertyValueInner])(rs: WrappedResultSet) = new PropertyValueInner(
-    propertyValueSetPk = UUID.randomUUID(),
-    propertyPk = UUID.randomUUID(),
-    pvcValues = None,
-    pviValues = None
-  )
-
-val propertyValueVarchars = new PropertyValueInnerTable(PropertyTypes.varchar)
-val propertyValueInts = new PropertyValueInnerTable(PropertyTypes.int)
-
 case class PropertyValues
 (
   propertyName: String,
@@ -56,38 +15,30 @@ case class PropertyValues
   pviValues: List[Int]
 )
 
-// TODO: Move apply function to top level and remove this object
-object PropertyValues extends SQLSyntaxSupport[PropertyValues]:
-  override val schemaName = Some("public")
-  override val tableName = "properties"
-
-  def apply(rs: WrappedResultSet) =
-    val propertyTypes = rs
-      .array("PROPERTY_TYPES")
-      .getArray
-      .asInstanceOf[Array[Object]]
-      .map(s => PropertyTypes.valueOf(s.asInstanceOf[String]))
-      .toList
-    val pvcValues = rs
-      .array("PVC_VALUES")
-      .getArray
-      .asInstanceOf[Array[Object]]
-      .map(_.asInstanceOf[String])
-      .toList
-    val pviValues = rs
-      .array("PVI_VALUES")
-      .getArray
-      .asInstanceOf[Array[Object]]
-      .map(_.asInstanceOf[Int])
-      .toList
-    new PropertyValues(
-      propertyName = rs.string("PROPERTY_NAME"),
-      propertyTypes = propertyTypes,
-      propertyValueSetPk = UUID.fromString(rs.string("PROPERTY_VALUE_SET_PK")),
-      propertyPk = UUID.fromString(rs.string("PROPERTY_PK")),
-      pvcValues = pvcValues,
-      pviValues = pviValues
-    )
-
-val pv1 = PropertyValues.syntax("pv1")
-
+def generatePropertyValuesFromWrappedResultSet(rs: WrappedResultSet) =
+  val propertyTypes = rs
+    .array("PROPERTY_TYPES")
+    .getArray
+    .asInstanceOf[Array[Object]]
+    .map(s => PropertyTypes.valueOf(s.asInstanceOf[String]))
+    .toList
+  val pvcValues = rs
+    .array("PVC_VALUES")
+    .getArray
+    .asInstanceOf[Array[Object]]
+    .map(_.asInstanceOf[String])
+    .toList
+  val pviValues = rs
+    .array("PVI_VALUES")
+    .getArray
+    .asInstanceOf[Array[Object]]
+    .map(_.asInstanceOf[Int])
+    .toList
+  PropertyValues(
+    propertyName = rs.string("PROPERTY_NAME"),
+    propertyTypes = propertyTypes,
+    propertyValueSetPk = UUID.fromString(rs.string("PROPERTY_VALUE_SET_PK")),
+    propertyPk = UUID.fromString(rs.string("PROPERTY_PK")),
+    pvcValues = pvcValues,
+    pviValues = pviValues
+  )
