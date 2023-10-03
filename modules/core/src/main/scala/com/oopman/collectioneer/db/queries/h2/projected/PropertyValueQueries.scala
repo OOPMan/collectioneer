5c78fa2da86ea0e326ca70f3f6a7274c787a33b5
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2023-10-03 16:27:03 -0400

    Updated PropertyValueQueries.propertyValuesByPropertyValueSets to include all propoerty value tables

--- modules/core/src/main/scala/com/oopman/collectioneer/db/queries/h2/projected/PropertyValueQueries.scala
+++ modules/core/src/main/scala/com/oopman/collectioneer/db/queries/h2/projected/PropertyValueQueries.scala
@@ -13,23 +13,430 @@ object PropertyValueQueries {
         pv.*
       FROM PROPERTY AS p
       INNER JOIN (
-          SELECT
-            pvc.PROPERTY_VALUE_SET_PK,
-            pvc.PROPERTY_PK,
-            ARRAY_AGG(pvc.PROPERTY_VALUE) AS PVC_VALUES,
-            ARRAY[] as PVI_VALUES
-          FROM PROPERTY_VALUE_VARCHAR as pvc
-          WHERE pvc.PROPERTY_VALUE_SET_PK IN (${pvsUUIDs})
-          GROUP BY pvc.PROPERTY_VALUE_SET_PK, pvc.PROPERTY_PK
+          /* Varchar Property Values */
+            SELECT
+              PROPERTY_VALUE_SET_PK,
+              PROPERTY_PK,
+              ARRAY_AGG(PROPERTY_VALUE) AS VARCHAR_VALUES,
+              ARRAY[] AS VARBINARY_VALUES,
+              ARRAY[] AS TINYINT_VALUES,
+              ARRAY[] AS SMALLINT_VALUES,
+              ARRAY[] AS INT_VALUES,
+              ARRAY[] AS BIGINT_VALUES,
+              ARRAY[] AS NUMERIC_VALUES,
+              ARRAY[] AS FLOAT_VALUES,
+              ARRAY[] AS DOUBLE_VALUES,
+              ARRAY[] AS BOOLEAN_VALUES,
+              ARRAY[] AS DATE_VALUES,
+              ARRAY[] AS TIME_VALUES,
+              ARRAY[] AS TIMESTAMP_VALUES,
+              ARRAY[] AS CLOB_VALUES,
+              ARRAY[] AS BLOB_VALUES,
+              ARRAY[] AS UUID_VALUES,
+              ARRAY[] AS JSON_VALUES
+            FROM PROPERTY_VALUE_VARCHAR
+            WHERE PROPERTY_VALUE_SET_PK IN (${pvsUUIDs})
+            GROUP BY PROPERTY_VALUE_SET_PK, PROPERTY_PK
+          /* Varbinary Property Values */
           UNION
-          SELECT
-            pvi.PROPERTY_VALUE_SET_PK,
-            pvi.PROPERTY_PK,
-            ARRAY[] as PVC_VALUES,
-            ARRAY_AGG(pvi.PROPERTY_VALUE) AS PVI_VALUES
-          FROM PROPERTY_VALUE_INT as pvi
-          WHERE pvi.PROPERTY_VALUE_SET_PK IN (${pvsUUIDs})
-          GROUP BY pvi.PROPERTY_VALUE_SET_PK, pvi.PROPERTY_PK
+            SELECT
+              PROPERTY_VALUE_SET_PK,
+              PROPERTY_PK,
+              ARRAY[] AS VARCHAR_VALUES,
+              ARRAY_AGG(PROPERTY_VALUE) AS VARBINARY_VALUES,
+              ARRAY[] AS TINYINT_VALUES,
+              ARRAY[] AS SMALLINT_VALUES,
+              ARRAY[] AS INT_VALUES,
+              ARRAY[] AS BIGINT_VALUES,
+              ARRAY[] AS NUMERIC_VALUES,
+              ARRAY[] AS FLOAT_VALUES,
+              ARRAY[] AS DOUBLE_VALUES,
+              ARRAY[] AS BOOLEAN_VALUES,
+              ARRAY[] AS DATE_VALUES,
+              ARRAY[] AS TIME_VALUES,
+              ARRAY[] AS TIMESTAMP_VALUES,
+              ARRAY[] AS CLOB_VALUES,
+              ARRAY[] AS BLOB_VALUES,
+              ARRAY[] AS UUID_VALUES,
+              ARRAY[] AS JSON_VALUES
+            FROM PROPERTY_VALUE_VARBINARY
+            WHERE PROPERTY_VALUE_SET_PK IN (${pvsUUIDs})
+            GROUP BY PROPERTY_VALUE_SET_PK, PROPERTY_PK
+          /* Tinyint Property Values */
+          UNION
+            SELECT
+              PROPERTY_VALUE_SET_PK,
+              PROPERTY_PK,
+              ARRAY[] AS VARCHAR_VALUES,
+              ARRAY[] AS VARBINARY_VALUES,
+              ARRAY_AGG(PROPERTY_VALUE) AS TINYINT_VALUES,
+              ARRAY[] AS SMALLINT_VALUES,
+              ARRAY[] AS INT_VALUES,
+              ARRAY[] AS BIGINT_VALUES,
+              ARRAY[] AS NUMERIC_VALUES,
+              ARRAY[] AS FLOAT_VALUES,
+              ARRAY[] AS DOUBLE_VALUES,
+              ARRAY[] AS BOOLEAN_VALUES,
+              ARRAY[] AS DATE_VALUES,
+              ARRAY[] AS TIME_VALUES,
+              ARRAY[] AS TIMESTAMP_VALUES,
+              ARRAY[] AS CLOB_VALUES,
+              ARRAY[] AS BLOB_VALUES,
+              ARRAY[] AS UUID_VALUES,
+              ARRAY[] AS JSON_VALUES
+            FROM PROPERTY_VALUE_TINYINT
+            WHERE PROPERTY_VALUE_SET_PK IN (${pvsUUIDs})
+            GROUP BY PROPERTY_VALUE_SET_PK, PROPERTY_PK
+          /* Smallint Property Values */
+          UNION
+            SELECT
+              PROPERTY_VALUE_SET_PK,
+              PROPERTY_PK,
+              ARRAY[] AS VARCHAR_VALUES,
+              ARRAY[] AS VARBINARY_VALUES,
+              ARRAY[] AS TINYINT_VALUES,
+              ARRAY_AGG(PROPERTY_VALUE) AS SMALLINT_VALUES,
+              ARRAY[] AS INT_VALUES,
+              ARRAY[] AS BIGINT_VALUES,
+              ARRAY[] AS NUMERIC_VALUES,
+              ARRAY[] AS FLOAT_VALUES,
+              ARRAY[] AS DOUBLE_VALUES,
+              ARRAY[] AS BOOLEAN_VALUES,
+              ARRAY[] AS DATE_VALUES,
+              ARRAY[] AS TIME_VALUES,
+              ARRAY[] AS TIMESTAMP_VALUES,
+              ARRAY[] AS CLOB_VALUES,
+              ARRAY[] AS BLOB_VALUES,
+              ARRAY[] AS UUID_VALUES,
+              ARRAY[] AS JSON_VALUES
+          FROM PROPERTY_VALUE_SMALLINT
+          WHERE PROPERTY_VALUE_SET_PK IN (${pvsUUIDs})
+          GROUP BY PROPERTY_VALUE_SET_PK, PROPERTY_PK
+          /* Int Property Values */
+          UNION
+            SELECT
+              PROPERTY_VALUE_SET_PK,
+              PROPERTY_PK,
+              ARRAY[] AS VARCHAR_VALUES,
+              ARRAY[] AS VARBINARY_VALUES,
+              ARRAY[] AS TINYINT_VALUES,
+              ARRAY[] AS SMALLINT_VALUES,
+              ARRAY_AGG(PROPERTY_VALUE) AS INT_VALUES,
+              ARRAY[] AS BIGINT_VALUES,
+              ARRAY[] AS NUMERIC_VALUES,
+              ARRAY[] AS FLOAT_VALUES,
+              ARRAY[] AS DOUBLE_VALUES,
+              ARRAY[] AS BOOLEAN_VALUES,
+              ARRAY[] AS DATE_VALUES,
+              ARRAY[] AS TIME_VALUES,
+              ARRAY[] AS TIMESTAMP_VALUES,
+              ARRAY[] AS CLOB_VALUES,
+              ARRAY[] AS BLOB_VALUES,
+              ARRAY[] AS UUID_VALUES,
+              ARRAY[] AS JSON_VALUES
+            FROM PROPERTY_VALUE_INT
+            WHERE PROPERTY_VALUE_SET_PK IN (${pvsUUIDs})
+            GROUP BY PROPERTY_VALUE_SET_PK, PROPERTY_PK
+          /* Bigint Property Values */
+          UNION
+            SELECT
+              PROPERTY_VALUE_SET_PK,
+              PROPERTY_PK,
+              ARRAY[] AS VARCHAR_VALUES,
+              ARRAY[] AS VARBINARY_VALUES,
+              ARRAY[] AS TINYINT_VALUES,
+              ARRAY[] AS SMALLINT_VALUES,
+              ARRAY[] AS INT_VALUES,
+              ARRAY_AGG(PROPERTY_VALUE) AS BIGINT_VALUES,
+              ARRAY[] AS NUMERIC_VALUES,
+              ARRAY[] AS FLOAT_VALUES,
+              ARRAY[] AS DOUBLE_VALUES,
+              ARRAY[] AS BOOLEAN_VALUES,
+              ARRAY[] AS DATE_VALUES,
+              ARRAY[] AS TIME_VALUES,
+              ARRAY[] AS TIMESTAMP_VALUES,
+              ARRAY[] AS CLOB_VALUES,
+              ARRAY[] AS BLOB_VALUES,
+              ARRAY[] AS UUID_VALUES,
+              ARRAY[] AS JSON_VALUES
+            FROM PROPERTY_VALUE_BIGINT
+            WHERE PROPERTY_VALUE_SET_PK IN (${pvsUUIDs})
+            GROUP BY PROPERTY_VALUE_SET_PK, PROPERTY_PK
+          /* Numeric Property Values */
+          UNION
+            SELECT
+              PROPERTY_VALUE_SET_PK,
+              PROPERTY_PK,
+              ARRAY[] AS VARCHAR_VALUES,
+              ARRAY[] AS VARBINARY_VALUES,
+              ARRAY[] AS TINYINT_VALUES,
+              ARRAY[] AS SMALLINT_VALUES,
+              ARRAY[] AS INT_VALUES,
+              ARRAY[] AS BIGINT_VALUES,
+              ARRAY_AGG(PROPERTY_VALUE) AS NUMERIC_VALUES,
+              ARRAY[] AS FLOAT_VALUES,
+              ARRAY[] AS DOUBLE_VALUES,
+              ARRAY[] AS BOOLEAN_VALUES,
+              ARRAY[] AS DATE_VALUES,
+              ARRAY[] AS TIME_VALUES,
+              ARRAY[] AS TIMESTAMP_VALUES,
+              ARRAY[] AS CLOB_VALUES,
+              ARRAY[] AS BLOB_VALUES,
+              ARRAY[] AS UUID_VALUES,
+              ARRAY[] AS JSON_VALUES
+            FROM PROPERTY_VALUE_NUMERIC
+            WHERE PROPERTY_VALUE_SET_PK IN (${pvsUUIDs})
+            GROUP BY PROPERTY_VALUE_SET_PK, PROPERTY_PK
+          /* Float Property Values */
+          UNION
+            SELECT
+              PROPERTY_VALUE_SET_PK,
+              PROPERTY_PK,
+              ARRAY[] AS VARCHAR_VALUES,
+              ARRAY[] AS VARBINARY_VALUES,
+              ARRAY[] AS TINYINT_VALUES,
+              ARRAY[] AS SMALLINT_VALUES,
+              ARRAY[] AS INT_VALUES,
+              ARRAY[] AS BIGINT_VALUES,
+              ARRAY[] AS NUMERIC_VALUES,
+              ARRAY_AGG(PROPERTY_VALUE) AS FLOAT_VALUES,
+              ARRAY[] AS DOUBLE_VALUES,
+              ARRAY[] AS BOOLEAN_VALUES,
+              ARRAY[] AS DATE_VALUES,
+              ARRAY[] AS TIME_VALUES,
+              ARRAY[] AS TIMESTAMP_VALUES,
+              ARRAY[] AS CLOB_VALUES,
+              ARRAY[] AS BLOB_VALUES,
+              ARRAY[] AS UUID_VALUES,
+              ARRAY[] AS JSON_VALUES
+            FROM PROPERTY_VALUE_FLOAT
+            WHERE PROPERTY_VALUE_SET_PK IN (${pvsUUIDs})
+            GROUP BY PROPERTY_VALUE_SET_PK, PROPERTY_PK
+          /* Double Property Values */
+          UNION
+            SELECT
+              PROPERTY_VALUE_SET_PK,
+              PROPERTY_PK,
+              ARRAY[] AS VARCHAR_VALUES,
+              ARRAY[] AS VARBINARY_VALUES,
+              ARRAY[] AS TINYINT_VALUES,
+              ARRAY[] AS SMALLINT_VALUES,
+              ARRAY[] AS INT_VALUES,
+              ARRAY[] AS BIGINT_VALUES,
+              ARRAY[] AS NUMERIC_VALUES,
+              ARRAY[] AS FLOAT_VALUES,
+              ARRAY_AGG(PROPERTY_VALUE) AS DOUBLE_VALUES,
+              ARRAY[] AS BOOLEAN_VALUES,
+              ARRAY[] AS DATE_VALUES,
+              ARRAY[] AS TIME_VALUES,
+              ARRAY[] AS TIMESTAMP_VALUES,
+              ARRAY[] AS CLOB_VALUES,
+              ARRAY[] AS BLOB_VALUES,
+              ARRAY[] AS UUID_VALUES,
+              ARRAY[] AS JSON_VALUES
+            FROM PROPERTY_VALUE_DOUBLE
+            WHERE PROPERTY_VALUE_SET_PK IN (${pvsUUIDs})
+            GROUP BY PROPERTY_VALUE_SET_PK, PROPERTY_PK
+          /* Boolean Property Values */
+          UNION
+            SELECT
+              PROPERTY_VALUE_SET_PK,
+              PROPERTY_PK,
+              ARRAY[] AS VARCHAR_VALUES,
+              ARRAY[] AS VARBINARY_VALUES,
+              ARRAY[] AS TINYINT_VALUES,
+              ARRAY[] AS SMALLINT_VALUES,
+              ARRAY[] AS INT_VALUES,
+              ARRAY[] AS BIGINT_VALUES,
+              ARRAY[] AS NUMERIC_VALUES,
+              ARRAY[] AS FLOAT_VALUES,
+              ARRAY[] AS DOUBLE_VALUES,
+              ARRAY_AGG(PROPERTY_VALUE) AS BOOLEAN_VALUES,
+              ARRAY[] AS DATE_VALUES,
+              ARRAY[] AS TIME_VALUES,
+              ARRAY[] AS TIMESTAMP_VALUES,
+              ARRAY[] AS CLOB_VALUES,
+              ARRAY[] AS BLOB_VALUES,
+              ARRAY[] AS UUID_VALUES,
+              ARRAY[] AS JSON_VALUES
+            FROM PROPERTY_VALUE_BOOLEAN
+            WHERE PROPERTY_VALUE_SET_PK IN (${pvsUUIDs})
+            GROUP BY PROPERTY_VALUE_SET_PK, PROPERTY_PK
+          /* Date Property Values */
+          UNION
+            SELECT
+              PROPERTY_VALUE_SET_PK,
+              PROPERTY_PK,
+              ARRAY[] AS VARCHAR_VALUES,
+              ARRAY[] AS VARBINARY_VALUES,
+              ARRAY[] AS TINYINT_VALUES,
+              ARRAY[] AS SMALLINT_VALUES,
+              ARRAY[] AS INT_VALUES,
+              ARRAY[] AS BIGINT_VALUES,
+              ARRAY[] AS NUMERIC_VALUES,
+              ARRAY[] AS FLOAT_VALUES,
+              ARRAY[] AS DOUBLE_VALUES,
+              ARRAY[] AS BOOLEAN_VALUES,
+              ARRAY_AGG(PROPERTY_VALUE) AS DATE_VALUES,
+              ARRAY[] AS TIME_VALUES,
+              ARRAY[] AS TIMESTAMP_VALUES,
+              ARRAY[] AS CLOB_VALUES,
+              ARRAY[] AS BLOB_VALUES,
+              ARRAY[] AS UUID_VALUES,
+              ARRAY[] AS JSON_VALUES
+            FROM PROPERTY_VALUE_DATE
+            WHERE PROPERTY_VALUE_SET_PK IN (${pvsUUIDs})
+            GROUP BY PROPERTY_VALUE_SET_PK, PROPERTY_PK
+          /* Time Property Values */
+          UNION
+            SELECT
+              PROPERTY_VALUE_SET_PK,
+              PROPERTY_PK,
+              ARRAY[] AS VARCHAR_VALUES,
+              ARRAY[] AS VARBINARY_VALUES,
+              ARRAY[] AS TINYINT_VALUES,
+              ARRAY[] AS SMALLINT_VALUES,
+              ARRAY[] AS INT_VALUES,
+              ARRAY[] AS BIGINT_VALUES,
+              ARRAY[] AS NUMERIC_VALUES,
+              ARRAY[] AS FLOAT_VALUES,
+              ARRAY[] AS DOUBLE_VALUES,
+              ARRAY[] AS BOOLEAN_VALUES,
+              ARRAY[] AS DATE_VALUES,
+              ARRAY_AGG(PROPERTY_VALUE) AS TIME_VALUES,
+              ARRAY[] AS TIMESTAMP_VALUES,
+              ARRAY[] AS CLOB_VALUES,
+              ARRAY[] AS BLOB_VALUES,
+              ARRAY[] AS UUID_VALUES,
+              ARRAY[] AS JSON_VALUES
+            FROM PROPERTY_VALUE_TIME
+            WHERE PROPERTY_VALUE_SET_PK IN (${pvsUUIDs})
+            GROUP BY PROPERTY_VALUE_SET_PK, PROPERTY_PK
+          /* Timestamp Property Values */
+          UNION
+            SELECT
+              PROPERTY_VALUE_SET_PK,
+              PROPERTY_PK,
+              ARRAY[] AS VARCHAR_VALUES,
+              ARRAY[] AS VARBINARY_VALUES,
+              ARRAY[] AS TINYINT_VALUES,
+              ARRAY[] AS SMALLINT_VALUES,
+              ARRAY[] AS INT_VALUES,
+              ARRAY[] AS BIGINT_VALUES,
+              ARRAY[] AS NUMERIC_VALUES,
+              ARRAY[] AS FLOAT_VALUES,
+              ARRAY[] AS DOUBLE_VALUES,
+              ARRAY[] AS BOOLEAN_VALUES,
+              ARRAY[] AS DATE_VALUES,
+              ARRAY[] AS TIME_VALUES,
+              ARRAY_AGG(PROPERTY_VALUE) AS TIMESTAMP_VALUES,
+              ARRAY[] AS CLOB_VALUES,
+              ARRAY[] AS BLOB_VALUES,
+              ARRAY[] AS UUID_VALUES,
+              ARRAY[] AS JSON_VALUES
+            FROM PROPERTY_VALUE_TIMESTAMP
+            WHERE PROPERTY_VALUE_SET_PK IN (${pvsUUIDs})
+            GROUP BY PROPERTY_VALUE_SET_PK, PROPERTY_PK
+          /* CLOB Property Values */
+          UNION
+            SELECT
+              PROPERTY_VALUE_SET_PK,
+              PROPERTY_PK,
+              ARRAY[] AS VARCHAR_VALUES,
+              ARRAY[] AS VARBINARY_VALUES,
+              ARRAY[] AS TINYINT_VALUES,
+              ARRAY[] AS SMALLINT_VALUES,
+              ARRAY[] AS INT_VALUES,
+              ARRAY[] AS BIGINT_VALUES,
+              ARRAY[] AS NUMERIC_VALUES,
+              ARRAY[] AS FLOAT_VALUES,
+              ARRAY[] AS DOUBLE_VALUES,
+              ARRAY[] AS BOOLEAN_VALUES,
+              ARRAY[] AS DATE_VALUES,
+              ARRAY[] AS TIME_VALUES,
+              ARRAY[] AS TIMESTAMP_VALUES,
+              ARRAY_AGG(PROPERTY_VALUE) AS CLOB_VALUES,
+              ARRAY[] AS BLOB_VALUES,
+              ARRAY[] AS UUID_VALUES,
+              ARRAY[] AS JSON_VALUES
+            FROM PROPERTY_VALUE_CLOB
+            WHERE PROPERTY_VALUE_SET_PK IN (${pvsUUIDs})
+            GROUP BY PROPERTY_VALUE_SET_PK, PROPERTY_PK
+          /* BLOB Property Values */
+          UNION
+            SELECT
+              PROPERTY_VALUE_SET_PK,
+              PROPERTY_PK,
+              ARRAY[] AS VARCHAR_VALUES,
+              ARRAY[] AS VARBINARY_VALUES,
+              ARRAY[] AS TINYINT_VALUES,
+              ARRAY[] AS SMALLINT_VALUES,
+              ARRAY[] AS INT_VALUES,
+              ARRAY[] AS BIGINT_VALUES,
+              ARRAY[] AS NUMERIC_VALUES,
+              ARRAY[] AS FLOAT_VALUES,
+              ARRAY[] AS DOUBLE_VALUES,
+              ARRAY[] AS BOOLEAN_VALUES,
+              ARRAY[] AS DATE_VALUES,
+              ARRAY[] AS TIME_VALUES,
+              ARRAY[] AS TIMESTAMP_VALUES,
+              ARRAY[] AS CLOB_VALUES,
+              ARRAY_AGG(PROPERTY_VALUE) AS BLOB_VALUES,
+              ARRAY[] AS UUID_VALUES,
+              ARRAY[] AS JSON_VALUES
+            FROM PROPERTY_VALUE_BLOB
+            WHERE PROPERTY_VALUE_SET_PK IN (${pvsUUIDs})
+            GROUP BY PROPERTY_VALUE_SET_PK, PROPERTY_PK
+          /* UUID Property Values */
+          UNION
+            SELECT
+              PROPERTY_VALUE_SET_PK,
+              PROPERTY_PK,
+              ARRAY[] AS VARCHAR_VALUES,
+              ARRAY[] AS VARBINARY_VALUES,
+              ARRAY[] AS TINYINT_VALUES,
+              ARRAY[] AS SMALLINT_VALUES,
+              ARRAY[] AS INT_VALUES,
+              ARRAY[] AS BIGINT_VALUES,
+              ARRAY[] AS NUMERIC_VALUES,
+              ARRAY[] AS FLOAT_VALUES,
+              ARRAY[] AS DOUBLE_VALUES,
+              ARRAY[] AS BOOLEAN_VALUES,
+              ARRAY[] AS DATE_VALUES,
+              ARRAY[] AS TIME_VALUES,
+              ARRAY[] AS TIMESTAMP_VALUES,
+              ARRAY[] AS CLOB_VALUES,
+              ARRAY[] AS BLOB_VALUES,
+              ARRAY_AGG(PROPERTY_VALUE) AS UUID_VALUES,
+              ARRAY[] AS JSON_VALUES
+            FROM PROPERTY_VALUE_UUID
+            WHERE PROPERTY_VALUE_SET_PK IN (${pvsUUIDs})
+            GROUP BY PROPERTY_VALUE_SET_PK, PROPERTY_PK
+          /* JSON Property Values */
+          UNION
+            SELECT
+              PROPERTY_VALUE_SET_PK,
+              PROPERTY_PK,
+              ARRAY[] AS VARCHAR_VALUES,
+              ARRAY[] AS VARBINARY_VALUES,
+              ARRAY[] AS TINYINT_VALUES,
+              ARRAY[] AS SMALLINT_VALUES,
+              ARRAY[] AS INT_VALUES,
+              ARRAY[] AS BIGINT_VALUES,
+              ARRAY[] AS NUMERIC_VALUES,
+              ARRAY[] AS FLOAT_VALUES,
+              ARRAY[] AS DOUBLE_VALUES,
+              ARRAY[] AS BOOLEAN_VALUES,
+              ARRAY[] AS DATE_VALUES,
+              ARRAY[] AS TIME_VALUES,
+              ARRAY[] AS TIMESTAMP_VALUES,
+              ARRAY[] AS CLOB_VALUES,
+              ARRAY[] AS BLOB_VALUES,
+              ARRAY[] AS UUID_VALUES,
+              ARRAY_AGG(PROPERTY_VALUE) AS JSON_VALUES
+            FROM PROPERTY_VALUE_JSON
+            WHERE PROPERTY_VALUE_SET_PK IN (${pvsUUIDs})
+            GROUP BY PROPERTY_VALUE_SET_PK, PROPERTY_PK
       ) AS pv ON pv.PROPERTY_PK = p.PK
       ORDER BY pv.PROPERTY_VALUE_SET_PK, pv.PROPERTY_PK
        """
