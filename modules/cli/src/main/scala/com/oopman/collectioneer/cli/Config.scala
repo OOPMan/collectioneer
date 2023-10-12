7375991dd7d82bc880a318e6db8cc26d50e3bb82
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2023-10-12 17:14:25 -0400

    Removed Verb and Subject enums and began adding config elements to support plugins better

--- modules/cli/src/main/scala/com/oopman/collectioneer/cli/Config.scala
+++ modules/cli/src/main/scala/com/oopman/collectioneer/cli/Config.scala
@@ -1,31 +1,54 @@
 package com.oopman.collectioneer.cli
 
+import io.circe.*
+import io.circe.generic.auto.*
+import io.circe.syntax.*
+
 import java.io.File
 import java.util.UUID
+import scala.collection.immutable.Map.from
 
-enum Subjects:
-  case database extends Subjects
-  case collections extends Subjects
-  case properties extends Subjects
-  case plugins extends Subjects
+case class Verb
+(
+  name: String,
+  help: String
+)
 
-enum Verbs(val subjects: List[Subjects] = Nil):
-  case create extends Verbs
-  case delete extends Verbs
-  case update extends Verbs
-  case list extends Verbs(List(Subjects.collections, Subjects.properties, Subjects.plugins))
-  case get extends Verbs(List(Subjects.collections, Subjects.properties))
+object Verb:
+  def apply(name: String, help: String) =
+    new Verb(name.toLowerCase, help)
+
+  val create = Verb("create", "Create")
+  val delete = Verb("delete", "Delete")
+  val update = Verb("update", "Update")
+  val list = Verb("list", "List")
+  val get = Verb("get", "Retrieve")
+
+case class Subject
+(
+  name: String,
+  help: Map[Verb, String]
+)
+
+object Subject:
+  def apply(name: String, help: Map[Verb, String]) =
+    new Subject(name.toLowerCase, help)
+
+  val collections = Subject("collections", Map())
+  val properties = Subject("properties", Map())
 
 enum OutputFormat:
   case json extends OutputFormat
   case yaml extends OutputFormat
 
 // TODO: Add sub-config classes for verbs
+trait Subconfig
 
 case class Config
 (
-  verb: Option[Verbs] = None,
-  subject: Option[Subjects] = None,
+  action: Config => Json = config => "".asJson,
+  verb: Option[Verb] = None,
+  subject: Option[Subject] = None,
   datasourceUri: String = "jdbc:h2:./collection",
   datasourceUsername: String = "sa",
   datasourcePassword: String = "",
@@ -33,4 +56,6 @@ case class Config
   debug: Boolean = false,
   outputFormat: OutputFormat = OutputFormat.yaml,
   uuids: List[UUID] = Nil,
+  usePlugin: Option[String] = None,
+  subconfigs: Map[String, Subconfig] = Map(),
 )
