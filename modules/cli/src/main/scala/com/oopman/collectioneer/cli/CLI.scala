7ccc074fceb0c3bc55ea97a9d81345c022d10cff
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2023-10-12 17:15:04 -0400

    Updated CLI to generate commands structure dynamically using both plugins and base actions

--- modules/cli/src/main/scala/com/oopman/collectioneer/cli/CLI.scala
+++ modules/cli/src/main/scala/com/oopman/collectioneer/cli/CLI.scala
@@ -1,6 +1,6 @@
 package com.oopman.collectioneer.cli
 
-import com.oopman.collectioneer.cli.{Config, Subjects, Verbs}
+import com.oopman.collectioneer.cli.{Config, Subject, Verb}
 import com.oopman.collectioneer.db.migrations.executeMigrations
 import com.oopman.collectioneer.db.dao.CollectionsDAO
 import com.oopman.collectioneer.db.entity.Collection
@@ -15,20 +15,41 @@ import io.circe.generic.auto.*
 import io.circe.syntax.*
 import cats.syntax.either.*
 import com.oopman.collectioneer.cli.actions.list.{getCollections, listCollections, listPlugins, listProperties}
+import com.oopman.collectioneer.plugins.CLIPlugin
+import distage.{Injector, ModuleBase}
+import distage.plugins.PluginConfig
 import io.circe.Json
 import io.circe.yaml.*
 import io.circe.yaml.syntax.*
+import izumi.distage.model.exceptions.runtime.ProvisioningException
+import izumi.distage.plugins.load.PluginLoader
 
-val actionsMap: Map[(Option[Verbs], Option[Subjects]), Config => Json] = Map(
-  (Some(Verbs.list), Some(Subjects.collections)) -> listCollections,
-  (Some(Verbs.list), Some(Subjects.properties)) -> listProperties,
-  (Some(Verbs.list), Some(Subjects.plugins)) -> listPlugins,
-  (Some(Verbs.get), Some(Subjects.collections)) -> getCollections,
+val actionsMap: Map[(Option[Verb], Option[Subject]), Config => Json] = Map(
+  (Some(Verb.list), Some(Subject.collections)) -> listCollections,
+  (Some(Verb.list), Some(Subject.properties)) -> listProperties,
+//  (Some(Verb.list), Some(Subject.plugins)) -> listPlugins,
+  (Some(Verb.get), Some(Subject.collections)) -> getCollections,
 )
 
+type Action = Config => Json
+type ActionListItem = (Verb, Subject, Option[String], Action, List[OParser[_, Config]])
+type PluginNameActionListItemMap = Map[Option[String], ActionListItem]
+type SubjectMap = Map[Subject, PluginNameActionListItemMap]
+type ActionMap = Map[Verb, SubjectMap]
+
 object CLI:
+  val pluginConfig = PluginConfig.cached("com.oopman.collectioneer.plugins")
+  val pluginModules = PluginLoader().load(pluginConfig).result.merge
+  val plugins =
+    try Injector()
+      .produceGet[Set[CLIPlugin]](pluginModules)
+      .unsafeGet()
+      .toList
+    catch case e: ProvisioningException => Nil
+  // TODO: Build actionsMap from defaults + additional actions provided by Plugins
+  // TODO: Generate commands and children using actionsMap
   val builder = OParser.builder[Config]
-  val parser =
+  val parser: OParser[Unit, Config] =
     import builder._
     val uuidArgs = arg[String]("<UUID>...")
       .unbounded()
@@ -43,8 +64,63 @@ object CLI:
       .optional()
       .action((virtual, config) => config) // TODO: Set options on something
       .text("TODO:")
-    OParser.sequence(
-      programName("Collectioneer CLI"),
+
+    def cliSafeName(name: String) = name
+      .toLowerCase
+      .replaceAll("[^a-z- ]", "")
+      .replaceAll("\\s+", "-")
+
+    val pluginActions: List[ActionListItem] = plugins
+      .flatMap(plugin => plugin
+        .getActions(builder)
+        .map((verb, subject, action, oparserItems) => (verb, subject, Some(cliSafeName(plugin.getShortName)), action, oparserItems))
+      )
+    val baseActions: List[ActionListItem] = List(
+      (Verb.list, Subject.collections, None, listCollections, List(deletedOpt, virtualOp)),
+      (Verb.list, Subject.properties, None, listProperties, List(deletedOpt)),
+      (Verb.get, Subject.collections, None, getCollections, List(uuidArgs)),
+      // TODO: Add more
+    )
+
+    val actionsMap: ActionMap = baseActions
+      .concat(pluginActions)
+      .groupBy(_._1)
+      .map((verb: Verb, actionList: List[ActionListItem]) =>
+        verb -> actionList
+          .groupBy(_._2)
+          .map((subject: Subject, actionList: List[ActionListItem]) =>
+            subject -> actionList
+              .groupBy(_._3)
+              .map((pluginNameOption: Option[String], actionList: List[ActionListItem]) =>
+                pluginNameOption -> actionList.head))
+      )
+
+    def generateSubjectCmds(verb: Verb)(subject: Subject, pluginNameActionListItemMap: PluginNameActionListItemMap) =
+      val pluginNames = pluginNameActionListItemMap.keySet.flatten
+      val pluginNamesString = pluginNames.mkString(", ")
+      val actionListItems = pluginNameActionListItemMap.values.toList
+      val pluginNameOpt = opt[String]("use-plugin")
+        .text(s"Specify whether to use a specific plugin. Valid options: " + pluginNamesString)
+        .maxOccurs(1)
+        .optional()
+        .action((pluginName, config) => config.copy(usePlugin = Some(pluginName)))
+        .validate(pluginName =>
+          if pluginNames.contains(pluginName) then success
+          else failure("Invalid plugin name. Valid options: " + pluginNamesString))
+      val oParserItems = pluginNameOpt :: actionListItems.flatMap(_._5)
+      cmd(subject.name)
+        .text(subject.help.getOrElse(verb, ""))
+        .action((_, config) => config.copy(subject = Some(subject)))
+        .children(oParserItems: _*)
+
+    def generateVerbCmd(verb: Verb, subjectsMap: SubjectMap) =
+      val oParserItems = subjectsMap.map(generateSubjectCmds(verb)).toList
+      cmd(verb.name)
+        .text(verb.help)
+        .action((_, config) => config.copy(verb = Some(verb)))
+        .children(oParserItems: _*)
+
+    val oParserItems: List[OParser[_, Config]] = List(
       head("collectioneer-cli", "master"),
       help("help").text("Coming soon..."),
       opt[Unit]("json")
@@ -66,49 +142,13 @@ object CLI:
       opt[String]("datasourcePassword")
         .text("Datasource password")
         .action((datasourcePassword, config) => config.copy(datasourcePassword = datasourcePassword)),
-      cmd(Verbs.list.toString)
-        .text("List Collections or Properties")
-        .action((_, config) => config.copy(verb = Some(Verbs.list)))
-        .children(
-          cmd(Subjects.collections.toString)
-            .text("List All Collections")
-            .action((_, config) => config.copy(subject = Some(Subjects.collections)))
-            .children(
-              deletedOpt,
-              virtualOp
-              // TODO: Add filtering options
-            ),
-          cmd(Subjects.properties.toString)
-            .text("List All Properties")
-            .action((_, config) => config.copy(subject = Some(Subjects.properties)))
-            .children(
-              deletedOpt
-              // TODO: Add filtering options
-            ),
-          cmd(Subjects.plugins.toString)
-            .text("List All Plugins")
-            .action((_, config) => config.copy(subject = Some(Subjects.plugins)))
-        ),
-      cmd(Verbs.get.toString)
-        .text("Get Collections or Properties")
-        .action((_, config) => config.copy(verb = Some(Verbs.get)))
-        .children(
-          cmd(Subjects.collections.toString)
-            .text("Get 1..n Collections")
-            .action((_, config) => config.copy(subject = Some(Subjects.collections)))
-            .children(
-              uuidArgs
-              // TODO: Add args option for UUIDs
-            ),
-          cmd(Subjects.properties.toString)
-            .text("Get 1..n Properties")
-            .action((_, config) => config.copy(subject = Some(Subjects.properties)))
-            .children(
-              uuidArgs
-              // TODO: Add args option for UUIDs
-            ),
-        )
+    ) ++ actionsMap.map(generateVerbCmd).toList
+
+    OParser.sequence(
+      programName("Collectioneer CLI"),
+      oParserItems: _*
     )
+
   def main(args: Array[String]): Unit =
     OParser.parse(parser, args, Config()) match
       case Some(config) =>
