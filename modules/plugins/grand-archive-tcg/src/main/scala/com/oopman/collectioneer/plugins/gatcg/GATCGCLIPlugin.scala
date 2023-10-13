c4c3e72807f1660f364bd3f8d4e5abab40c73240
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2023-10-13 11:37:36 -0400

    Updated GATCGCLIPlugin to comply with CLIPlugin requirements

--- modules/plugins/grand-archive-tcg/src/main/scala/com/oopman/collectioneer/plugins/gatcg/GATCGCLIPlugin.scala
+++ modules/plugins/grand-archive-tcg/src/main/scala/com/oopman/collectioneer/plugins/gatcg/GATCGCLIPlugin.scala
@@ -1,6 +1,6 @@
 package com.oopman.collectioneer.plugins.gatcg
 
-import com.oopman.collectioneer.cli.{Action, Config, Subject, Verb}
+import com.oopman.collectioneer.cli.{Action, Config, Subconfig, Subject, Verb}
 import com.oopman.collectioneer.plugins.CLIPlugin
 import izumi.distage.plugins.PluginDef
 import scopt.{OParser, OParserBuilder}
@@ -15,12 +15,14 @@ class GATCGCLIPlugin extends CLIPlugin:
 
   override def getVersion: String = "master"
 
+  override def getDefaultSubconfig: Subconfig = GATCGPluginConfig()
+
   override def getActions(builder: OParserBuilder[Config]): List[(Verb, Subject, Action, List[OParser[_, Config]])] =
     List(
-      (Verb.list, Subject.collections, testAction, List()),
+      (Verb.imprt, Subject.database, importDatabase, List()),
     )
 
-  def testAction(config: Config) = "Something".asJson
+  def importDatabase(config: Config) = "Something".asJson
 
 object GATCGCLIPlugin extends PluginDef:
   many[CLIPlugin].add(GATCGCLIPlugin())
---
+++ modules/plugins/grand-archive-tcg/src/main/scala/com/oopman/collectioneer/plugins/gatcg/GATCGPluginConfig.scala
@@ -0,0 +1,10 @@
+package com.oopman.collectioneer.plugins.gatcg
+
+import com.oopman.collectioneer.cli.Subconfig
+
+import java.io.File
+
+case class GATCGPluginConfig
+(
+  grandArchiveTCGJSON: Option[File] = None
+) extends Subconfig
