a019dcefe05e5464fff6e4d5c530cab2b275930d
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2023-10-13 00:33:18 -0400

    Updated GATCGCLIPlugin to match CLIPlugin trait requirements

--- modules/plugins/grand-archive-tcg/src/main/scala/com/oopman/collectioneer/plugins/gatcg/GATCGCLIPlugin.scala
+++ modules/plugins/grand-archive-tcg/src/main/scala/com/oopman/collectioneer/plugins/gatcg/GATCGCLIPlugin.scala
@@ -1,12 +1,26 @@
 package com.oopman.collectioneer.plugins.gatcg
 
+import com.oopman.collectioneer.cli.{Action, Config, Subject, Verb}
 import com.oopman.collectioneer.plugins.CLIPlugin
 import izumi.distage.plugins.PluginDef
+import scopt.{OParser, OParserBuilder}
+import io.circe.*
+import io.circe.generic.auto.*
+import io.circe.syntax.*
 
 class GATCGCLIPlugin extends CLIPlugin:
   override def getName: String = "Grand Archive TCG"
 
+  override def getShortName: String = "gatcg"
+
   override def getVersion: String = "master"
+
+  override def getActions(builder: OParserBuilder[Config]): List[(Verb, Subject, Action, List[OParser[_, Config]])] =
+    List(
+      (Verb.list, Subject.collections, testAction, List()),
+    )
+
+  def testAction(config: Config) = "Something".asJson
 
 object GATCGCLIPlugin extends PluginDef:
   many[CLIPlugin].add(GATCGCLIPlugin())
