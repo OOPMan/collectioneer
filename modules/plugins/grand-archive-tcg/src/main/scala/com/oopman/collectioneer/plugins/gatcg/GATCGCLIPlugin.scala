77e2454ad977ad1df014780801eab9a282eb80d3
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2023-10-24 23:18:50 -0400

    Added test property create from plugin

--- modules/plugins/grand-archive-tcg/src/main/scala/com/oopman/collectioneer/plugins/gatcg/GATCGCLIPlugin.scala
+++ modules/plugins/grand-archive-tcg/src/main/scala/com/oopman/collectioneer/plugins/gatcg/GATCGCLIPlugin.scala
@@ -1,6 +1,9 @@
 package com.oopman.collectioneer.plugins.gatcg
 
-import com.oopman.collectioneer.cli.{Verb, Subject, Config, Subconfig}
+import com.oopman.collectioneer.cli.{Config, Subconfig, Subject, Verb}
+import com.oopman.collectioneer.db.entity.PropertyType
+import com.oopman.collectioneer.db.entity.Property
+import com.oopman.collectioneer.db.dao.PropertiesDAO
 import com.oopman.collectioneer.plugins.CLIPlugin
 import izumi.distage.plugins.PluginDef
 import scopt.{OParser, OParserBuilder}
@@ -22,7 +25,15 @@ class GATCGCLIPlugin extends CLIPlugin:
       (Verb.imprt, Subject.database, importDatabase, List()),
     )
 
-  def importDatabase(config: Config) = "Something".asJson
+  def importDatabase(config: Config) =
+    // TODO: Create GA-specific properties if they don't exist using an UPSERT
+    val propertiesDAO = new PropertiesDAO(config.datasourceUri)
+    propertiesDAO.createProperties(List(Property(
+      propertyName = "newTest",
+      propertyTypes = List(PropertyType.varchar),
+    )))
+    // TODO: Replace with a real response
+    "Something".asJson
 
 object GATCGCLIPlugin extends PluginDef:
   many[CLIPlugin].add(GATCGCLIPlugin())
