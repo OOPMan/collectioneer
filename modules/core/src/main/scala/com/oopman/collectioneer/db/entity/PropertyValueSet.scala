b6bfe988c86e4bd3b216e73ae38bb52789a9ae65
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2023-11-04 00:06:55 -0400

    Refactored PropertyValueSet to raw.PropertyValueSet and added PropertyValueSet trait

--- modules/core/src/main/scala/com/oopman/collectioneer/db/entity/PropertyValueSet.scala
+++ modules/core/src/main/scala/com/oopman/collectioneer/db/entity/PropertyValueSet.scala
@@ -3,22 +3,7 @@ package com.oopman.collectioneer.db.entity
 import java.time.ZonedDateTime
 import java.util.UUID
 
-import scalikejdbc._
+trait PropertyValueSet:
+  val pk: UUID
+  val created: ZonedDateTime
 
-case class PropertyValueSet
-(
-  pk: UUID = UUID.randomUUID(),
-  created: ZonedDateTime = ZonedDateTime.now()
-)
-
-object PropertyValueSet extends SQLSyntaxSupport[PropertyValueSet]:
-  override val schemaName = Some("public")
-  override val tableName = "property_value_set"
-
-  def apply(pvc: ResultName[PropertyValueSet])(rs: WrappedResultSet) =
-    new PropertyValueSet(
-      pk = UUID.fromString(rs.string(pvc.pk)),
-      created = rs.zonedDateTime(pvc.created)
-    )
-
-val pvc1 = PropertyValueSet.syntax("pvc1")
---
+++ modules/core/src/main/scala/com/oopman/collectioneer/db/entity/raw/PropertyValueSet.scala
@@ -0,0 +1,25 @@
+package com.oopman.collectioneer.db.entity.raw
+
+import com.oopman.collectioneer.db.entity.PropertyValueSet
+import scalikejdbc.*
+
+import java.time.ZonedDateTime
+import java.util.UUID
+
+case class PropertyValueSet
+(
+  pk: UUID = UUID.randomUUID(),
+  created: ZonedDateTime = ZonedDateTime.now()
+)
+
+object PropertyValueSet extends SQLSyntaxSupport[PropertyValueSet]:
+  override val schemaName = Some("public")
+  override val tableName = "property_value_set"
+
+  def apply(pvc: ResultName[PropertyValueSet])(rs: WrappedResultSet) =
+    new PropertyValueSet(
+      pk = UUID.fromString(rs.string(pvc.pk)),
+      created = rs.zonedDateTime(pvc.created)
+    )
+
+val pvc1 = PropertyValueSet.syntax("pvc1")
