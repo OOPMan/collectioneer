23d822b65a41fcaaf7cf1682b0203d768299c5ab
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2023-11-07 21:37:23 -0500

    Updated CoreProperties to store UUID strings on a separate private object

--- modules/core/src/main/scala/com/oopman/collectioneer/CoreProperties.scala
+++ modules/core/src/main/scala/com/oopman/collectioneer/CoreProperties.scala
@@ -2,47 +2,56 @@ package com.oopman.collectioneer
 
 import com.oopman.collectioneer.given
 import com.oopman.collectioneer.db.entity.PropertyType
-import com.oopman.collectioneer.db.entity.raw.Property
+import com.oopman.collectioneer.db.entity.projected.Property
 
 import java.util.UUID
+
+private object CorePropertyUUIDs:
+  val name = "f65d1d21-3542-48f9-a9d5-f96921e4ba15"
+  val description = "ea3c3226-2a04-45f0-a50e-1db9c4aa0072"
+  val defaultValue = "7b7b9577-da92-41d2-9ff8-a174e191b030"
+  val minValue = "4d3460bd-2cbf-45f9-9059-59edea9c1a17"
+  val maxValue = "9eeca250-53c8-4a35-a636-6954d0718b93"
+  val minValues = "df39b389-5055-46f7-82f3-53c736fb6950"
+  val maxValues = "158a2358-b5d1-4e73-a6bf-c93d0587590d"
 
 /**
  * This enumeration contains the UUIDs and PropertyTypes associated with the core properties in the application
  */
 enum CoreProperties(val property: Property):
   case name extends CoreProperties(Property(
-    "f65d1d21-3542-48f9-a9d5-f96921e4ba15",
-    "Name",
-    List(PropertyType.varchar)
+    pk = CorePropertyUUIDs.name,
+    propertyName = "Name",
+    propertyTypes = List(PropertyType.varchar)
   ))
   case description extends CoreProperties(Property(
-    "ea3c3226-2a04-45f0-a50e-1db9c4aa0072",
-    "Description",
-    List(PropertyType.varchar, PropertyType.clob)
+    pk = CorePropertyUUIDs.description,
+    propertyName = "Description",
+    propertyTypes = List(PropertyType.varchar, PropertyType.clob)
   ))
-  case default_value extends CoreProperties(Property(
-    "7b7b9577-da92-41d2-9ff8-a174e191b030",
-    "Default Value",
-    PropertyType.values.toList
+  case defaultValue extends CoreProperties(Property(
+    pk = CorePropertyUUIDs.defaultValue,
+    propertyName = "Default Value",
+    propertyTypes = PropertyType.values.toList
   ))
-  case min_value extends CoreProperties(Property(
-    "4d3460bd-2cbf-45f9-9059-59edea9c1a17",
-    "Minimum Value",
-    PropertyType.values.toList
+  case minValue extends CoreProperties(Property(
+    pk = CorePropertyUUIDs.minValue,
+    propertyName = "Minimum Value",
+    propertyTypes = PropertyType.values.toList
   ))
-  case max_value extends CoreProperties(Property(
-    "9eeca250-53c8-4a35-a636-6954d0718b93",
-    "Maximum Value",
-    PropertyType.values.toList
+  case maxValue extends CoreProperties(Property(
+    pk = CorePropertyUUIDs.maxValue,
+    propertyName = "Maximum Value",
+    propertyTypes = PropertyType.values.toList
   ))
-  case min_values extends CoreProperties(Property(
-    "df39b389-5055-46f7-82f3-53c736fb6950",
-    "Minimum Number of Values",
-    List(PropertyType.int)
+  case minValues extends CoreProperties(Property(
+    pk = CorePropertyUUIDs.minValues,
+    propertyName = "Minimum Number of Values",
+    propertyTypes = List(PropertyType.int)
   ))
-  case max_values extends CoreProperties(Property(
-    "158a2358-b5d1-4e73-a6bf-c93d0587590d",
-    "Maximum Number of Values",
-    List(PropertyType.int)
+  case maxValues extends CoreProperties(Property(
+    pk = CorePropertyUUIDs.maxValues,
+    propertyName = "Maximum Number of Values",
+    propertyTypes = List(PropertyType.int)
   ))
 
