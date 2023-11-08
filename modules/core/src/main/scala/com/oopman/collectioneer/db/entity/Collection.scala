85c8a074190717b7b9063039e2659dbd623dafac
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2023-11-07 21:37:55 -0500

    Added Colllection trait

--- modules/core/src/main/scala/com/oopman/collectioneer/db/entity/Collection.scala
+++ modules/core/src/main/scala/com/oopman/collectioneer/db/entity/Collection.scala
@@ -3,29 +3,9 @@ package com.oopman.collectioneer.db.entity
 import java.time.ZonedDateTime
 import java.util.UUID
 
-import scalikejdbc._
-
-case class Collection
-(
-  pk: UUID = UUID.randomUUID(),
-  virtual: Boolean = false,
-  deleted: Boolean = false,
-  created: ZonedDateTime = ZonedDateTime.now(),
-  modified: ZonedDateTime = ZonedDateTime.now(),
-)
-
-object Collection extends SQLSyntaxSupport[Collection]:
-  override val schemaName = Some("public")
-  override val tableName = "collection"
-
-  def apply(c: ResultName[Collection])(rs: WrappedResultSet) =
-    new Collection(
-      pk = UUID.fromString(rs.string(c.pk)),
-      virtual = rs.boolean(c.virtual),
-      deleted = rs.boolean(c.deleted),
-      created = rs.zonedDateTime(c.created),
-      modified = rs.zonedDateTime(c.modified)
-    )
-
-val c1 = Collection.syntax("c1")
-val c2 = Collection.syntax("c2")
+trait Collection:
+  val pk: UUID
+  val virtual: Boolean
+  val deleted: Boolean
+  val created: ZonedDateTime
+  val modified: ZonedDateTime
