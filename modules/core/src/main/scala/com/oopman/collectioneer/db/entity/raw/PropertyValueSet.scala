96e7fac5e1cf09bbaed58d401168cd14e47496df
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2023-11-17 15:15:35 -0500

    Updated raw PropertyValueSet to extend PropertyValueSet trait

--- modules/core/src/main/scala/com/oopman/collectioneer/db/entity/raw/PropertyValueSet.scala
+++ modules/core/src/main/scala/com/oopman/collectioneer/db/entity/raw/PropertyValueSet.scala
@@ -1,6 +1,6 @@
 package com.oopman.collectioneer.db.entity.raw
 
-import com.oopman.collectioneer.db.entity.PropertyValueSet
+import com.oopman.collectioneer.db.entity
 import scalikejdbc.*
 
 import java.time.ZonedDateTime
@@ -10,7 +10,7 @@ case class PropertyValueSet
 (
   pk: UUID = UUID.randomUUID(),
   created: ZonedDateTime = ZonedDateTime.now()
-)
+) extends entity.PropertyValueSet
 
 object PropertyValueSet extends SQLSyntaxSupport[PropertyValueSet]:
   override val schemaName = Some("public")
