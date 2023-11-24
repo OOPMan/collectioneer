b589bcfae2195d9de3e14531d60ffb6d0df1dbef
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2023-11-24 11:22:26 -0500

    Added generic raw PropertyValueQueries

---
+++ modules/core/src/main/scala/com/oopman/collectioneer/db/queries/h2/raw/PropertyValueQueries.scala
@@ -0,0 +1,52 @@
+package com.oopman.collectioneer.db.queries.h2.raw
+
+import scalikejdbc.*
+import com.oopman.collectioneer.db.entity
+import com.oopman.collectioneer.db.entity.raw.propertyvalue
+
+class PropertyValueQueries[T <: entity.PropertyValue[?]](val pv: propertyvalue.PropertyValueSQLSyntaxSupport[T]):
+  def insert =
+    sql"""
+          INSERT INTO ${pv.table} (pk, property_value_set_pk, property_pk, property_value, index)
+          VALUES ( ?, ?, ?, ?, ? )
+       """
+
+  def upsert =
+    sql"""
+          MERGE INTO ${pv.table} (pk, property_value_set_pk, property_pk, property_value, index, modified)
+          KEY (pk)
+          VALUES (?, ?, ?, ?, ?, ?)
+       """
+
+  def deleteByPk =
+    sql"""
+          DELETE FROM ${pv.table}
+          WHERE PK IN (?)
+       """
+
+  def deleteByPropertyValueSetPksAndPropertyPks =
+    sql"""
+          DELETE FROM ${pv.table}
+          WHERE PROPERTY_VALUE_SET_PK IN (?)
+          AND PROPERTY_PK IN (?)
+       """
+
+object PropertyValueVarcharQueries extends PropertyValueQueries(propertyvalue.PropertyValueVarchar)
+object PropertyValueVarbinaryQueries extends PropertyValueQueries(propertyvalue.PropertyValueVarbinary)
+object PropertyValueTinyintQueries extends PropertyValueQueries(propertyvalue.PropertyValueTinyint)
+object PropertyValueSmallintQueries extends PropertyValueQueries(propertyvalue.PropertyValueSmallint)
+object PropertyValueIntQueries extends PropertyValueQueries(propertyvalue.PropertyValueInt)
+object PropertyValueBigintQueries extends PropertyValueQueries(propertyvalue.PropertyValueBigint)
+object PropertyValueNumericQueries extends PropertyValueQueries(propertyvalue.PropertyValueNumeric)
+object PropertyValueFloatQueries extends PropertyValueQueries(propertyvalue.PropertyValueFloat)
+object PropertyValueDoubleQueries extends PropertyValueQueries(propertyvalue.PropertyValueDouble)
+object PropertyValueBooleanQueries extends PropertyValueQueries(propertyvalue.PropertyValueBoolean)
+object PropertyValueDateQueries extends PropertyValueQueries(propertyvalue.PropertyValueDate)
+object PropertyValueTimeQueries extends PropertyValueQueries(propertyvalue.PropertyValueTime)
+object PropertyValueTimestampQueries extends PropertyValueQueries(propertyvalue.PropertyValueTimestamp)
+object PropertyValueCLOBQueries extends PropertyValueQueries(propertyvalue.PropertyValueCLOB)
+object PropertyValueBLOBQueries extends PropertyValueQueries(propertyvalue.PropertyValueBLOB)
+object PropertyValueUUIDQueries extends PropertyValueQueries(propertyvalue.PropertyValueUUID)
+object PropertyValueJSONQueries extends PropertyValueQueries(propertyvalue.PropertyValueJSON)
+
+
