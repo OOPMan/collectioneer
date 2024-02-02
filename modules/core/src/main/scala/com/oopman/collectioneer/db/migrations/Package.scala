748025ad485dea6e2a8e1548eb1c45c3a0ce8a44
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2024-02-01 20:10:09 -0500

    Updated executeMigrations to additionally attempt to execute migrations for plugins

--- modules/core/src/main/scala/com/oopman/collectioneer/db/migrations/Package.scala
+++ modules/core/src/main/scala/com/oopman/collectioneer/db/migrations/Package.scala
@@ -1,5 +1,6 @@
 package com.oopman.collectioneer.db.migrations
 
+import com.oopman.collectioneer.Plugin
 import com.oopman.collectioneer.db.{Injection, traits}
 import distage.Functoid
 import izumi.fundamentals.platform.functional.Identity
@@ -9,13 +10,27 @@ import org.flywaydb.core.api.output.MigrateResult
 import javax.sql.DataSource
 
 
-def executeMigrations(dataSource: DataSource): MigrateResult =
-  def executeMigrations(db: traits.DatabaseBackend) =
-    Flyway
+def executeMigrations(dataSource: DataSource): Set[MigrateResult] =
+  def executeMigrations(db: traits.DatabaseBackend, plugins: Set[Plugin]) =
+    val migrationResult = Flyway
       .configure()
       .dataSource(dataSource)
       .locations(db.migrationLocations: _*)
       .load()
       .migrate()
+    val pluginMigrationResults = plugins
+      .map( plugin => (plugin.getShortName, plugin.getMigrationLocations(db)))
+      .filter(_._2.nonEmpty)
+      .map { (shortName: String, migrationLocations: Seq[String]) =>
+        Flyway
+          .configure()
+          .baselineOnMigrate(true)
+          .table(shortName)
+          .dataSource(dataSource)
+          .locations(migrationLocations: _*)
+          .load()
+          .migrate()
+      }
+    pluginMigrationResults + migrationResult
   Injection.produceRun(dataSource, false)(executeMigrations)
 
