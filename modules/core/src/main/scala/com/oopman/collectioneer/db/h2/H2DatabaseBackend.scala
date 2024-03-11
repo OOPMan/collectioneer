e9c6618923c6e5050318248f3489aa2a2d739414
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2024-03-11 17:06:42 -0400

    Updated H2DatabaseBackend to reflect changes to traits

--- modules/core/src/main/scala/com/oopman/collectioneer/db/h2/H2DatabaseBackend.scala
+++ modules/core/src/main/scala/com/oopman/collectioneer/db/h2/H2DatabaseBackend.scala
@@ -5,64 +5,68 @@ import com.oopman.collectioneer.db.{h2, traits}
 
 case class H2ProjectedQueryObjects
 (
-  override val PropertyValueQueries: traits.queries.projected.PropertyValueQueries
+  PropertyValueQueries: traits.queries.projected.PropertyValueQueries
 ) extends traits.queries.ProjectedQueryObjects
 
 case class H2RawQueryObjects
 (
-  override val CollectionQueries: traits.queries.raw.CollectionQueries,
-  override val PropertyCollectionQueries: traits.queries.raw.PropertyCollectionQueries,
-  override val PropertyQueries: traits.queries.raw.PropertyQueries,
-  override val PropertyValueSetQueries: traits.queries.raw.PropertyValueSetQueries,
-  override val PropertyValueVarcharQueries: traits.queries.raw.PropertyValueQueries @Id("Varchar"),
-  override val PropertyValueVarbinaryQueries: traits.queries.raw.PropertyValueQueries @Id("Varbinary"),
-  override val PropertyValueTinyintQueries: traits.queries.raw.PropertyValueQueries @Id("Tinyint"),
-  override val PropertyValueSmallintQueries: traits.queries.raw.PropertyValueQueries @Id("Smallint"),
-  override val PropertyValueIntQueries: traits.queries.raw.PropertyValueQueries @Id("Int"),
-  override val PropertyValueBigintQueries: traits.queries.raw.PropertyValueQueries @Id("Bigint"),
-  override val PropertyValueNumericQueries: traits.queries.raw.PropertyValueQueries @Id("Numeric"),
-  override val PropertyValueFloatQueries: traits.queries.raw.PropertyValueQueries @Id("Float"),
-  override val PropertyValueDoubleQueries: traits.queries.raw.PropertyValueQueries @Id("Double"),
-  override val PropertyValueBooleanQueries: traits.queries.raw.PropertyValueQueries @Id("Boolean"),
-  override val PropertyValueDateQueries: traits.queries.raw.PropertyValueQueries @Id("Date"),
-  override val PropertyValueTimeQueries: traits.queries.raw.PropertyValueQueries @Id("Time"),
-  override val PropertyValueTimestampQueries: traits.queries.raw.PropertyValueQueries @Id("Timestamp"),
-  override val PropertyValueCLOBQueries: traits.queries.raw.PropertyValueQueries @Id("CLOB"),
-  override val PropertyValueBLOBQueries: traits.queries.raw.PropertyValueQueries @Id("BLOB"),
-  override val PropertyValueUUIDQueries: traits.queries.raw.PropertyValueQueries @Id("UUID"),
-  override val PropertyValueJSONQueries: traits.queries.raw.PropertyValueQueries @Id("JSON"),
+  CollectionQueries: traits.queries.raw.CollectionQueries,
+  CollectionPropertyValueSetQueries: traits.queries.raw.CollectionPropertyValueSetQueries,
+  CollectionRelatedCollectionPropertyValueSetQueries: traits.queries.raw.CollectionRelatedCollectionPropertyValueSetQueries,
+  PropertyPropertyValueSetQueries: traits.queries.raw.PropertyPropertyValueSetQueries,
+  PropertyQueries: traits.queries.raw.PropertyQueries,
+  PropertyValueSetQueries: traits.queries.raw.PropertyValueSetQueries,
+  PropertyValueVarcharQueries: traits.queries.raw.PropertyValueQueries @Id("Varchar"),
+  PropertyValueVarbinaryQueries: traits.queries.raw.PropertyValueQueries @Id("Varbinary"),
+  PropertyValueTinyintQueries: traits.queries.raw.PropertyValueQueries @Id("Tinyint"),
+  PropertyValueSmallintQueries: traits.queries.raw.PropertyValueQueries @Id("Smallint"),
+  PropertyValueIntQueries: traits.queries.raw.PropertyValueQueries @Id("Int"),
+  PropertyValueBigintQueries: traits.queries.raw.PropertyValueQueries @Id("Bigint"),
+  PropertyValueNumericQueries: traits.queries.raw.PropertyValueQueries @Id("Numeric"),
+  PropertyValueFloatQueries: traits.queries.raw.PropertyValueQueries @Id("Float"),
+  PropertyValueDoubleQueries: traits.queries.raw.PropertyValueQueries @Id("Double"),
+  PropertyValueBooleanQueries: traits.queries.raw.PropertyValueQueries @Id("Boolean"),
+  PropertyValueDateQueries: traits.queries.raw.PropertyValueQueries @Id("Date"),
+  PropertyValueTimeQueries: traits.queries.raw.PropertyValueQueries @Id("Time"),
+  PropertyValueTimestampQueries: traits.queries.raw.PropertyValueQueries @Id("Timestamp"),
+  PropertyValueCLOBQueries: traits.queries.raw.PropertyValueQueries @Id("CLOB"),
+  PropertyValueBLOBQueries: traits.queries.raw.PropertyValueQueries @Id("BLOB"),
+  PropertyValueUUIDQueries: traits.queries.raw.PropertyValueQueries @Id("UUID"),
+  PropertyValueJSONQueries: traits.queries.raw.PropertyValueQueries @Id("JSON"),
 ) extends traits.queries.RawQueryObjects
 
 case class H2QueryObjects
 (
-  override val projected: traits.queries.ProjectedQueryObjects,
-  override val raw: traits.queries.RawQueryObjects
+  projected: traits.queries.ProjectedQueryObjects,
+  raw: traits.queries.RawQueryObjects
 ) extends traits.queries.QueryObjects
 
 case class H2ProjectedDAOObjects
 (
-  override val PropertyValueDAO: traits.dao.projected.PropertyValueDAO
+  CollectionDAO: traits.dao.projected.CollectionDAO,
+  PropertyDAO: traits.dao.projected.PropertyDAO,
+  PropertyValueDAO: traits.dao.projected.PropertyValueDAO
 ) extends traits.dao.ProjectedDAOObjects
 
 case class H2RawDAOObjects
 (
-  override val CollectionDAO: traits.dao.raw.CollectionDAO,
-  override val PropertyCollectionDAO: traits.dao.raw.PropertyCollectionDAO,
-  override val PropertyDAO: traits.dao.raw.PropertyDAO,
-  override val PropertyValueSetDAO: traits.dao.raw.PropertyValueSetDAO,
+  CollectionDAO: traits.dao.raw.CollectionDAO,
+  PropertyDAO: traits.dao.raw.PropertyDAO,
+  PropertyValueSetDAO: traits.dao.raw.PropertyValueSetDAO,
+  PropertyPropertyValueSetDAO: traits.dao.raw.PropertyPropertyValueSetDAO
 ) extends traits.dao.RawDAOObjects
 
 case class H2DAOObjects
 (
-  override val projected: traits.dao.ProjectedDAOObjects,
-  override val raw: traits.dao.RawDAOObjects
+  projected: traits.dao.ProjectedDAOObjects,
+  raw: traits.dao.RawDAOObjects
 
 ) extends traits.dao.DAOObjects
 
 case class H2DatabaseBackend
 (
-  override val queries: traits.queries.QueryObjects,
-  override val dao: traits.dao.DAOObjects
+  queries: traits.queries.QueryObjects,
+  dao: traits.dao.DAOObjects
 
 ) extends traits.DatabaseBackend:
   override val migrationLocations: Seq[String] = Seq(
@@ -75,18 +79,22 @@ val H2DatabaseBackendModule = new ModuleDef:
   make[traits.dao.DAOObjects].from[H2DAOObjects]
   make[traits.dao.ProjectedDAOObjects].from[H2ProjectedDAOObjects]
   make[traits.dao.RawDAOObjects].from[H2RawDAOObjects]
+  make[traits.dao.projected.CollectionDAO].from(h2.dao.projected.CollectionDAO)
+  make[traits.dao.projected.PropertyDAO].from(h2.dao.projected.PropertyDAO)
   make[traits.dao.projected.PropertyValueDAO].from(h2.dao.projected.PropertyValueDAO)
   make[traits.dao.raw.CollectionDAO].from(h2.dao.raw.CollectionDAO)
   make[traits.dao.raw.PropertyDAO].from(h2.dao.raw.PropertyDAO)
-  make[traits.dao.raw.PropertyCollectionDAO].from(h2.dao.raw.PropertyCollectionDAO)
   make[traits.dao.raw.PropertyValueSetDAO].from(h2.dao.raw.PropertyValueSetDAO)
+  make[traits.dao.raw.PropertyPropertyValueSetDAO].from(h2.dao.raw.PropertyPropertyValueSetDAO)
   make[traits.queries.QueryObjects].from[H2QueryObjects]
   make[traits.queries.ProjectedQueryObjects].from[H2ProjectedQueryObjects]
   make[traits.queries.RawQueryObjects].from[H2RawQueryObjects]
   make[traits.queries.projected.PropertyValueQueries].from(h2.queries.projected.PropertyValueQueries)
   make[traits.queries.raw.CollectionQueries].from(h2.queries.raw.CollectionQueries)
-  make[traits.queries.raw.PropertyCollectionQueries].from(h2.queries.raw.PropertyCollectionQueries)
+  make[traits.queries.raw.CollectionPropertyValueSetQueries].from(h2.queries.raw.CollectionPropertyValueSetQueries)
+  make[traits.queries.raw.CollectionRelatedCollectionPropertyValueSetQueries].from(h2.queries.raw.CollectionRelatedCollectionPropertyValueSetQueries)
   make[traits.queries.raw.PropertyQueries].from(h2.queries.raw.PropertyQueries)
+  make[traits.queries.raw.PropertyPropertyValueSetQueries].from(h2.queries.raw.PropertyPropertyValueSetQueries)
   make[traits.queries.raw.PropertyValueSetQueries].from(h2.queries.raw.PropertyValueSetQueries)
   make[traits.queries.raw.PropertyValueQueries].named("Varchar").from(h2.queries.raw.PropertyValueVarcharQueries)
   make[traits.queries.raw.PropertyValueQueries].named("Varbinary").from(h2.queries.raw.PropertyValueVarbinaryQueries)
