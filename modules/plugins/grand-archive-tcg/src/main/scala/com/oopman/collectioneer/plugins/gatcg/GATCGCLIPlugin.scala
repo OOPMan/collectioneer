94473a04624db21df9ad5076834a5142eb8c20de
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2024-03-18 22:42:22 -0400

    Began implementing action to download save GATCG card database to a JSON file

--- modules/plugins/grand-archive-tcg/src/main/scala/com/oopman/collectioneer/plugins/gatcg/GATCGCLIPlugin.scala
+++ modules/plugins/grand-archive-tcg/src/main/scala/com/oopman/collectioneer/plugins/gatcg/GATCGCLIPlugin.scala
@@ -4,37 +4,75 @@ import com.oopman.collectioneer.Plugin
 import com.oopman.collectioneer.cli.{Config, Subconfig, Subject, Verb}
 import com.oopman.collectioneer.db.h2.H2DatabaseBackend
 import com.oopman.collectioneer.db.traits.DatabaseBackend
-import com.oopman.collectioneer.db.traits.entity.raw.{Property, PropertyType}
 import com.oopman.collectioneer.plugins.CLIPlugin
+import io.circe.*
+import io.circe.optics.JsonPath.*
+import io.circe.syntax.*
 import izumi.distage.plugins.PluginDef
 import scopt.{OParser, OParserBuilder}
-import io.circe.*
-import io.circe.generic.auto.*
-import io.circe.syntax.*
+import sttp.client3.*
+import sttp.client3.circe.*
+
+import scala.language.postfixOps
 
 class GATCGCLIPlugin extends CLIPlugin:
-  override def getName: String = "Grand Archive TCG"
+  def getName: String = "Grand Archive TCG"
 
-  override def getShortName: String = "GATCG"
+  def getShortName: String = "GATCG"
 
-  override def getVersion: String = "master"
+  def getVersion: String = "master"
 
-  override def getMigrationLocations(databaseBackend: DatabaseBackend): Seq[String] = databaseBackend match
+  def getMigrationLocations(databaseBackend: DatabaseBackend): Seq[String] = databaseBackend match
     case _: H2DatabaseBackend => Seq("classpath:com/oopman/collectioneer/plugins/gatcg/migrations/h2")
 
-  override def getDefaultSubconfig: Subconfig = GATCGPluginConfig()
+  def getDefaultSubconfig: Subconfig = GATCGPluginConfig()
 
-  override def getActions(builder: OParserBuilder[Config]): List[(Verb, Subject, Config => Json, List[OParser[_, Config]])] =
+  def getActions(builder: OParserBuilder[Config]): List[(Verb, Subject, Config => Json, List[OParser[_, Config]])] =
     List(
       (Verb.imprt, Subject.database, importDatabase, List()),
     )
 
   def importDatabase(config: Config) =
-    // TODO: Create GA-specific properties if they don't exist using an UPSERT
 //    val propertiesDAO = new PropertyDAO(config.datasourceUri)
 //    val properties = GATCGProperties.values.toList.map(_.property)
 //    propertiesDAO.createOrUpdateProperties(properties)
     // TODO: Replace with a real response
+    "Something".asJson
+
+  def downloadDatabase(config: Config) =
+    val client = SimpleHttpClient()
+    def getData(page: Int = 1, pageSize: Int = 50, delayBetweenRequests: Long = 500): Vector[Json] =
+      // TODO: Print progress
+      wait(delayBetweenRequests)
+      val request = basicRequest
+        .get(uri"https://api.gatcg.com/cards/search?page=$page&page_size=$pageSize")
+        .response(asJson[io.circe.Json])
+      val response = client.send(request)
+      response.body match
+        case Left(body) =>
+          // TODO: Log errors
+          Vector()
+        case Right(body) =>
+          val hasMore = root.has_more.boolean.getOption(body).getOrElse(false)
+          val data = root.data.arr.getOption(body).getOrElse(Vector())
+          if hasMore then data :++ getData(page + 1, pageSize) else data
+    val data = getData()
+    val s = data.asJson.spaces2SortKeys
+    // TODO: Save data to file
+//      response
+//        .body
+//        .map(json => {
+//          val hasMore = root.has_more.boolean.getOption(json).getOrElse(false)
+//          val data = root.data.arr.getOption(json).getOrElse(Vector())
+//          data
+//          if hasMore:
+//            val z = "Something".asJson
+//            // TODO: Recurive call
+//          data
+//        })
+
+
+
     "Something".asJson
 
 object GATCGCLIPlugin extends PluginDef:
