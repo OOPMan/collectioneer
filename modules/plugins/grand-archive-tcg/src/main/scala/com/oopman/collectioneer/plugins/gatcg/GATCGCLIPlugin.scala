c9f35c75bf845e8160719ebc42301e686d290201
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2024-03-20 14:31:47 -0400

    Cleaned up implementation of GATCG dataset download

--- modules/plugins/grand-archive-tcg/src/main/scala/com/oopman/collectioneer/plugins/gatcg/GATCGCLIPlugin.scala
+++ modules/plugins/grand-archive-tcg/src/main/scala/com/oopman/collectioneer/plugins/gatcg/GATCGCLIPlugin.scala
@@ -7,6 +7,7 @@ import com.oopman.collectioneer.db.traits.DatabaseBackend
 import com.oopman.collectioneer.plugins.CLIPlugin
 import com.typesafe.scalalogging.LazyLogging
 import io.circe.*
+import io.circe.generic.auto.*
 import io.circe.optics.JsonPath.*
 import io.circe.syntax.*
 import izumi.distage.plugins.PluginDef
@@ -14,9 +15,21 @@ import scopt.{OParser, OParserBuilder}
 import sttp.client3.*
 import sttp.client3.circe.*
 
+import java.io.File
 import scala.language.postfixOps
+import scala.util.*
+
+case class DownloadDatasetResult
+(
+  datasetPath: Option[String] = None,
+  datasetSize: Int = 0,
+  downloadSucceeded: Boolean = true,
+  errorMessage: Option[String] = None
+)
 
 class GATCGCLIPlugin extends CLIPlugin with LazyLogging:
+  val defaultDatasetPath = os.pwd / "gatcg.json"
+
   def getName: String = "Grand Archive TCG"
 
   def getShortName: String = "GATCG"
@@ -26,13 +39,19 @@ class GATCGCLIPlugin extends CLIPlugin with LazyLogging:
   def getMigrationLocations(databaseBackend: DatabaseBackend): Seq[String] = databaseBackend match
     case _: H2DatabaseBackend => Seq("classpath:com/oopman/collectioneer/plugins/gatcg/migrations/h2")
 
-  def getDefaultSubconfig: Subconfig = GATCGPluginConfig()
+  def getDefaultSubconfig: GATCGPluginConfig = GATCGPluginConfig()
+
+  def getSubconfigFromConfig(config: Config): GATCGPluginConfig =
+    config.subconfigs.getOrElse(getShortName, getDefaultSubconfig).asInstanceOf[GATCGPluginConfig]
 
   def getActions(builder: OParserBuilder[Config]): List[(Verb, Subject, Config => Json, List[OParser[_, Config]])] =
+    val datasetPathOpt = builder.opt[File]("gatcg-json-dataset-path")
+      .optional()
+      .action((f, config) => config.copy(subconfigs = config.subconfigs.updated(getShortName, getSubconfigFromConfig(config).copy(grandArchiveTCGJSON = Some(f)))))
+      .text("Something")
     List(
-      (Verb.imprt, Subject.database, importDatabase, List()),
-      // TODO: Need a better verb?
-      (Verb("download", ""), Subject.database, downloadDatabase, List())
+      (Verb.imprt, Subject.dataset, importDataset, List(datasetPathOpt)),
+      (Verb.download, Subject.dataset, downloadDataset, List(datasetPathOpt))
     )
 
   def getData
@@ -42,7 +61,7 @@ class GATCGCLIPlugin extends CLIPlugin with LazyLogging:
     page: Int = 1,
     pageSize: Int = 50,
     delayBetweenRequests: Long = 500
-  ): Vector[Json] =
+  ): Try[Vector[Json]] =
     logger.info(s"Retrieving page $page with page size $pageSize from $baseUri")
     val request = basicRequest
       .get(uri"$baseUri/cards/search?page=$page&page_size=$pageSize")
@@ -50,35 +69,39 @@ class GATCGCLIPlugin extends CLIPlugin with LazyLogging:
     val response = client.send(request)
     response.body match
       case Left(body) =>
-        logger.error(s"Error downloading $page with page size $pageSize from $baseUri: ${response.code}")
-        Vector()
+        val message = s"Error downloading $page with page size $pageSize from $baseUri: ${response.code}"
+        logger.error(message)
+        Failure(RuntimeException(message))
       case Right(body) =>
         val hasMore = root.has_more.boolean.getOption(body).getOrElse(false)
         val data = root.data.arr.getOption(body).getOrElse(Vector())
-        if hasMore then
-          this.synchronized {
-            wait(delayBetweenRequests)
-          }
-          data :++ getData(client, baseUri, page + 1, pageSize)
-        else data
+        if !hasMore then
+          Success(data)
+        else
+          this.synchronized { wait(delayBetweenRequests) }
+          getData(client, baseUri, page + 1, pageSize).map(newData => data :++ newData)
 
-  def importDatabase(config: Config) =
+  def importDataset(config: Config) =
 //    val propertiesDAO = new PropertyDAO(config.datasourceUri)
 //    val properties = GATCGProperties.values.toList.map(_.property)
 //    propertiesDAO.createOrUpdateProperties(properties)
     // TODO: Replace with a real response
     "Something".asJson
 
-  def downloadDatabase(config: Config) =
+  def downloadDataset(config: Config) =
     logger.info("Downloading GATCG dataaset")
-    val data = getData()
-    logger.info(s"Download ${data.length} GATCG cards")
-    val s = data.asJson.spaces2SortKeys
-    // TODO: Save data to file
-    val workingDirectory = os.pwd
-    os.write(workingDirectory / "test.json", s)
-    // TODO: Return useful data
-    "Something".asJson
+    val result = getData() match
+      case Failure(e) => DownloadDatasetResult(downloadSucceeded = false, errorMessage = Some(e.getMessage))
+      case Success(data) =>
+        logger.info(s"Downloaded ${data.length} GATCG cards")
+        val path = getSubconfigFromConfig(config).grandArchiveTCGJSON.map(f => os.FilePath(f)).getOrElse(defaultDatasetPath)
+        val dataAsString = data.asJson.spaces2
+        path match
+          case p: os.Path => os.write(p, dataAsString)
+          case p: os.RelPath => os.write(os.pwd / p, dataAsString)
+          case p: os.SubPath => os.write(os.pwd / p, dataAsString)
+        DownloadDatasetResult(datasetPath = Some(path.toString), datasetSize = data.length)
+    result.asJson
 
 object GATCGCLIPlugin extends PluginDef:
   many[CLIPlugin].add(GATCGCLIPlugin())
