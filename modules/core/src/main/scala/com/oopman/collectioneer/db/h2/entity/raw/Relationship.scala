efe65b07bc953481d9d8bd48bdeb1538a3256476
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2024-03-29 09:57:14 -0400

    Added db.h2.entity.raw.Relationship helper object

---
+++ modules/core/src/main/scala/com/oopman/collectioneer/db/h2/entity/raw/Relationship.scala
@@ -0,0 +1,12 @@
+package com.oopman.collectioneer.db.h2.entity.raw
+
+import com.oopman.collectioneer.db.traits.entity.raw.Relationship
+
+object Relationship:
+  def relationshipSeqToBatchUpsertSeq(relationships: Seq[Relationship]): Seq[Seq[Any]] =
+    relationships.map(relationship => Seq(
+      relationship.pk.toString,
+      relationship.collectionPK.toString,
+      relationship.relatedCollectionPK.toString,
+      relationship.relationshipType.toString
+    ))
