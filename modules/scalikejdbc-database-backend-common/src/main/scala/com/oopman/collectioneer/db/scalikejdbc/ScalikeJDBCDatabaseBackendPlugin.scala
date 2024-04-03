464a12eacb7cc393d1a418318e61adb4c8bc7395
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2024-04-03 14:27:46 -0400

    Implemented startUp and shutDown on ScalikeJDBCDatabaseBackendPlugin to configure/close ScalikeJDBC ConnectionPool for datasource

--- modules/scalikejdbc-database-backend-common/src/main/scala/com/oopman/collectioneer/db/scalikejdbc/ScalikeJDBCDatabaseBackendPlugin.scala
+++ modules/scalikejdbc-database-backend-common/src/main/scala/com/oopman/collectioneer/db/scalikejdbc/ScalikeJDBCDatabaseBackendPlugin.scala
@@ -1,11 +1,18 @@
 package com.oopman.collectioneer.db.scalikejdbc
 
 import com.oopman.collectioneer.db.DatabaseBackendPlugin
+import scalikejdbc.*
 
 import java.sql.Connection
 import javax.sql.DataSource
 
 trait ScalikeJDBCDatabaseBackendPlugin extends DatabaseBackendPlugin:
+  override def startUp(): Unit =
+    if !ConnectionPool.isInitialized(config.datasourceUri)
+    then ConnectionPool.add(config.datasourceUri, DataSourceConnectionPool(getDatasource))
+    // TODO: Run Flyway migrations?
+  override def shutDown(): Unit =
+    ConnectionPool.close(config.datasourceUri)
   def getDatasource: DataSource
   def getConnection: Connection
   def getMigrationLocations: Seq[String]
