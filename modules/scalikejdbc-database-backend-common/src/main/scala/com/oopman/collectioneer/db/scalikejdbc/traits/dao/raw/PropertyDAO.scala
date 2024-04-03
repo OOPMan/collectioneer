ccb3829f855eb694926f4643134b8b6d29205673
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2024-04-03 10:55:49 -0400

    Temporarily commented out getAllMatchingPKs signatue on PropertyDAO trait in ScalikeJDBC Database Backend Common

--- modules/scalikejdbc-database-backend-common/src/main/scala/com/oopman/collectioneer/db/scalikejdbc/traits/dao/raw/PropertyDAO.scala
+++ modules/scalikejdbc-database-backend-common/src/main/scala/com/oopman/collectioneer/db/scalikejdbc/traits/dao/raw/PropertyDAO.scala
@@ -3,10 +3,10 @@ package com.oopman.collectioneer.db.scalikejdbc.traits.dao.raw
 import com.oopman.collectioneer.db.traits.entity.raw.Property
 import scalikejdbc.DBSession
 
-import java.util.UUID
-
 trait PropertyDAO:
   def createProperties(properties: Seq[Property])(implicit session: DBSession): Array[Int]
   def createOrUpdateProperties(properties: Seq[Property])(implicit session: DBSession): Array[Int]
   def getAll(implicit session: DBSession): List[Property]
-  def getAllMatchingPKs(uuids: Seq[UUID])(implicit session: DBSession): List[Property]
+
+// Temporarily commented out as it was breaking compilcation of postgres backend for some unknown reason
+//  def getAllMatchingPKs(uuids: Seq[UUID])(implicit session: DBSession): List[Property]
