de3af6e17f4a28191abcdc2c4dfcbbe1b1f50f81
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2024-04-03 14:41:23 -0400

    Added some addition given type conversions in core

--- modules/core/src/main/scala/com/oopman/collectioneer/Givens.scala
+++ modules/core/src/main/scala/com/oopman/collectioneer/Givens.scala
@@ -7,3 +7,9 @@ given stringToUUID: Conversion[String, UUID] with
   def apply(s: String): UUID = UUID.fromString(s)
 
 
+given coreCollectionsToCollection: Conversion[CoreCollections, db.entity.projected.Collection] with
+  def apply(c: CoreCollections): db.entity.projected.Collection = c.collection
+
+
+given corePropertiesToProperty: Conversion[CoreProperties, db.entity.projected.Property] with
+  def apply(p: CoreProperties): db.entity.projected.Property = p.property
