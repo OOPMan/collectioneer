d2ce950253381206d0f48676f13b172e25ce3b42
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2024-05-08 21:19:11 -0400

    De-duplicated common code in postgresDatabaseBackend CollectionDAOImpl

--- modules/plugins/postgres-database-backend/src/main/scala/com/oopman/collectioneer/plugins/postgresbackend/dao/projected/CollectionDAOImpl.scala
+++ modules/plugins/postgres-database-backend/src/main/scala/com/oopman/collectioneer/plugins/postgresbackend/dao/projected/CollectionDAOImpl.scala
@@ -1,15 +1,22 @@
 package com.oopman.collectioneer.plugins.postgresbackend.dao.projected
 
 import com.oopman.collectioneer.db.entity
+import com.oopman.collectioneer.db.entity.projected.PropertyValue
 import com.oopman.collectioneer.db.scalikejdbc.traits
 import com.oopman.collectioneer.db.traits.entity.projected.Collection
+import com.oopman.collectioneer.db.traits.entity.raw.{Property, PropertyCollection}
 import com.oopman.collectioneer.plugins.postgresbackend
 import scalikejdbc.*
 
 import java.util.UUID
 
 object CollectionDAOImpl extends traits.dao.projected.ScalikeCollectionDAO:
-  def createCollections(collections: Seq[Collection])(implicit session: DBSession = AutoSession): Array[Int] =
+  private def performCreateOrUpdateCollectionsOperation
+  (collections: Seq[Collection], performCreateOrUpdateCollections: Seq[Collection] => Array[Int],
+   performCreateOrUpdateProperties: Seq[Property] => Array[Int],
+   performCreateOrUpdatePropertyCollections: Seq[PropertyCollection] => Array[Int],
+   performUpdatePropertyValues: Seq[PropertyValue] => Seq[Boolean])
+  (implicit session: DBSession = AutoSession): Array[Int] =
     val propertyValues = collections.flatMap(collection => collection.propertyValues.map {
       case propertyValue: entity.projected.PropertyValue => propertyValue.copy(collection = propertyValue.collection.copy(pk = collection.pk))
     })
@@ -19,30 +26,30 @@ object CollectionDAOImpl extends traits.dao.projected.ScalikeCollectionDAO:
     val propertyCollections = collectionPKPropetiesListSeq.flatMap((collectionPK, properties) => properties.map(property => entity.raw.PropertyCollection(propertyPK = property.pk, collectionPK = collectionPK)))
     // TODO: Generate index values for PropertyCollections
     val distinctPropertyCollections = propertyCollections.distinctBy(propertyCollection => (propertyCollection.propertyPK, propertyCollection.collectionPK))
-    postgresbackend.dao.raw.CollectionDAOImpl.createCollections(distinctCollections)
-    postgresbackend.dao.raw.PropertyDAOImpl.createProperties(distinctProperties)
-    postgresbackend.dao.raw.PropertyCollectionDAOImpl.createPropertyCollections(distinctPropertyCollections)
-    postgresbackend.dao.projected.PropertyValueDAOImpl.updatePropertyValues(propertyValues)
+    performCreateOrUpdateCollections(distinctCollections)
+    performCreateOrUpdateProperties(distinctProperties)
+    performCreateOrUpdatePropertyCollections(distinctPropertyCollections)
+    performUpdatePropertyValues(propertyValues)
     // TODO: Return a more useful result?
     Array.empty
 
+  def createCollections(collections: Seq[Collection])(implicit session: DBSession = AutoSession): Array[Int] =
+    performCreateOrUpdateCollectionsOperation(
+      collections,
+      postgresbackend.dao.raw.CollectionDAOImpl.createCollections,
+      postgresbackend.dao.raw.PropertyDAOImpl.createProperties,
+      postgresbackend.dao.raw.PropertyCollectionDAOImpl.createPropertyCollections,
+      postgresbackend.dao.projected.PropertyValueDAOImpl.updatePropertyValues
+    )
+
   def createOrUpdateCollections(collections: Seq[Collection])(implicit session: DBSession = AutoSession): Array[Int] =
-    // TODO: De-duplicate wotj above function
-    val propertyValues = collections.flatMap(collection => collection.propertyValues.map {
-      case propertyValue: entity.projected.PropertyValue => propertyValue.copy(collection = propertyValue.collection.copy(pk = collection.pk))
-    })
-    val distinctCollections = collections.distinctBy(_.pk)
-    val collectionPKPropetiesListSeq = collections.map(collection => (collection.pk, collection.properties ++ collection.propertyValues.map(_.property)))
-    val distinctProperties = collectionPKPropetiesListSeq.flatMap(_._2).distinctBy(_.pk)
-    val propertyCollections = collectionPKPropetiesListSeq.flatMap((collectionPK, properties) => properties.map(property => entity.raw.PropertyCollection(propertyPK = property.pk, collectionPK = collectionPK)))
-    // TODO: Generate index values for PropertyCollections
-    val distinctPropertyCollections = propertyCollections.distinctBy(propertyCollection => (propertyCollection.propertyPK, propertyCollection.collectionPK))
-    postgresbackend.dao.raw.CollectionDAOImpl.createOrUpdateCollections(distinctCollections)
-    postgresbackend.dao.raw.PropertyDAOImpl.createOrUpdateProperties(distinctProperties)
-    postgresbackend.dao.raw.PropertyCollectionDAOImpl.createOrUpdatePropertyCollections(distinctPropertyCollections)
-    postgresbackend.dao.projected.PropertyValueDAOImpl.updatePropertyValues(propertyValues)
-    // TODO: Return a more useful result?
-    Array.empty
+    performCreateOrUpdateCollectionsOperation(
+      collections,
+      postgresbackend.dao.raw.CollectionDAOImpl.createOrUpdateCollections,
+      postgresbackend.dao.raw.PropertyDAOImpl.createOrUpdateProperties,
+      postgresbackend.dao.raw.PropertyCollectionDAOImpl.createOrUpdatePropertyCollections,
+      postgresbackend.dao.projected.PropertyValueDAOImpl.updatePropertyValues
+    )
 
   def getAll(implicit session: DBSession = AutoSession): List[Collection] = ???
 
