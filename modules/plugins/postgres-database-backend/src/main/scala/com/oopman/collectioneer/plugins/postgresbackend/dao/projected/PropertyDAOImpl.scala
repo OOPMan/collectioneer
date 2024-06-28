4efd692e57cac3e3047bcfcd203e2299fe5a91b2
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2024-06-27 22:31:12 -0400

    Updated PropertyDAOImpl to handle writing of data for nested Properties and PropertyValues correctly

--- modules/plugins/postgres-database-backend/src/main/scala/com/oopman/collectioneer/plugins/postgresbackend/dao/projected/PropertyDAOImpl.scala
+++ modules/plugins/postgres-database-backend/src/main/scala/com/oopman/collectioneer/plugins/postgresbackend/dao/projected/PropertyDAOImpl.scala
@@ -1,15 +1,45 @@
 package com.oopman.collectioneer.plugins.postgresbackend.dao.projected
 
-import com.oopman.collectioneer.db.entity
-import com.oopman.collectioneer.db.entity.projected.Collection
-import com.oopman.collectioneer.db.scalikejdbc.traits
-import com.oopman.collectioneer.db.traits.entity.projected.Property
-import com.oopman.collectioneer.db.traits.entity.raw.PropertyCollectionRelationshipType
+import com.oopman.collectioneer.db.scalikejdbc.traits.dao.projected.ScalikePropertyDAO
+import com.oopman.collectioneer.db.traits.entity.projected.Property as ProjectedProperty
+import com.oopman.collectioneer.db.traits.entity.raw.{Collection, Property, PropertyCollection, PropertyCollectionRelationshipType}
+import com.oopman.collectioneer.db.{entity, traits}
 import com.oopman.collectioneer.plugins.postgresbackend
 import scalikejdbc.*
 
-object PropertyDAOImpl extends traits.dao.projected.ScalikePropertyDAO:
-  def createProperties(properties: Seq[Property])(implicit session: DBSession = AutoSession) = ???
+object PropertyDAOImpl extends ScalikePropertyDAO:
+  private def performCreateOrUpdatePropertiesOperation
+  (properties: Seq[ProjectedProperty],
+   performCreateOrUpdateProperties: Seq[Property] => Array[Int],
+   performCreateOrUpdateCollections: Seq[Collection] => Array[Int],
+   performCreateOrUpdatePropertyCollections: Seq[PropertyCollection] => Array[Int])
+  (implicit session: DBSession = AutoSession): Array[Int] =
+    val distinctProperties = ProjectedProperty.collectProperties(properties)
+    val collections = distinctProperties.map(p => entity.raw.Collection(pk = p.pk))
+    val propertyValues = properties.flatMap(property => property.propertyValues.map {
+      case propertyValue: entity.projected.PropertyValue => propertyValue.copy(collection = propertyValue.collection.copy(pk = property.pk))
+    })
+    val propertyCollections = propertyValues
+      .groupBy(_.collection.pk)
+      .flatMap((_, propertyValues) => propertyValues.zipWithIndex.map((propertyValue, index) => entity.raw.PropertyCollection(
+        propertyPK = propertyValue.property.pk,
+        collectionPK = propertyValue.property.pk,
+        index = index,
+        propertyCollectionRelationshipType = PropertyCollectionRelationshipType.CollectionOfPropertiesOfProperty
+      )))
+      .toSeq
+    performCreateOrUpdateProperties(distinctProperties)
+    performCreateOrUpdateCollections(collections)
+    postgresbackend.dao.projected.PropertyValueDAOImpl.updatePropertyValues(propertyValues)
+    performCreateOrUpdatePropertyCollections(propertyCollections)
+
+  def createProperties(properties: Seq[ProjectedProperty])(implicit session: DBSession = AutoSession) =
+    performCreateOrUpdatePropertiesOperation(
+      properties,
+      postgresbackend.dao.raw.PropertyDAOImpl.createProperties,
+      postgresbackend.dao.raw.CollectionDAOImpl.createCollections,
+      postgresbackend.dao.raw.PropertyCollectionDAOImpl.createPropertyCollections,
+    )
 
   /**
    * Creates/Updates Projected Properties along with all nested Properties and PropertyValues
@@ -24,24 +54,12 @@ object PropertyDAOImpl extends traits.dao.projected.ScalikePropertyDAO:
    * @param session
    * @return
    */
-  def createOrUpdateProperties(properties: Seq[Property])(implicit session: DBSession = AutoSession) =
-    val distinctProperties = Property.collectProperties(properties)
-    val propertyValues = properties.flatMap(property => property.propertyValues.map {
-      case propertyValue: entity.projected.PropertyValue => propertyValue.copy(collection = propertyValue.collection.copy(pk = property.pk))
-    })
-    val collections = propertyValues.map(_.collection).distinctBy(_.pk)
-    val propertyCollections = propertyValues
-      .groupBy(_.collection.pk)
-      .flatMap((_, propertyValues) => propertyValues.zipWithIndex.map((propertyValue, index) => entity.raw.PropertyCollection(
-        propertyPK = propertyValue.property.pk,
-        collectionPK = propertyValue.collection.pk,
-        index = index,
-        propertyCollectionRelationshipType = PropertyCollectionRelationshipType.CollectionOfPropertiesOfProperty
-      )))
-      .toSeq
-    postgresbackend.dao.raw.PropertyDAOImpl.createOrUpdateProperties(distinctProperties)
-    postgresbackend.dao.raw.CollectionDAOImpl.createOrUpdateCollections(collections)
-    postgresbackend.dao.projected.PropertyValueDAOImpl.updatePropertyValues(propertyValues)
-    postgresbackend.dao.raw.PropertyCollectionDAOImpl.createOrUpdatePropertyCollections(propertyCollections)
+  def createOrUpdateProperties(properties: Seq[ProjectedProperty])(implicit session: DBSession = AutoSession) =
+    performCreateOrUpdatePropertiesOperation(
+      properties,
+      postgresbackend.dao.raw.PropertyDAOImpl.createOrUpdateProperties,
+      postgresbackend.dao.raw.CollectionDAOImpl.createOrUpdateCollections,
+      postgresbackend.dao.raw.PropertyCollectionDAOImpl.createOrUpdatePropertyCollections,
+    )
 
-  def getAll(implicit session: DBSession = AutoSession): List[Property] = ???
+  def getAll(implicit session: DBSession = AutoSession): List[ProjectedProperty] = ???
