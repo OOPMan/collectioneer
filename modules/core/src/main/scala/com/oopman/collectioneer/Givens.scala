23f57250f75f4eec92613b26e8a6c05ad7b28458
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2024-09-06 23:31:13 -0400

    Reduced boilerplate in com.oopman.collectioneer.Givens

--- modules/core/src/main/scala/com/oopman/collectioneer/Givens.scala
+++ modules/core/src/main/scala/com/oopman/collectioneer/Givens.scala
@@ -3,13 +3,10 @@ package com.oopman.collectioneer
 import java.util.UUID
 
 
-given stringToUUID: Conversion[String, UUID] with
-  def apply(s: String): UUID = UUID.fromString(s)
+given stringToUUID: Conversion[String, UUID] = (s: String) => UUID.fromString(s)
 
+given coreCollectionsToCollection: Conversion[CoreCollections, db.entity.projected.Collection] =
+  (c: CoreCollections) => c.collection
 
-given coreCollectionsToCollection: Conversion[CoreCollections, db.entity.projected.Collection] with
-  def apply(c: CoreCollections): db.entity.projected.Collection = c.collection
-
-
-given corePropertiesToProperty: Conversion[CoreProperties, db.entity.projected.Property] with
-  def apply(p: CoreProperties): db.entity.projected.Property = p.property
+given corePropertiesToProperty: Conversion[CoreProperties, db.entity.projected.Property] =
+  (p: CoreProperties) =>  p.property
