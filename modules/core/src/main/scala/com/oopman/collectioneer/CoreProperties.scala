a544344c078cee6a2d7e6a9b2ce2b6e298123f99
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2024-09-06 23:32:29 -0400

    Added visible to CoreProperties

--- modules/core/src/main/scala/com/oopman/collectioneer/CoreProperties.scala
+++ modules/core/src/main/scala/com/oopman/collectioneer/CoreProperties.scala
@@ -4,7 +4,7 @@ import com.oopman.collectioneer.db.entity.projected.Property
 import com.oopman.collectioneer.db.traits.entity.raw.PropertyType
 import com.oopman.collectioneer.given
 
-private object CorePropertyUUIDs:
+object CorePropertyUUIDs:
   val name = "f65d1d21-3542-48f9-a9d5-f96921e4ba15"
   val description = "ea3c3226-2a04-45f0-a50e-1db9c4aa0072"
   val defaultValue = "7b7b9577-da92-41d2-9ff8-a174e191b030"
@@ -12,6 +12,7 @@ private object CorePropertyUUIDs:
   val maxValue = "9eeca250-53c8-4a35-a636-6954d0718b93"
   val minValues = "df39b389-5055-46f7-82f3-53c736fb6950"
   val maxValues = "158a2358-b5d1-4e73-a6bf-c93d0587590d"
+  val visible = "baa75583-d622-47ab-962e-444693bc4af5"
 
 /**
  * This enumeration contains the UUIDs and PropertyTypes associated with the core properties in the application
@@ -52,4 +53,8 @@ enum CoreProperties(val property: Property):
     propertyName = "Maximum Number of Values",
     propertyTypes = List(PropertyType.int),
   ))
-
+  case visible extends CoreProperties(Property(
+    pk = CorePropertyUUIDs.visible,
+    propertyName = "Visible",
+    propertyTypes = List(PropertyType.boolean)
+  ))
