057fab7ae8a52f2c4ed14df6526fd4ae3dac8e3d
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2024-09-08 17:09:09 -0400

    Refactored getCollections from com.oopman.collectioneer.cli.list to com.oopman.collectioneer.cli.get

--- modules/cli/src/main/scala/com/oopman/collectioneer/cli/actions/list/Collections.scala
+++ modules/cli/src/main/scala/com/oopman/collectioneer/cli/actions/list/Collections.scala
@@ -9,8 +9,7 @@ import io.circe.*
 import io.circe.generic.auto.*
 import io.circe.syntax.*
 
-import java.time.ZonedDateTime
-import java.util.{HexFormat, UUID}
+import java.util.UUID
 
 implicit val encodeCollection: Encoder[traits.entity.raw.Collection] = (c: traits.entity.raw.Collection) =>
   Json.obj(
@@ -67,73 +66,16 @@ def listCollectionsAction(config: Config) =
     case Some(_) => Injection.produceRun(config)(listCollectionsByPropertyValueQueries(config))
     case None => Injection.produceRun(config)(listCollections(config))
   if config.verbose then
+    // TODO: Verbose list includes all property values for each collection
     ListCollectionsVerboseResult(
       dataSourceUri = config.datasourceUri,
       count = collections.size,
       collections = collections
     ).asJson
   else
+    // TODO: non-verbose list should include UUID and Property Name
     ListCollectionsResult(
       dataSourceUri = config.datasourceUri,
       count = collections.size,
       uuids = collections.map(_.pk)
     ).asJson
-
-case class CollectionWithPropertyValues
-(
-  pk: UUID = UUID.randomUUID(),
-  virtual: Boolean = false,
-  deleted: Boolean = false,
-  created: ZonedDateTime = ZonedDateTime.now(),
-  modified: ZonedDateTime = ZonedDateTime.now(),
-  properties: Map[String, List[String]]
-)
-
-case class GetCollectionsResult
-(
-  dataSourceUri: String,
-  count: Int,
-  collections: List[CollectionWithPropertyValues]
-)
-
-def propertyValuesToMapTuple(propertyValue: traits.entity.projected.PropertyValue): (String, List[String]) =
-  val hexFormat = HexFormat.of()
-  propertyValue.property.propertyName -> (
-    propertyValue.textValues ++
-    propertyValue.byteValues.map(hexFormat.formatHex) ++
-    propertyValue.smallintValues.map(_.toString) ++
-    propertyValue.intValues.map(_.toString) ++
-    propertyValue.bigintValues.map(_.toString()) ++
-    propertyValue.numericValues.map(_.toString()) ++
-    propertyValue.floatValues.map(_.toString) ++
-    propertyValue.doubleValues.map(_.toString) ++
-    propertyValue.booleanValues.map(_.toString) ++
-    propertyValue.dateValues.map(_.toString) ++
-    propertyValue.timeValues.map(_.toString) ++
-    propertyValue.timestampValues.map(_.toString) ++
-    propertyValue.uuidValues.map(_.toString) ++
-    propertyValue.jsonValues.map(_.spaces2)
-  )
-
-
-def getCollections(config: Config): Json =
-  def getCollections(collectionDAO: traits.dao.raw.CollectionDAO, propertyValueDAO: traits.dao.projected.PropertyValueDAO) =
-    val collections = collectionDAO.getAllMatchingPKs(config.uuids)
-    // TODO: Retrieve PropertyValueSets associated with Collection
-    val propertyValues = propertyValueDAO.getPropertyValuesByCollectionUUIDs(config.uuids)
-    val propertyValuesByPVSUUID = propertyValues.groupBy(_.collection.pk)
-    GetCollectionsResult(
-      dataSourceUri = config.datasourceUri,
-      count = collections.size,
-      collections = collections
-        .map(collection => CollectionWithPropertyValues(
-          pk = collection.pk,
-          virtual = collection.virtual,
-          deleted = collection.deleted,
-          created = collection.created,
-          modified = collection.modified,
-          properties = Map.from(propertyValuesByPVSUUID
-            .getOrElse(collection.pk, Nil)
-            .map(propertyValuesToMapTuple))
-        ))).asJson
-  Injection.produceRun(config)(getCollections)
