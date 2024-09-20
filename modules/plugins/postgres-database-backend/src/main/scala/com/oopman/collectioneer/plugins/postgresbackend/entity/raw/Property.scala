caddb64aefa075bc7b9ebe278521ae30c909a1bd
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2024-09-19 23:01:15 -0400

    #6: Reduced duplicate code in entity.raw.Property in postgresDatabaseBackend

--- modules/plugins/postgres-database-backend/src/main/scala/com/oopman/collectioneer/plugins/postgresbackend/entity/raw/Property.scala
+++ modules/plugins/postgres-database-backend/src/main/scala/com/oopman/collectioneer/plugins/postgresbackend/entity/raw/Property.scala
@@ -13,36 +13,29 @@ object Property extends SQLSyntaxSupport[raw.Property]:
   val p1 = Property.syntax("p1")
   val p2 = Property.syntax("p2")
 
-  def apply(p: ResultName[raw.Property])(rs: WrappedResultSet) =
-    val propertyTypes = rs
-      .array(p.propertyTypes)
-      .getArray()
-      .asInstanceOf[Array[Object]]
-      .map(s => PropertyType.valueOf(s.asInstanceOf[String]))
-      .toList
-    raw.Property(
-      pk = UUID.fromString(rs.string(p.pk)),
-      propertyName = rs.string(p.propertyName),
-      propertyTypes = propertyTypes,
-      deleted = rs.boolean(p.deleted),
-      created = rs.zonedDateTime(p.created),
-      modified = rs.zonedDateTime(p.modified)
-    )
+  def apply(p: ResultName[raw.Property])(rs: WrappedResultSet): raw.Property =
+    apply(rs, p.pk, p.propertyName, p.propertyTypes, p.deleted, p.created, p.modified)
 
-  def apply(rs: WrappedResultSet) =
+  def apply(rs: WrappedResultSet,
+            pkColumn: String = "pk",
+            propertyNameColumn: String = "property_name",
+            propertyTypesColumn: String = "property_types",
+            deletedColumn: String = "deleted",
+            createdColumn: String = "created",
+            modifiedColmn: String = "modified"): raw.Property =
     val propertyTypes = rs
-      .array("property_types")
+      .array(propertyTypesColumn)
       .getArray()
       .asInstanceOf[Array[Object]]
       .map(s => PropertyType.valueOf(s.asInstanceOf[String]))
       .toList
     raw.Property(
-      pk = UUID.fromString(rs.string("pk")),
-      propertyName = rs.string("property_name"),
+      pk = UUID.fromString(rs.string(pkColumn)),
+      propertyName = rs.string(propertyNameColumn),
       propertyTypes = propertyTypes,
-      deleted = rs.boolean("deleted"),
-      created = rs.zonedDateTime("created"),
-      modified = rs.zonedDateTime("modified")
+      deleted = rs.boolean(deletedColumn),
+      created = rs.zonedDateTime(createdColumn),
+      modified = rs.zonedDateTime(modifiedColmn)
     )
 
   def propertiesSeqToBatchInsertSeq(properties: Seq[traits.entity.raw.Property]): Seq[Seq[Any]] =
