187b42e96af58488fca87bb3c292ebcd5043c010
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2024-09-30 21:06:02 -0400

    #9: Implemented BaseFunSuite in PostgreSQL Database Backend module

--- modules/plugins/postgres-database-backend/src/test/scala/com/oopman/collectioneer/plugins/postgresbackend/test/BaseFunSuite.scala
+++ modules/plugins/postgres-database-backend/src/test/scala/com/oopman/collectioneer/plugins/postgresbackend/test/BaseFunSuite.scala
@@ -1,8 +1,24 @@
 package com.oopman.collectioneer.plugins.postgresbackend.test
 
+import com.oopman.collectioneer.db.scalikejdbc.ScalikeJDBCDatabaseBackendPlugin
+import com.oopman.collectioneer.db.{DatabaseBackendPlugin, Injection}
+import com.oopman.collectioneer.plugins.postgresbacken.TestPostgresDatabaseBackendPlugin
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.flatspec.FixtureAnyFlatSpec
+import scalikejdbc.*
 import scalikejdbc.scalatest.AutoRollback
 
-class BaseFunSuite extends FixtureAnyFlatSpec with AutoRollback with BeforeAndAfterAll
-  // TODO Add fixture stuff for database
+object T:
+  protected object Config extends com.oopman.collectioneer.Config:
+    override val datasourceUri: String = "jdbc:testpostgresql"
+
+  lazy val initialized: Boolean = Injection.produceRun(Config) { (databaseBackendPlugin: DatabaseBackendPlugin) =>
+    databaseBackendPlugin.startUp()
+    true
+  }
+
+abstract class BaseFunSuite extends FixtureAnyFlatSpec with AutoRollback with BeforeAndAfterAll:
+
+  override def db(): DB =
+    if (!T.initialized) throw RuntimeException("Boo!")
+    DB(conn = ConnectionPool.borrow(), settingsProvider = SettingsProvider.default)
