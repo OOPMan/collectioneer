e56730643adce4085c42eeb33d356533f96ad1c6
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2024-10-01 11:12:47 -0400

    #9: Rename T object in BaseFunSuite.scala to DatabaseBackendPluginSupport

--- modules/plugins/postgres-database-backend/src/test/scala/com/oopman/collectioneer/plugins/postgresbackend/test/BaseFunSuite.scala
+++ modules/plugins/postgres-database-backend/src/test/scala/com/oopman/collectioneer/plugins/postgresbackend/test/BaseFunSuite.scala
@@ -1,14 +1,12 @@
 package com.oopman.collectioneer.plugins.postgresbackend.test
 
-import com.oopman.collectioneer.db.scalikejdbc.ScalikeJDBCDatabaseBackendPlugin
 import com.oopman.collectioneer.db.{DatabaseBackendPlugin, Injection}
-import com.oopman.collectioneer.plugins.postgresbacken.TestPostgresDatabaseBackendPlugin
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.flatspec.FixtureAnyFlatSpec
 import scalikejdbc.*
 import scalikejdbc.scalatest.AutoRollback
 
-object T:
+object DatabaseBackendPluginSupport:
   protected object Config extends com.oopman.collectioneer.Config:
     override val datasourceUri: String = "jdbc:testpostgresql"
 
@@ -20,5 +18,5 @@ object T:
 abstract class BaseFunSuite extends FixtureAnyFlatSpec with AutoRollback with BeforeAndAfterAll:
 
   override def db(): DB =
-    if (!T.initialized) throw RuntimeException("Boo!")
+    if (!DatabaseBackendPluginSupport.initialized) throw RuntimeException("Boo!")
     DB(conn = ConnectionPool.borrow(), settingsProvider = SettingsProvider.default)
