caecdb7d468df790f98dcee27ad1a3e3aa4021a4
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2024-10-04 20:26:24 -0400

    #9: Added skeleton Specs for remaining DAOImpl classes

---
+++ modules/plugins/postgres-database-backend/src/test/scala/com/oopman/collectioneer/plugins/postgresbackend/test/dao/projected/ProjectedPropertyDAOImplSpec.scala
@@ -0,0 +1,7 @@
+package com.oopman.collectioneer.plugins.postgresbackend.test.dao.projected
+
+import com.oopman.collectioneer.plugins.postgresbackend.test.BaseFunSuite
+
+class ProjectedPropertyDAOImplSpec extends BaseFunSuite {
+
+}
---
+++ modules/plugins/postgres-database-backend/src/test/scala/com/oopman/collectioneer/plugins/postgresbackend/test/dao/raw/RawRelationshipDAOImplSpec.scala
@@ -0,0 +1,7 @@
+package com.oopman.collectioneer.plugins.postgresbackend.test.dao.raw
+
+import com.oopman.collectioneer.plugins.postgresbackend.test.BaseFunSuite
+
+class RawRelationshipDAOImplSpec extends BaseFunSuite {
+
+}
