7787b1c9fb8b5fbb3062dfcfd0271976923bdc08
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2024-10-06 20:56:57 -0400

    #9: Began adding additional tests in RawCollectionDAOImplSpec

--- modules/plugins/postgres-database-backend/src/test/scala/com/oopman/collectioneer/plugins/postgresbackend/test/dao/raw/RawCollectionDAOImplSpec.scala
+++ modules/plugins/postgres-database-backend/src/test/scala/com/oopman/collectioneer/plugins/postgresbackend/test/dao/raw/RawCollectionDAOImplSpec.scala
@@ -1,12 +1,34 @@
 package com.oopman.collectioneer.plugins.postgresbackend.test.dao.raw
 
+import com.oopman.collectioneer.db.entity.raw.Collection
 import com.oopman.collectioneer.plugins.postgresbackend.dao.raw.CollectionDAOImpl
 import com.oopman.collectioneer.plugins.postgresbackend.test.BaseFunSuite
 
+import java.util.UUID
+
 class RawCollectionDAOImplSpec extends BaseFunSuite:
-  behavior of "com.oopman.collectioneer.plugins.postgresbackend.dao.raw.CollectionDAOImpl.getAll"
+  behavior of "com.oopman.collectioneer.plugins.postgresbackend.dao.raw.CollectionDAOImpl.createCollections"
+
+  it should "create a Collection" in { implicit session =>
+    val collection = Collection()
+    val result = CollectionDAOImpl.createCollections(Seq(collection))
+    assert(result.length > 0)
+  }
+  
+  it should "fail to create a Collection that already exists" in { implicit session => 
+    val collection = Collection()
+    val result = CollectionDAOImpl.createCollections(Seq(collection))
+    assert(result.length > 0)
+    assertThrows[java.sql.BatchUpdateException](CollectionDAOImpl.createCollections(Seq(collection)))
+  }
+
+  behavior of "com.oopman.collectioneer.plugins.postgresbackend.dao.raw.CollectionDAOImpl.createOrUpdateCollections"
 
   it should "return a List of raw Collection objects" in { implicit session =>
     val collections = CollectionDAOImpl.getAll
     assert(collections.nonEmpty)
   }
+
+  behavior of "com.oopman.collectioneer.plugins.postgresbackend.dao.raw.CollectionDAOImpl.getAllMatchingPKs"
+  behavior of "com.oopman.collectioneer.plugins.postgresbackend.dao.raw.CollectionDAOImpl.getAllMatchingPropertyValues"
+  behavior of "com.oopman.collectioneer.plugins.postgresbackend.dao.raw.CollectionDAOImpl.getAllRelatedMatchingPropertyValues"
