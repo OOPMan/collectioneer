ad467bda3f448de78ba5ad2493eba3a652143d83
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2024-10-13 20:42:14 -0400

    #9: Continued adding additional tests in RawCollectionDAOImplSpec

--- modules/plugins/postgres-database-backend/src/test/scala/com/oopman/collectioneer/plugins/postgresbackend/test/dao/raw/RawCollectionDAOImplSpec.scala
+++ modules/plugins/postgres-database-backend/src/test/scala/com/oopman/collectioneer/plugins/postgresbackend/test/dao/raw/RawCollectionDAOImplSpec.scala
@@ -4,8 +4,6 @@ import com.oopman.collectioneer.db.entity.raw.Collection
 import com.oopman.collectioneer.plugins.postgresbackend.dao.raw.CollectionDAOImpl
 import com.oopman.collectioneer.plugins.postgresbackend.test.BaseFunSuite
 
-import java.util.UUID
-
 class RawCollectionDAOImplSpec extends BaseFunSuite:
   behavior of "com.oopman.collectioneer.plugins.postgresbackend.dao.raw.CollectionDAOImpl.createCollections"
 
@@ -23,6 +21,23 @@ class RawCollectionDAOImplSpec extends BaseFunSuite:
   }
 
   behavior of "com.oopman.collectioneer.plugins.postgresbackend.dao.raw.CollectionDAOImpl.createOrUpdateCollections"
+
+  it should "create a Collection" in { implicit session =>
+    val collection = Collection()
+    val result = CollectionDAOImpl.createOrUpdateCollections(Seq(collection))
+    assert(result.length > 0)
+  }
+
+  it should "update a Collection" in { implicit session =>
+    val collection = Collection()
+    val result = CollectionDAOImpl.createOrUpdateCollections(Seq(collection))
+    assert(result.length > 0)
+    val updatedCollection = collection.copy(virtual = true)
+    val newResult = CollectionDAOImpl.createOrUpdateCollections(Seq(updatedCollection))
+    assert(result.length > 0)
+  }
+
+  behavior of "com.oopman.collectioneer.plugins.postgresbackend.dao.raw.CollectionDAOImpl.getAll"
 
   it should "return a List of raw Collection objects" in { implicit session =>
     val collections = CollectionDAOImpl.getAll
