5fefe11a8393695f3fcb9a86d7ca016fb342756d
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2024-10-16 12:56:49 -0400

    #9: Added test for raw CollectionDAOImpl.getAllMatchingPKs

--- modules/plugins/postgres-database-backend/src/test/scala/com/oopman/collectioneer/plugins/postgresbackend/test/dao/raw/RawCollectionDAOImplSpec.scala
+++ modules/plugins/postgres-database-backend/src/test/scala/com/oopman/collectioneer/plugins/postgresbackend/test/dao/raw/RawCollectionDAOImplSpec.scala
@@ -1,5 +1,6 @@
 package com.oopman.collectioneer.plugins.postgresbackend.test.dao.raw
 
+import com.oopman.collectioneer.CoreCollections
 import com.oopman.collectioneer.db.entity.raw.Collection
 import com.oopman.collectioneer.plugins.postgresbackend.dao.raw.CollectionDAOImpl
 import com.oopman.collectioneer.plugins.postgresbackend.test.BaseFunSuite
@@ -45,5 +46,14 @@ class RawCollectionDAOImplSpec extends BaseFunSuite:
   }
 
   behavior of "com.oopman.collectioneer.plugins.postgresbackend.dao.raw.CollectionDAOImpl.getAllMatchingPKs"
+
+  it should "return a List of raw Collection objects" in { implicit session =>
+    val collections = CollectionDAOImpl.getAllMatchingPKs(Seq(
+      CoreCollections.commonProperties.collection.pk,
+      CoreCollections.commonPropertiesOfProperties.collection.pk
+    ))
+    assert(collections.length == 2)
+  }
+
   behavior of "com.oopman.collectioneer.plugins.postgresbackend.dao.raw.CollectionDAOImpl.getAllMatchingPropertyValues"
   behavior of "com.oopman.collectioneer.plugins.postgresbackend.dao.raw.CollectionDAOImpl.getAllRelatedMatchingPropertyValues"
