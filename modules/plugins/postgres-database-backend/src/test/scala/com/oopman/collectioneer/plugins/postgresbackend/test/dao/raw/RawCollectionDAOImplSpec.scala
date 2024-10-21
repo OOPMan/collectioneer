fc95d5743424ed711e034836a0c96e03ea677429
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2024-10-20 20:59:35 -0400

    #9: Implemented final test case in RawCollectionDAOImplSpec

--- modules/plugins/postgres-database-backend/src/test/scala/com/oopman/collectioneer/plugins/postgresbackend/test/dao/raw/RawCollectionDAOImplSpec.scala
+++ modules/plugins/postgres-database-backend/src/test/scala/com/oopman/collectioneer/plugins/postgresbackend/test/dao/raw/RawCollectionDAOImplSpec.scala
@@ -1,9 +1,10 @@
 package com.oopman.collectioneer.plugins.postgresbackend.test.dao.raw
 
-import com.oopman.collectioneer.db.entity.raw.Collection
+import com.oopman.collectioneer.CoreCollections
+import com.oopman.collectioneer.db.entity.raw.{Collection, Relationship}
+import com.oopman.collectioneer.db.traits.entity.raw.RelationshipType.SourceOfPropertiesAndPropertyValues
 import com.oopman.collectioneer.plugins.postgresbackend.dao.raw.CollectionDAOImpl
 import com.oopman.collectioneer.plugins.postgresbackend.test.BaseFunSuite
-import com.oopman.collectioneer.{CoreCollections, CoreProperties}
 
 class RawCollectionDAOImplSpec extends BaseFunSuite:
   behavior of "com.oopman.collectioneer.plugins.postgresbackend.dao.raw.CollectionDAOImpl.createCollections"
@@ -60,9 +61,29 @@ class RawCollectionDAOImplSpec extends BaseFunSuite:
   it should "return a List of raw Collection objects matching the PropertyValueComparisons supplied" in { implicit session =>
     import com.oopman.collectioneer.db.PropertyValueQueryDSL.*
     import com.oopman.collectioneer.{CoreProperties, given}
-    val collections = CollectionDAOImpl.getAllRelatedMatchingPropertyValues(Seq(
+    val collections = CollectionDAOImpl.getAllMatchingPropertyValues(Seq(
       CoreProperties.name equalTo "Common Properties"
     ))
     assert(collections.length == 1)
   }
+
   behavior of "com.oopman.collectioneer.plugins.postgresbackend.dao.raw.CollectionDAOImpl.getAllRelatedMatchingPropertyValues"
+
+  it should "return a List of raw Collection objects matching the PropertyValueComparisons supplied, either directly or via Relationship" in { implicit session =>
+    import com.oopman.collectioneer.plugins.postgresbackend.dao.raw.RelationshipDAOImpl
+    import com.oopman.collectioneer.{CoreCollections, CoreProperties, given}
+    val collection = Collection()
+    val relationship = Relationship(
+      collectionPK = collection.pk,
+      relatedCollectionPK = CoreCollections.commonProperties.pk,
+      relationshipType = SourceOfPropertiesAndPropertyValues
+    )
+    val createCollectionResult = CollectionDAOImpl.createCollections(Seq(collection))
+    assert(createCollectionResult.length > 0)
+    val createRelationshipResult = RelationshipDAOImpl.createRelationships(Seq(relationship))
+    assert(createRelationshipResult.length > 0)
+    val collections = CollectionDAOImpl.getAllRelatedMatchingPropertyValues(Seq(
+      CoreProperties.name equalTo "Common Properties"
+    ))
+    assert(collections.length == 2)
+  }
