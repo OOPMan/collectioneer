80e936a72b4eb89a36a437bfae7f8244d504b214
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2024-10-22 20:46:21 -0400

    #9: Began implementing tests in RawPropertyDAOImplSpec

--- modules/plugins/postgres-database-backend/src/test/scala/com/oopman/collectioneer/plugins/postgresbackend/test/dao/raw/RawPropertyDAOImplSpec.scala
+++ modules/plugins/postgres-database-backend/src/test/scala/com/oopman/collectioneer/plugins/postgresbackend/test/dao/raw/RawPropertyDAOImplSpec.scala
@@ -1,7 +1,22 @@
 package com.oopman.collectioneer.plugins.postgresbackend.test.dao.raw
 
 import com.oopman.collectioneer.plugins.postgresbackend.test.BaseFunSuite
+import com.oopman.collectioneer.plugins.postgresbackend.dao.raw.PropertyDAOImpl
 
-class RawPropertyDAOImplSpec extends BaseFunSuite {
+class RawPropertyDAOImplSpec extends BaseFunSuite:
+  behavior of "com.oopman.collectioneer.plugins.postgresbackend.dao.raw.PropertyDAOImpl.createProperties"
 
-}
+  behavior of "com.oopman.collectioneer.plugins.postgresbackend.dao.raw.PropertyDAOImpl.createOrUpdateProperties"
+
+  behavior of "com.oopman.collectioneer.plugins.postgresbackend.dao.raw.PropertyDAOImpl.getAll"
+  
+  it should "return a list of Properties" in { implicit session =>
+    val properties = PropertyDAOImpl.getAll
+    assert(properties.nonEmpty)
+  }
+
+  behavior of "com.oopman.collectioneer.plugins.postgresbackend.dao.raw.PropertyDAOImpl.getAllMatchingPKs"
+
+  behavior of "com.oopman.collectioneer.plugins.postgresbackend.dao.raw.PropertyDAOImpl.getAllMatchingPropertyValues"
+
+  behavior of "com.oopman.collectioneer.plugins.postgresbackend.dao.raw.PropertyDAOImpl.getAllByPropertyCollection"
