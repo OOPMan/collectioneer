718a27603527a037c533a6b11f1334045eb6b8b6
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2024-10-27 21:13:29 -0400

    #9: Implemented tests for Raw PropertyDAOImpl createProperties

--- modules/plugins/postgres-database-backend/src/test/scala/com/oopman/collectioneer/plugins/postgresbackend/test/dao/raw/RawPropertyDAOImplSpec.scala
+++ modules/plugins/postgres-database-backend/src/test/scala/com/oopman/collectioneer/plugins/postgresbackend/test/dao/raw/RawPropertyDAOImplSpec.scala
@@ -1,11 +1,25 @@
 package com.oopman.collectioneer.plugins.postgresbackend.test.dao.raw
 
 import com.oopman.collectioneer.{CoreProperties, given}
+import com.oopman.collectioneer.db.entity.raw.Property
 import com.oopman.collectioneer.plugins.postgresbackend.test.BaseFunSuite
 import com.oopman.collectioneer.plugins.postgresbackend.dao.raw.PropertyDAOImpl
 
 class RawPropertyDAOImplSpec extends BaseFunSuite:
   behavior of "com.oopman.collectioneer.plugins.postgresbackend.dao.raw.PropertyDAOImpl.createProperties"
+
+  it should "create a Property" in { implicit session =>
+    val property = Property()
+    val result = PropertyDAOImpl.createProperties(Seq(property))
+    assert(result.length > 0)
+  }
+
+  it should "fail to create a Property that already exists" in { implicit session =>
+    val property = Property()
+    val result = PropertyDAOImpl.createProperties(Seq(property))
+    assert(result.length > 0)
+    assertThrows[java.sql.BatchUpdateException](PropertyDAOImpl.createProperties(Seq(property)))
+  }
 
   behavior of "com.oopman.collectioneer.plugins.postgresbackend.dao.raw.PropertyDAOImpl.createOrUpdateProperties"
 
