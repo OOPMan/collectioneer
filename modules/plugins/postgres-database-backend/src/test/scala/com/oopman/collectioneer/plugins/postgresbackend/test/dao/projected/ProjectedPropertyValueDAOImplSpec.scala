1ad7eb1e4b3351089b5b2d12e0fe13e394c99ea9
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2025-02-17 20:57:37 -0500

    #18: Added test for projected ProopertyValueDAOImpl.getPropertyValuesByCollectionUUIDs in ProjectedPropertyValueDAOImplSpec

--- modules/plugins/postgres-database-backend/src/test/scala/com/oopman/collectioneer/plugins/postgresbackend/test/dao/projected/ProjectedPropertyValueDAOImplSpec.scala
+++ modules/plugins/postgres-database-backend/src/test/scala/com/oopman/collectioneer/plugins/postgresbackend/test/dao/projected/ProjectedPropertyValueDAOImplSpec.scala
@@ -1,7 +1,15 @@
 package com.oopman.collectioneer.plugins.postgresbackend.test.dao.projected
 
-import com.oopman.collectioneer.plugins.postgresbackend.test.BaseFunSuite
+import com.oopman.collectioneer.plugins.postgresbackend.dao.projected.PropertyValueDAOImpl
+import com.oopman.collectioneer.plugins.postgresbackend.test.{BaseFunSuite, Fixtures}
 
 class ProjectedPropertyValueDAOImplSpec extends BaseFunSuite {
+  behavior of "com.oopman.collectioneer.plugins.postgresbackend.dao.projected.PropertyValueDAOImpl.getPropertyValuesByCollectionUUIDs"
+
+  it should "return a List of PropertyValue objects for the provided Collections" in { implicit session =>
+    val fixtures = new Fixtures()
+    import fixtures._
+    val propertyValues = PropertyValueDAOImpl.getPropertyValuesByCollectionUUIDs(Seq(childAofRootA.pk))
+  }
 
 }
