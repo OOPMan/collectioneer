3deac1f5c2402edb3840469099b59780f733ef6d
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2025-02-21 21:03:42 -0500

    #19: Significantly restructured PropertyValueQueryDSL in order to support correction of faulty handling of PV comparisons against vector data

--- modules/core/src/main/scala/com/oopman/collectioneer/db/PropertyValueQueryDSL.scala
+++ modules/core/src/main/scala/com/oopman/collectioneer/db/PropertyValueQueryDSL.scala
@@ -8,50 +8,90 @@ import java.util.UUID
 
 object PropertyValueQueryDSL:
 
-  enum Operator:
-    case greaterThan extends Operator
-    case greaterThanOrEqualTo extends Operator
-    case equalTo extends Operator
-    case notEqualTo extends Operator
-    case lessThanOrEualTo extends Operator
-    case lessThan extends Operator
-    case like extends Operator
+  sealed trait Operator
 
-  enum LogicalOperator:
+  enum ScalarOperator extends Operator:
+    case greaterThan extends ScalarOperator
+    case greaterThanOrEqualTo extends ScalarOperator
+    case equalTo extends ScalarOperator
+    case notEqualTo extends ScalarOperator
+    case lessThanOrEualTo extends ScalarOperator
+    case lessThan extends ScalarOperator
+    case like extends ScalarOperator
+
+  enum VectorOperator extends Operator:
+    case greaterThanAny extends VectorOperator
+    case greaterThanOrEqualToAny extends VectorOperator
+    case equalToAny extends VectorOperator
+    case notEqualToAny extends VectorOperator
+    case lessThanOrEualToAny extends VectorOperator
+    case lessThanAny extends VectorOperator
+    case likeAny extends VectorOperator
+    case greaterThanAll extends VectorOperator
+    case greaterThanOrEqualToAll extends VectorOperator
+    case equalToAll extends VectorOperator
+    case notEqualToAll extends VectorOperator
+    case lessThanOrEualToAll extends VectorOperator
+    case lessThanAll extends VectorOperator
+    case likeAll extends VectorOperator
+
+  enum LogicalOperator extends Operator:
     case and extends LogicalOperator
     case or extends LogicalOperator
 
-  type Value = BigInt | Boolean | Byte | LocalDate | Double | Float | Short | Int | io.circe.Json | BigDecimal | String | LocalTime | ZonedDateTime | UUID
-  type Values = Value | Seq[Value] | Array[Value]
+  type ScalarValues =
+    BigInt | Boolean | Array[Byte] | LocalDate | Double | Float | Short | Int | io.circe.Json | BigDecimal | String |
+    LocalTime | ZonedDateTime | UUID
+  type VectorValues =
+    Seq[BigInt] | Seq[Boolean] | Seq[Array[Byte]] | Seq[LocalDate] | Seq[Double] | Seq[Float] | Seq[Short] | Seq[Int] |
+    Seq[io.circe.Json] | Seq[BigDecimal] | Seq[String] | Seq[LocalTime] | Seq[ZonedDateTime] | Seq[UUID]
+  type Values = ScalarValues | VectorValues
   type Operand = Property | Values | Comparison
 
   sealed trait Comparison:
     val lhs: Operand
-    val operator: Operator | LogicalOperator
+    val operator: Operator
     val rhs: Operand
 
-  case class PropertyValueComparison(lhs: Property, operator: Operator, rhs: Values) extends Comparison
-  case class PropertyPropertyComparison(lhs: Property, operator: Operator, rhs: Property) extends Comparison
+  case class PropertyValueScalarComparison(lhs: Property, operator: ScalarOperator, rhs: ScalarValues) extends Comparison
+  case class PropertyValueVectorComparison(lhs: Property, operator: VectorOperator, rhs: VectorValues) extends Comparison
+  case class PropertyPropertyComparison(lhs: Property, operator: ScalarOperator, rhs: Property) extends Comparison
   case class NestedComparison(lhs: Comparison, operator: LogicalOperator, rhs: Comparison) extends Comparison
 
   extension (lhs: Property)
-    def gt(rhs: Values) = PropertyValueComparison(lhs, Operator.greaterThan, rhs)
-    def gte(rhs: Values) = PropertyValueComparison(lhs, Operator.greaterThanOrEqualTo, rhs)
-    def equalTo(rhs: Values) = PropertyValueComparison(lhs, Operator.equalTo, rhs)
-    def notEqualTo(rhs: Values) = PropertyValueComparison(lhs, Operator.notEqualTo, rhs)
-    def lte(rhs: Values) = PropertyValueComparison(lhs, Operator.lessThanOrEualTo, rhs)
-    def lt(rhs: Values) = PropertyValueComparison(lhs, Operator.lessThan, rhs)
-    def like(rhs: Values) = PropertyValueComparison(lhs, Operator.like, rhs)
+    infix def gt(rhs: ScalarValues) = PropertyValueScalarComparison(lhs, ScalarOperator.greaterThan, rhs)
+    infix def gte(rhs: ScalarValues) = PropertyValueScalarComparison(lhs, ScalarOperator.greaterThanOrEqualTo, rhs)
+    infix def equalTo(rhs: ScalarValues) = PropertyValueScalarComparison(lhs, ScalarOperator.equalTo, rhs)
+    infix def notEqualTo(rhs: ScalarValues) = PropertyValueScalarComparison(lhs, ScalarOperator.notEqualTo, rhs)
+    infix def lte(rhs: ScalarValues) = PropertyValueScalarComparison(lhs, ScalarOperator.lessThanOrEualTo, rhs)
+    infix def lt(rhs: ScalarValues) = PropertyValueScalarComparison(lhs, ScalarOperator.lessThan, rhs)
+    infix def like(rhs: ScalarValues) = PropertyValueScalarComparison(lhs, ScalarOperator.like, rhs)
 
   extension (lhs: Property)
-      def gt(rhs: Property) = PropertyPropertyComparison(lhs, Operator.greaterThan, rhs)
-      def gte(rhs: Property) = PropertyPropertyComparison(lhs, Operator.greaterThanOrEqualTo, rhs)
-      def equalTo(rhs: Property) = PropertyPropertyComparison(lhs, Operator.equalTo, rhs)
-      def notEqualTo(rhs: Property) = PropertyPropertyComparison(lhs, Operator.notEqualTo, rhs)
-      def lte(rhs: Property) = PropertyPropertyComparison(lhs, Operator.lessThanOrEualTo, rhs)
-      def lt(rhs: Property) = PropertyPropertyComparison(lhs, Operator.lessThan, rhs)
-      def like(rhs: Property) = PropertyPropertyComparison(lhs, Operator.like, rhs)
+    infix def gtAny(rhs: VectorValues) = PropertyValueVectorComparison(lhs, VectorOperator.greaterThanAny, rhs)
+    infix def gteAny(rhs: VectorValues) = PropertyValueVectorComparison(lhs, VectorOperator.greaterThanOrEqualToAny, rhs)
+    infix def equalToAny(rhs: VectorValues) = PropertyValueVectorComparison(lhs, VectorOperator.equalToAny, rhs)
+    infix def notEqualToAny(rhs: VectorValues) = PropertyValueVectorComparison(lhs, VectorOperator.notEqualToAny, rhs)
+    infix def lteAny(rhs: VectorValues) = PropertyValueVectorComparison(lhs, VectorOperator.lessThanOrEualToAny, rhs)
+    infix def ltAny(rhs: VectorValues) = PropertyValueVectorComparison(lhs, VectorOperator.lessThanAny, rhs)
+    infix def likeAny(rhs: VectorValues) = PropertyValueVectorComparison(lhs, VectorOperator.likeAny, rhs)
+    infix def gtAll(rhs: VectorValues) = PropertyValueVectorComparison(lhs, VectorOperator.greaterThanAll, rhs)
+    infix def gteAll(rhs: VectorValues) = PropertyValueVectorComparison(lhs, VectorOperator.greaterThanOrEqualToAll, rhs)
+    infix def equalToAll(rhs: VectorValues) = PropertyValueVectorComparison(lhs, VectorOperator.equalToAll, rhs)
+    infix def notEqualToAll(rhs: VectorValues) = PropertyValueVectorComparison(lhs, VectorOperator.notEqualToAll, rhs)
+    infix def lteAll(rhs: VectorValues) = PropertyValueVectorComparison(lhs, VectorOperator.lessThanOrEualToAll, rhs)
+    infix def ltAll(rhs: VectorValues) = PropertyValueVectorComparison(lhs, VectorOperator.lessThanAll, rhs)
+    infix def likeAll(rhs: VectorValues) = PropertyValueVectorComparison(lhs, VectorOperator.likeAll, rhs)
+
+  extension (lhs: Property)
+      infix def gt(rhs: Property) = PropertyPropertyComparison(lhs, ScalarOperator.greaterThan, rhs)
+      infix def gte(rhs: Property) = PropertyPropertyComparison(lhs, ScalarOperator.greaterThanOrEqualTo, rhs)
+      infix def equalTo(rhs: Property) = PropertyPropertyComparison(lhs, ScalarOperator.equalTo, rhs)
+      infix def notEqualTo(rhs: Property) = PropertyPropertyComparison(lhs, ScalarOperator.notEqualTo, rhs)
+      infix def lte(rhs: Property) = PropertyPropertyComparison(lhs, ScalarOperator.lessThanOrEualTo, rhs)
+      infix def lt(rhs: Property) = PropertyPropertyComparison(lhs, ScalarOperator.lessThan, rhs)
+      infix def like(rhs: Property) = PropertyPropertyComparison(lhs, ScalarOperator.like, rhs)
 
   extension (lhs: Comparison)
-    def and(rhs: Comparison) = NestedComparison(lhs, LogicalOperator.and, rhs)
-    def or(rhs: Comparison) = NestedComparison(lhs, LogicalOperator.or, rhs)
+    infix def and(rhs: Comparison) = NestedComparison(lhs, LogicalOperator.and, rhs)
+    infix def or(rhs: Comparison) = NestedComparison(lhs, LogicalOperator.or, rhs)
