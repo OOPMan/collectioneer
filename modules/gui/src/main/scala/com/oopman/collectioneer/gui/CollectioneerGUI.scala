63b26d8ad7e021d7bf0d1b6ef72da6690ce46148
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2025-02-27 20:45:05 -0500

    #29: Added super basic main window

--- modules/gui/src/main/scala/com/oopman/collectioneer/gui/CollectioneerGUI.scala
+++ modules/gui/src/main/scala/com/oopman/collectioneer/gui/CollectioneerGUI.scala
@@ -3,37 +3,42 @@ package com.oopman.collectioneer.gui
 import scalafx.application.JFXApp3
 import scalafx.geometry.Insets
 import scalafx.scene.Scene
+import scalafx.scene.control.{Menu, MenuBar, MenuItem}
 import scalafx.scene.effect.DropShadow
-import scalafx.scene.layout.HBox
+import scalafx.scene.layout.{BorderPane, HBox}
 import scalafx.scene.paint.*
 import scalafx.scene.paint.Color.*
 import scalafx.scene.text.Text
+import scalafx.Includes.*
 
 import scala.language.implicitConversions
 
 object CollectioneerGUI extends JFXApp3 :
+  // TODO: Create lazy vals for menu items
+  // TODO: Implement handlers for menu items
+  // TODO: Tie menu-bar width to stage width somehow
 
   override def start(): Unit =
-
     stage = new JFXApp3.PrimaryStage :
-      //    initStyle(StageStyle.Unified)
       title = "Collectioneer"
+      width = 1024
+      height = 768
       scene = new Scene :
         fill = Color.rgb(38, 38, 38)
-        content = new HBox :
-          padding = Insets(50, 80, 50, 80)
-          children = Seq(
-            new Text :
-              text = "Collectioneer"
-              style = "-fx-font: normal bold 100pt sans-serif"
-              fill = new LinearGradient(endX = 0, stops = Stops(Red, DarkRed))
-            ,
-            new Text :
-              text = "ScalaFX GUI"
-              style = "-fx-font: italic bold 100pt sans-serif"
-              fill = new LinearGradient(endX = 0, stops = Stops(White, DarkGray))
-              effect = new DropShadow :
-                color = DarkGray
-                radius = 15
-                spread = 0.25
-          )
+        content = new BorderPane:
+          top = new MenuBar:
+            useSystemMenuBar = true
+            minWidth = 1024
+            menus = List(
+              new Menu("File") {
+                items = List(
+                  new MenuItem("Open"),
+                  new MenuItem("Exit")
+                )
+              },
+              new Menu("Help") {
+                items = List(
+                  new MenuItem("About")
+                )
+              }
+            )
