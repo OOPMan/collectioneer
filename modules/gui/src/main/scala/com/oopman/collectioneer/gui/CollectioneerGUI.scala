b682563d7836f11a99ad993ada95f29d98338379
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2025-03-01 21:05:46 -0500

    #31: WIP integration of DatabaseBackendUIPlugin implementations into CollectioneerGUI

--- modules/gui/src/main/scala/com/oopman/collectioneer/gui/CollectioneerGUI.scala
+++ modules/gui/src/main/scala/com/oopman/collectioneer/gui/CollectioneerGUI.scala
@@ -1,15 +1,18 @@
 package com.oopman.collectioneer.gui
 
+import com.oopman.collectioneer.db.Injection
+import com.oopman.collectioneer.plugins.DatabaseBackendGUIPlugin
 import scalafx.application.JFXApp3
 import scalafx.geometry.Insets
 import scalafx.scene.Scene
-import scalafx.scene.control.{Menu, MenuBar, MenuItem}
+import scalafx.scene.control.{ChoiceBox, Menu, MenuBar, MenuItem}
 import scalafx.scene.effect.DropShadow
-import scalafx.scene.layout.{BorderPane, HBox}
+import scalafx.scene.layout.{BorderPane, HBox, VBox}
 import scalafx.scene.paint.*
 import scalafx.scene.paint.Color.*
 import scalafx.scene.text.Text
 import scalafx.Includes.*
+import scalafx.collections.ObservableBuffer
 
 import scala.language.implicitConversions
 
@@ -17,8 +20,19 @@ object CollectioneerGUI extends JFXApp3 :
   // TODO: Create lazy vals for menu items
   // TODO: Implement handlers for menu items
   // TODO: Tie menu-bar width to stage width somehow
+  lazy val plugins = Injection.produceRun() { (databaseBackendGUIPlugins: Set[DatabaseBackendGUIPlugin]) => databaseBackendGUIPlugins }
+  lazy val pluginNodeMap = plugins.map(p => (p.getUIName, p.getNode)).toMap
+  lazy val pluginPicker: ChoiceBox[String] = new ChoiceBox[String]:
+    items = ObservableBuffer.from(pluginNodeMap.keys)
+    onAction = event => {
+      val selectedPlugin = pluginPicker.selectionModel().getSelectedItem
+      pluginNodeMap.get(selectedPlugin).map(pluginNode => centerVBox.children = Seq(pluginPicker, pluginNode))
+    }
+  lazy val centerVBox = new VBox:
+    children = Seq(pluginPicker)
 
   override def start(): Unit =
+
     stage = new JFXApp3.PrimaryStage :
       title = "Collectioneer"
       width = 1024
@@ -42,3 +56,4 @@ object CollectioneerGUI extends JFXApp3 :
                 )
               }
             )
+          center = centerVBox
