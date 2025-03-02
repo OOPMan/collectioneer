4763bc1c04b108114c8d179988dbee9424fb4d57
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2025-03-01 21:03:43 -0500

    #31: Refactored DatabaseBackendGUIPlugin.getDatabaseBackendPlugin to DatabaseBackendGUIPlugin.getDatasourceURI

--- modules/gui-core/src/main/scala/com/oopman/collectioneer/plugins/DatabaseBackendGUIPlugin.scala
+++ modules/gui-core/src/main/scala/com/oopman/collectioneer/plugins/DatabaseBackendGUIPlugin.scala
@@ -1,9 +1,8 @@
 package com.oopman.collectioneer.plugins
 
-import com.oopman.collectioneer.db.DatabaseBackendPlugin
 import scalafx.scene.Node
 
 trait DatabaseBackendGUIPlugin extends GUIPlugin:
   def getUIName: String
   def getNode: Node
-  def getDatabaseBackendPlugin: DatabaseBackendPlugin
+  def getDatasourceURI: String
