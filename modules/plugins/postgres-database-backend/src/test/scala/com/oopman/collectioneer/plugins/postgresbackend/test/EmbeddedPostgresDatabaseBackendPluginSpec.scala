ed4502f8057d125358886c2f4cd027507dd4f5a9
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2025-03-05 10:31:36 -0500

    #31: Added additional tests for EmbeddedPostgresDatabaseBackendPlugin.parseUrl

--- modules/plugins/postgres-database-backend/src/test/scala/com/oopman/collectioneer/plugins/postgresbackend/test/EmbeddedPostgresDatabaseBackendPluginSpec.scala
+++ modules/plugins/postgres-database-backend/src/test/scala/com/oopman/collectioneer/plugins/postgresbackend/test/EmbeddedPostgresDatabaseBackendPluginSpec.scala
@@ -9,8 +9,35 @@ class EmbeddedPostgresDatabaseBackendPluginSpec extends AnyFlatSpec:
 
   behavior of "com.oopman.collectioneer.plugins.postgresbackend.EmbeddedPostgresDatabaseBackendPlugin.compatibleWithDatasourceUri"
 
-  it should "return true for JDBC URLs that start with jdbc:embeddedpostgresql:" in {
+  it should "return true for JDBC URLs that start with jdbc:embeddedpostgresql: and match the required JDBC URL structure" in {
+    assertResult(true) {
+      EmbeddedPostgresDatabaseBackendPlugin(TestConfig(Some("jdbc:embeddedpostgresql:"))).compatibleWithDatasourceUri
+    }
+    assertResult(true) {
+      EmbeddedPostgresDatabaseBackendPlugin(TestConfig(Some("jdbc:embeddedpostgresql:/"))).compatibleWithDatasourceUri
+    }
+    assertResult(true) {
+      EmbeddedPostgresDatabaseBackendPlugin(TestConfig(Some("jdbc:embeddedpostgresql://"))).compatibleWithDatasourceUri
+    }
     assertResult(true) {
       EmbeddedPostgresDatabaseBackendPlugin(TestConfig(Some("jdbc:embeddedpostgresql:///"))).compatibleWithDatasourceUri
+    }
+    assertResult(true) {
+      EmbeddedPostgresDatabaseBackendPlugin(TestConfig(Some("jdbc:embeddedpostgresql://PATH/"))).compatibleWithDatasourceUri
+    }
+    assertResult(true) {
+      EmbeddedPostgresDatabaseBackendPlugin(TestConfig(Some("jdbc:embeddedpostgresql://PATH/DATABASE"))).compatibleWithDatasourceUri
+    }
+    assertResult(true) {
+      EmbeddedPostgresDatabaseBackendPlugin(TestConfig(Some("jdbc:embeddedpostgresql://PATH/DATABASE?parameter1=true"))).compatibleWithDatasourceUri
+    }
+    assertResult(true) {
+      EmbeddedPostgresDatabaseBackendPlugin(TestConfig(Some("jdbc:embeddedpostgresql://PATH/DATABASE?parameter1=true&parameter2=false"))).compatibleWithDatasourceUri
+    }
+  }
+
+  it should "return false for JDBC URLs that do not start with jdbc:embeddedpostgresql:" in {
+    assertResult(false) {
+      EmbeddedPostgresDatabaseBackendPlugin(TestConfig(Some("jdbc:postgresql:///"))).compatibleWithDatasourceUri
     }
   }
