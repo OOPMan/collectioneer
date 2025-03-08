edbf3c3edf3fa70c1c9355dd048d794d788dd075
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2025-03-08 16:32:54 -0500

    #31: Updated EmbeddedPostgresDatabaseBackendUIPlugin to handle stage parameter of GUIPlugin and use said parameter to display DirectoryChooser when Pick button is clicked

--- modules/plugins/postgres-database-backend-gui/src/main/scala/com/oopman/collectioneer/plugins/postgresbackend/gui/EmbeddedPostgresDatabaseBackendUIPlugin.scala
+++ modules/plugins/postgres-database-backend-gui/src/main/scala/com/oopman/collectioneer/plugins/postgresbackend/gui/EmbeddedPostgresDatabaseBackendUIPlugin.scala
@@ -1,11 +1,18 @@
 package com.oopman.collectioneer.plugins.postgresbackend.gui
 
-import com.oopman.collectioneer.plugins.DatabaseBackendGUIPlugin
+import com.oopman.collectioneer.plugins.{DatabaseBackendGUIPlugin, GUIPlugin}
+import distage.Id
 import scalafx.scene.Node
 import scalafx.scene.control.{Button, Label, TextField}
 import scalafx.scene.layout.GridPane
+import scalafx.stage.{DirectoryChooser, Stage}
+import scalafx.Includes.*
 
-class EmbeddedPostgresDatabaseBackendUIPlugin extends DatabaseBackendGUIPlugin:
+class EmbeddedPostgresDatabaseBackendUIPlugin(stage: Stage @Id("com.oopman.collectioneer.plugins.GUIPlugin.stage"))
+extends GUIPlugin(stage), DatabaseBackendGUIPlugin:
+  protected lazy val directoryChooser = new DirectoryChooser:
+    initialDirectory = os.home.toIO
+
   protected lazy val locationLabel = new Label:
     text = "Location"
 
@@ -13,6 +20,10 @@ class EmbeddedPostgresDatabaseBackendUIPlugin extends DatabaseBackendGUIPlugin:
 
   protected lazy val locationChooserButton = new Button:
     text = "Pick"
+    onAction = { e =>
+      val path =  directoryChooser.showDialog(stage)
+      locationInput.text = path.getAbsolutePath
+    }
 
   protected lazy val databaseBaseLabel = new Label:
     text = "Database"
