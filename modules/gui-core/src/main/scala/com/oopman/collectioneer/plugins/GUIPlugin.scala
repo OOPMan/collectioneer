8414c1e164dec110c6e2954180792e94a88bfd3c
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2025-03-08 11:00:21 -0500

    #31: Added trait parameter stage to GUIPlugin. This parameter is intended to be supplied by Distage using the named binding GUIPlugin.stage to a scalafx.stage.Stage instance

--- modules/gui-core/src/main/scala/com/oopman/collectioneer/plugins/GUIPlugin.scala
+++ modules/gui-core/src/main/scala/com/oopman/collectioneer/plugins/GUIPlugin.scala
@@ -1,5 +1,7 @@
 package com.oopman.collectioneer.plugins
 
 import com.oopman.collectioneer.Plugin
+import distage.Id
+import scalafx.stage.Stage
 
-trait GUIPlugin extends Plugin
+trait GUIPlugin(val stage: Stage @Id("com.oopman.collectioneer.plugins.GUIPlugin.stage")) extends Plugin
