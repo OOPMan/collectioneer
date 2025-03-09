3b6b74de76dc4a66a36f1c2ca1532c45ea58cc46
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2025-03-08 19:50:24 -0500

    #31: Some minor tweaks to DatabaseBackendPicker

--- modules/gui/src/main/scala/com/oopman/collectioneer/gui/DatabaseBackendPicker.scala
+++ modules/gui/src/main/scala/com/oopman/collectioneer/gui/DatabaseBackendPicker.scala
@@ -3,24 +3,23 @@ package com.oopman.collectioneer.gui
 import com.oopman.collectioneer.db.Injection
 import com.oopman.collectioneer.plugins.DatabaseBackendGUIPlugin
 import scalafx.scene.Node
-import scalafx.scene.control.ChoiceBox
+import scalafx.scene.control.{ChoiceBox, Label}
 import scalafx.scene.layout.{BorderPane, HBox}
-import scalafx.scene.text.Text
 import scalafx.Includes.*
 import scalafx.collections.ObservableBuffer
 import scalafx.util.StringConverter
 
 object DatabaseBackendPicker:
-  lazy val plugins: Set[DatabaseBackendGUIPlugin] =
+  private lazy val plugins: Set[DatabaseBackendGUIPlugin] =
     Injection.produceRun(inputModule = CollectioneerGUI.collectioneerGUIModule) {
       (databaseBackendGUIPlugins: Set[DatabaseBackendGUIPlugin]) => databaseBackendGUIPlugins
     }
-  lazy val pluginMap: Map[String, DatabaseBackendGUIPlugin] = plugins.map(plugin => (plugin.getUIName, plugin)).toMap
+  private lazy val pluginMap: Map[String, DatabaseBackendGUIPlugin] = plugins.map(plugin => (plugin.getUIName, plugin)).toMap
 
-  lazy val pluginChoiceBoxLabel = new Text:
-    text = "Database Backend Plugin:"
+  private lazy val pluginChoiceBoxLabel = new Label:
+    text = "Database Backend Plugin"
 
-  lazy val pluginChoiceBox: ChoiceBox[DatabaseBackendGUIPlugin] = new ChoiceBox:
+  private lazy val pluginChoiceBox: ChoiceBox[DatabaseBackendGUIPlugin] = new ChoiceBox:
     items = ObservableBuffer.from(plugins)
     converter = StringConverter(
       fromStringFunction = pluginMap.apply,
@@ -28,11 +27,11 @@ object DatabaseBackendPicker:
     )
     onAction = event => getSelectedPlugin.map(plugin => layout.center = plugin.getNode)
 
-  lazy val layout = new BorderPane:
+  private lazy val layout = new BorderPane:
     top = new HBox:
       children = Seq(pluginChoiceBoxLabel, pluginChoiceBox)
 
-  def getSelectedPlugin: Option[DatabaseBackendGUIPlugin] = Option(pluginChoiceBox.selectionModel().getSelectedItem)
+  private def getSelectedPlugin: Option[DatabaseBackendGUIPlugin] = Option(pluginChoiceBox.selectionModel().getSelectedItem)
 
   def getNode: Node = layout
 
