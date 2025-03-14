4a314afa99eae44f33ba9b866c2b869a77bb6fa3
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2025-03-13 21:23:36 -0400

    #31: Added additional given conversions from Collection/Property to UUID

--- modules/core/src/main/scala/com/oopman/collectioneer/Givens.scala
+++ modules/core/src/main/scala/com/oopman/collectioneer/Givens.scala
@@ -5,8 +5,22 @@ import java.util.UUID
 
 given stringToUUID: Conversion[String, UUID] = (s: String) => UUID.fromString(s)
 
+// TODO: Shouldn't this be db.traits.entity.raw.Collection?
 given coreCollectionsToCollection: Conversion[CoreCollections, db.entity.projected.Collection] =
   (c: CoreCollections) => c.collection
 
+// TODO: Shouldn't this be db.traits.entity.raw.Property?
 given corePropertiesToProperty: Conversion[CoreProperties, db.entity.projected.Property] =
   (p: CoreProperties) =>  p.property
+
+given coreCollectionsToUUID: Conversion[CoreCollections, UUID] =
+  (c: CoreCollections) => c.collection.pk
+
+given CollectionToUUID: Conversion[db.traits.entity.raw.Collection, UUID] =
+  (c: db.traits.entity.raw.Collection) => c.pk
+
+given corePropertiesToUUID: Conversion[CoreProperties, UUID] =
+  (p: CoreProperties) => p.property.pk
+
+given PropertyToUUID: Conversion[db.traits.entity.raw.Property, UUID] =
+  (p: db.traits.entity.raw.Property) => p.pk
