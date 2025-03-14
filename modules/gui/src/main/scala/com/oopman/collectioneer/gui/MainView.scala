21c07b05eee4b301f50f5d304d2054b0b4bb14e7
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2025-03-14 11:09:39 -0400

    #31: Basic implementation of loading of child collections

--- modules/gui/src/main/scala/com/oopman/collectioneer/gui/MainView.scala
+++ modules/gui/src/main/scala/com/oopman/collectioneer/gui/MainView.scala
@@ -4,7 +4,7 @@ import com.oopman.collectioneer.CoreCollections
 import com.oopman.collectioneer.db.Injection
 import com.oopman.collectioneer.db.entity.projected.Collection
 import com.oopman.collectioneer.db.traits.dao.raw.CollectionDAO
-import scalafx.scene.control.{ScrollPane, SplitPane, TreeItem, TreeView}
+import scalafx.scene.control.{ScrollPane, SelectionMode, SplitPane, TreeItem, TreeView}
 import scalafx.scene.layout.Region
 import scalafx.Includes.*
 
@@ -18,7 +18,14 @@ class MainView(val config: GUIConfig):
     root = rootTreeViewItem
     showRoot = true
     cellFactory = (cell, collection) => {
+      // TODO: Hook this into plugins to allow for custom formatting of items based on their properties with fallback default to render the name or UUID
       cell.text = collection.pk.toString
+    }
+    // TODO: In the future, SelctionMode.Multiple may be required
+    selectionModel().selectionMode = SelectionMode.Single
+    selectionModel().selectedItem.onChange {
+      (observableValue, oldItem, newItem) =>
+        refreshChildren(newItem) // TODO: We should probably only refresh if children is empty or data has changed...
     }
 
   // TODO: Implement this in https://github.com/OOPMan/collectioneer/issues/34
@@ -36,6 +43,7 @@ class MainView(val config: GUIConfig):
 
   def refreshChildren(treeItem: TreeItem[Collection] = rootTreeViewItem): Unit =
     Injection.produceRun(Some(config)) { (collectionDAO: CollectionDAO) =>
+      // TODO: This could be hooked into plugins to allow default parameters or something...?
       val collections = collectionDAO.getAllMatchingConstraints(
         parentCollectionPKs = Some(Seq(treeItem.value.value.pk))
       )
