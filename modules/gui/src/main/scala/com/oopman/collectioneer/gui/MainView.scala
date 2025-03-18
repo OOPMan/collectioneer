8f308af27fd78df1c1ab0274b2fabdcfb09e3a2d
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2025-03-17 21:08:07 -0400

    #31: Implemented support for MainViewGUIPlugin in MainViewGUI

--- modules/gui/src/main/scala/com/oopman/collectioneer/gui/MainView.scala
+++ modules/gui/src/main/scala/com/oopman/collectioneer/gui/MainView.scala
@@ -3,13 +3,20 @@ package com.oopman.collectioneer.gui
 import com.oopman.collectioneer.{CoreCollections, CoreProperties}
 import com.oopman.collectioneer.db.{Injection, SortDirection, traits}
 import com.oopman.collectioneer.db.traits.entity.projected.Collection
-import scalafx.scene.control.{ProgressIndicator, ScrollPane, SelectionMode, SplitPane, TreeItem, TreeView}
+import com.oopman.collectioneer.db.traits.entity.raw.Collection as RawCollection
+import com.oopman.collectioneer.plugins.MainViewGUIPlugin
+import scalafx.scene.control.{ProgressIndicator, ScrollPane, SelectionMode, SplitPane, TreeCell, TreeItem, TreeView}
 import scalafx.scene.layout.Region
 import scalafx.Includes.*
 import scalafx.concurrent.Task
+import scalafx.scene.Node
 
 
 class MainView(val config: GUIConfig):
+  private lazy val plugins: Set[MainViewGUIPlugin] =
+    Injection.produceRun(Some(config), CollectioneerGUI.collectioneerGUIModule) {
+      (mainViewGUIPlugins: Set[MainViewGUIPlugin]) => mainViewGUIPlugins
+    }
 
   private lazy val rootCollection = CoreCollections.root.collection
 
@@ -19,19 +26,24 @@ class MainView(val config: GUIConfig):
     root = rootTreeViewItem
     showRoot = true
     cellFactory = (cell, collection) => {
-      // TODO: Hook this into plugins to allow for custom formatting of items based on their properties with fallback default to render the name or UUID
-      val text = collection.propertyValues
-        .find(pv => pv.property.pk == CoreProperties.name.property.pk)
-        .flatMap(pv => pv.textValues.headOption)
-        .getOrElse(collection.pk.toString)
-      cell.text = text
+      val cellFactory: (TreeCell[Collection], Collection) => Unit = plugins
+        .find(plugin => plugin.canGetCollectionsListTreeViewCellFactory(collection))
+        .map(plugin => plugin.getCollectionsListTreeViewCellFactory(collection))
+        .getOrElse((cell, collection) => {
+          val text = collection.propertyValues
+            .find(pv => pv.property.pk == CoreProperties.name.property.pk)
+            .flatMap(pv => pv.textValues.headOption)
+            .getOrElse(collection.pk.toString)
+          cell.text = text
+        })
+      cellFactory(cell, collection)
     }
     // TODO: In the future, SelctionMode.Multiple may be required
     selectionModel().selectionMode = SelectionMode.Single
     selectionModel().selectedItem.onChange {
       (observableValue, oldItem, newItem) =>
-        refreshChildren(newItem) // TODO: We should probably only refresh if children is empty or data has changed...
-        // TODO: Render Collection detail into Collection detail pane
+        if newItem.getChildren.size() == 0 then refreshChildren(newItem)
+        // TODO: Render Collection detail into Collection detail pane in https://github.com/OOPMan/collectioneer/issues/34
     }
 
   // TODO: Implement this in https://github.com/OOPMan/collectioneer/issues/34
@@ -48,33 +60,31 @@ class MainView(val config: GUIConfig):
     content = collectionDetailView
 
   def refreshChildren(treeItem: TreeItem[Collection] = rootTreeViewItem): Unit =
-    val originalGraphic = treeItem.getGraphic
-    treeItem.graphic = new ProgressIndicator
     val worker = Task {
       val collections = Injection.produceRun(Some(config)) {
         (collectionDAO: traits.dao.raw.CollectionDAO, projectedCollectionDAO: traits.dao.projected.CollectionDAO) =>
-          // TODO: This could be hooked into plugins to allow default parameters or something...?
-          // TODO: Query and update should happen in a worker, we should probably show a progress spinner somewhere
-          val collections = collectionDAO.getAllMatchingConstraints(
-            parentCollectionPKs = Some(Seq(treeItem.value.value.pk)),
-            sortProperties = Seq(CoreProperties.name.property -> SortDirection.Asc)
-          )
-          // TODO: We should probably only inflate the CoreProperties.name property
-          // TODO: Decision of which properties to inflate should depend on a plugin
-          val projectedCollections = projectedCollectionDAO.inflateRawCollections(
+          val collection = treeItem.getValue
+          val collections: List[RawCollection] = plugins
+            .find(plugin => plugin.canGetRawChildCollections(collection))
+            .map(plugin => plugin.getRawChildCollections(collection, collectionDAO))
+            .getOrElse(collectionDAO.getAllMatchingConstraints(
+              parentCollectionPKs = Some(Seq(collection.pk)),
+              sortProperties = Seq(CoreProperties.name.property -> SortDirection.Asc)
+            ))
+          val propertyPKs = plugins
+            .find(plugin => plugin.canGetPropertyPKsForInflation(collection))
+            .map(plugin => plugin.getPropertyPKsForInflation(collection))
+            .getOrElse(Seq(CoreProperties.name.property.pk))
+          projectedCollectionDAO.inflateRawCollections(
             collections,
-            propertyPKs = Seq(CoreProperties.name.property.pk)
+            propertyPKs = propertyPKs
           )
-          projectedCollections
-//          val treeItems = projectedCollections.map(projectedCollection => TreeItem(projectedCollection))
-//          treeItem.children = treeItems
       }
       collections
     }
     worker.onSucceeded = { e =>
       val collections = worker.getValue
       val treeItems = collections.map(projectedCollection => TreeItem(projectedCollection))
-      treeItem.graphic = originalGraphic
       treeItem.children = treeItems
     }
     // TODO: Handle failure
@@ -82,7 +92,6 @@ class MainView(val config: GUIConfig):
     thread.setDaemon(true)
     thread.start()
 
-  // TODO: Use config to load database
-  def getNode =
+  def getNode: Node =
     refreshChildren()
     splitPane
