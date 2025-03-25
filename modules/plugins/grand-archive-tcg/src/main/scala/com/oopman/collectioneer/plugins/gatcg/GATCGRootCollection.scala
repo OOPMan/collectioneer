812b156640440a32ccd980306466e55efc7fefd8
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2025-03-24 20:54:09 -0400

    #42: Updated GATCGRootCollection to reflect change ProjectedCollection.propertyValues from Seq[projected.PropertyValue] to Map[projected.PropertyValue]

--- modules/plugins/grand-archive-tcg/src/main/scala/com/oopman/collectioneer/plugins/gatcg/GATCGRootCollection.scala
+++ modules/plugins/grand-archive-tcg/src/main/scala/com/oopman/collectioneer/plugins/gatcg/GATCGRootCollection.scala
@@ -9,9 +9,9 @@ val description = "An anime TCG with western game design"
 val GATCGRootCollection = Collection(
   pk = "b3192f7b-d4d6-4510-ba5c-aa1b60ab3982",
   virtual = true,
-  propertyValues = List(
-    PropertyValue(property = CoreProperties.name, textValues = List(name)),
-    PropertyValue(property = CoreProperties.description, textValues = List(description)),
+  propertyValues = Map(
+    CoreProperties.name -> PropertyValue(textValues = List(name)),
+    CoreProperties.description -> PropertyValue(textValues = List(description)),
     // TODO: Add properties for links
   )
 )
