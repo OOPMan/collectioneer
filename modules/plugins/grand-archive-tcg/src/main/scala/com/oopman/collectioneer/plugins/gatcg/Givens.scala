d6a0c8a99df45fd94a0229c19d91742c12e316cb
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2025-03-24 21:07:38 -0400

    #42: Added new Given conversion in GATCG plugin for (properties.HasProperty, PropertyValue) to (Property, PropertyValue)

--- modules/plugins/grand-archive-tcg/src/main/scala/com/oopman/collectioneer/plugins/gatcg/Givens.scala
+++ modules/plugins/grand-archive-tcg/src/main/scala/com/oopman/collectioneer/plugins/gatcg/Givens.scala
@@ -1,6 +1,10 @@
 package com.oopman.collectioneer.plugins.gatcg
 
-import com.oopman.collectioneer.db.entity.projected.Property
+import com.oopman.collectioneer.db.traits.entity.projected.{Property, PropertyValue}
 
 given hasPropertyToProperty: Conversion[properties.HasProperty, Property] with
   def apply(a: properties.HasProperty) = a.property
+
+given hasPropertyPropertyValueTupleToPropertyPropetyValueTuple
+: Conversion[(properties.HasProperty, PropertyValue), (Property, PropertyValue)] =
+  (t: (properties.HasProperty, PropertyValue)) => t._1.property -> t._2
