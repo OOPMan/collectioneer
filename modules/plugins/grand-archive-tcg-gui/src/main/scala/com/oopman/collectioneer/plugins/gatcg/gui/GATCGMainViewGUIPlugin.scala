a244e734843cd40b356ade2c1412b4abdc165452
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2025-03-27 21:06:34 -0400

    #42: GATCGMainViewGUIPlugin to reflect projected PropertyValue.propertyValues changing to a Map

--- modules/plugins/grand-archive-tcg-gui/src/main/scala/com/oopman/collectioneer/plugins/gatcg/gui/GATCGMainViewGUIPlugin.scala
+++ modules/plugins/grand-archive-tcg-gui/src/main/scala/com/oopman/collectioneer/plugins/gatcg/gui/GATCGMainViewGUIPlugin.scala
@@ -1,11 +1,11 @@
 package com.oopman.collectioneer.plugins.gatcg.gui
 
-import com.oopman.collectioneer.CoreProperties
+import com.oopman.collectioneer.{CoreProperties, given}
 import com.oopman.collectioneer.db.SortDirection
 import com.oopman.collectioneer.db.traits.dao.raw.CollectionDAO
-import com.oopman.collectioneer.db.traits.entity.projected.Collection
+import com.oopman.collectioneer.db.traits.entity.projected.{Collection, Property}
 import com.oopman.collectioneer.db.traits.entity.raw.Collection as RawCollection
-import com.oopman.collectioneer.plugins.gatcg.GATCGRootCollection
+import com.oopman.collectioneer.plugins.gatcg.{GATCGRootCollection, given}
 import com.oopman.collectioneer.plugins.gatcg.properties.{CommonProperties, EditionProperties, SetProperties}
 import com.oopman.collectioneer.plugins.{GUIPlugin, MainViewGUIPlugin}
 import distage.Id
@@ -16,42 +16,42 @@ import java.util.UUID
 
 class GATCGMainViewGUIPlugin(stage: Stage @Id("com.oopman.collectioneer.plugins.GUIPlugin.stage"))
 extends GUIPlugin(stage), MainViewGUIPlugin:
-  private val whitelistedPropertyPKs = Seq(SetProperties.setPrefix.property.pk, EditionProperties.collectorNumber.property.pk)
+  private val whitelistedPropertyPKs: Seq[Property] = Seq(SetProperties.setPrefix, EditionProperties.collectorNumber)
 
   private def setCollectionCellFactory(cell: TreeCell[Collection], collection: Collection): Unit =
     val setName = collection.propertyValues
-      .find(pv => pv.property.pk == CoreProperties.name.property.pk)
-      .flatMap(pv => pv.textValues.headOption)
+      .find((property, pv) => property == CoreProperties.name)
+      .flatMap((property, pv) => pv.textValues.headOption)
       .getOrElse(collection.pk.toString)
     val setPrefix = collection.propertyValues
-      .find(pv => pv.property.pk == SetProperties.setPrefix.property.pk)
-      .flatMap(pv => pv.textValues.headOption)
+      .find((property, pv) => property == SetProperties.setPrefix)
+      .flatMap((property, pv) => pv.textValues.headOption)
       .getOrElse("?")
     cell.text = s"$setName ($setPrefix)"
 
   private def cardCollectionCellFactory(cell: TreeCell[Collection], collection: Collection): Unit =
     val cardName = collection.propertyValues
-      .find(pv => pv.property.pk == CoreProperties.name.property.pk)
-      .flatMap(pv => pv.textValues.headOption)
+      .find((property, pv) => property == CoreProperties.name)
+      .flatMap((property, pv) => pv.textValues.headOption)
       .getOrElse(collection.pk.toString)
     val collectorNumber = collection.propertyValues
-      .find(pv => pv.property.pk == EditionProperties.collectorNumber.property.pk)
-      .flatMap(pv => pv.textValues.headOption)
+      .find((property, pv) => property == EditionProperties.collectorNumber)
+      .flatMap((property, pv) => pv.textValues.headOption)
       .getOrElse("?")
     cell.text = s"$cardName ($collectorNumber)"
 
   def canGetCollectionsListTreeViewCellFactory(collection: Collection): Boolean =
-    collection.propertyValues.exists(pv => whitelistedPropertyPKs.contains(pv.property.pk))
+    collection.propertyValues.keySet.intersect(whitelistedPropertyPKs.toSet).nonEmpty
 
   def canGetRawChildCollections(collection: Collection): Boolean =
-    collection.propertyValues.exists(pv => pv.property.pk == SetProperties.setPrefix.property.pk)
+    collection.propertyValues.contains(SetProperties.setPrefix)
 
   def canGetPropertyPKsForInflation(collection: Collection): Boolean =
-    if collection.pk == GATCGRootCollection.pk then true
-    else collection.propertyValues.exists(pv => pv.property.pk == SetProperties.setPrefix.property.pk)
+    if collection == GATCGRootCollection then true
+    else collection.propertyValues.contains(SetProperties.setPrefix)
 
   def getCollectionsListTreeViewCellFactory(collection: Collection): (TreeCell[Collection], Collection) => Unit =
-    if collection.propertyValues.exists(pv => pv.property.pk == SetProperties.setPrefix.property.pk)
+    if collection.propertyValues.contains(SetProperties.setPrefix)
     then setCollectionCellFactory
     else cardCollectionCellFactory
 
