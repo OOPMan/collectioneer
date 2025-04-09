b20a9f1988a4a2cde2f46f77f2376604747301a9
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2025-04-09 10:46:16 -0400

    #42: Updated V2__initial_data migration to explicitly create Properties

--- modules/plugins/postgres-database-backend/src/main/scala/com/oopman/collectioneer/plugins/postgresbackend/migrations/V2__initial_data.scala
+++ modules/plugins/postgres-database-backend/src/main/scala/com/oopman/collectioneer/plugins/postgresbackend/migrations/V2__initial_data.scala
@@ -1,9 +1,9 @@
 package com.oopman.collectioneer.plugins.postgresbackend.migrations
 
-import com.oopman.collectioneer.{CoreCollections, given}
+import com.oopman.collectioneer.{CoreCollections, CoreProperties, given}
 import com.oopman.collectioneer.db.entity.raw.Relationship
 import com.oopman.collectioneer.db.traits.entity.raw.RelationshipType.ParentCollection
-import com.oopman.collectioneer.plugins.postgresbackend.dao.projected.CollectionDAOImpl
+import com.oopman.collectioneer.plugins.postgresbackend.dao.projected.{CollectionDAOImpl, PropertyDAOImpl}
 import com.oopman.collectioneer.plugins.postgresbackend.dao.raw.RelationshipDAOImpl
 import org.flywaydb.core.api.migration.{BaseJavaMigration, Context}
 import scalikejdbc.DB
@@ -15,6 +15,7 @@ class V2__initial_data extends BaseJavaMigration:
   override def canExecuteInTransaction: Boolean = false
   override def migrate(context: Context): Unit =
     DB(context.getConnection).localTx { implicit session =>
+      PropertyDAOImpl.createProperties(CoreProperties.values.map(_.property))
       CollectionDAOImpl.createCollections(CoreCollections.values.map(_.collection))
       RelationshipDAOImpl.createRelationships(Seq(
         Relationship(
