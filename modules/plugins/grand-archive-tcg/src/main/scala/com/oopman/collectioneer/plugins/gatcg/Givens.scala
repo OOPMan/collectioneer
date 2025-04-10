76820dc5ccf8d0e68fbe1662630c2385df17b6a5
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2025-04-09 21:21:42 -0400

    #42: Normalized GATCG plugin givens

--- modules/plugins/grand-archive-tcg/src/main/scala/com/oopman/collectioneer/plugins/gatcg/Givens.scala
+++ modules/plugins/grand-archive-tcg/src/main/scala/com/oopman/collectioneer/plugins/gatcg/Givens.scala
@@ -2,9 +2,9 @@ package com.oopman.collectioneer.plugins.gatcg
 
 import com.oopman.collectioneer.db.traits.entity.projected
 
-given hasPropertyToProperty: Conversion[properties.HasProperty, projected.Property] with
-  def apply(a: properties.HasProperty) = a.property
+given hasPropertyToProperty: Conversion[properties.HasProperty, projected.Property] =
+  (hasProperty: properties.HasProperty) => hasProperty.property
 
 given hasPropertyPropertyValueTupleToPropertyPropetyValueTuple
 : Conversion[(properties.HasProperty, projected.PropertyValue), (projected.Property, projected.PropertyValue)] =
-  (t: (properties.HasProperty, projected.PropertyValue)) => t._1.property -> t._2
+  (tuple: (properties.HasProperty, projected.PropertyValue)) => tuple._1.property -> tuple._2
