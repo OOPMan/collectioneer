60fd208fc81f5847e42191eafbfdc22f2c6a3cf3
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2025-04-09 21:24:46 -0400

    #42: Update CLI actions to reflect propertyValue field on projected Collection/Property changing from Seq[projected.PropertyValue] to Map[raw.Property, projected.ProperyValue]

--- modules/cli/src/main/scala/com/oopman/collectioneer/cli/actions/get/Common.scala
+++ modules/cli/src/main/scala/com/oopman/collectioneer/cli/actions/get/Common.scala
@@ -5,21 +5,20 @@ import com.oopman.collectioneer.db.traits
 import java.util.HexFormat
 
 object Common:
-  def propertyValuesToMapTuple(propertyValue: traits.entity.projected.PropertyValue): (String, Seq[String]) =
-    val hexFormat = HexFormat.of()
-    propertyValue.property.propertyName -> (
-      propertyValue.textValues ++
-        propertyValue.byteValues.map(hexFormat.formatHex) ++
-        propertyValue.smallintValues.map(_.toString) ++
-        propertyValue.intValues.map(_.toString) ++
-        propertyValue.bigintValues.map(_.toString()) ++
-        propertyValue.numericValues.map(_.toString()) ++
-        propertyValue.floatValues.map(_.toString) ++
-        propertyValue.doubleValues.map(_.toString) ++
-        propertyValue.booleanValues.map(_.toString) ++
-        propertyValue.dateValues.map(_.toString) ++
-        propertyValue.timeValues.map(_.toString) ++
-        propertyValue.timestampValues.map(_.toString) ++
-        propertyValue.uuidValues.map(_.toString) ++
-        propertyValue.jsonValues.map(_.spaces2)
-      )
+  private val hexFormat = HexFormat.of()
+
+  def propertyValueToSeqOfStrings(propertyValue: traits.entity.projected.PropertyValue): Seq[String] =
+    propertyValue.textValues ++
+    propertyValue.byteValues.map(hexFormat.formatHex) ++
+    propertyValue.smallintValues.map(_.toString) ++
+    propertyValue.intValues.map(_.toString) ++
+    propertyValue.bigintValues.map(_.toString()) ++
+    propertyValue.numericValues.map(_.toString()) ++
+    propertyValue.floatValues.map(_.toString) ++
+    propertyValue.doubleValues.map(_.toString) ++
+    propertyValue.booleanValues.map(_.toString) ++
+    propertyValue.dateValues.map(_.toString) ++
+    propertyValue.timeValues.map(_.toString) ++
+    propertyValue.timestampValues.map(_.toString) ++
+    propertyValue.uuidValues.map(_.toString) ++
+    propertyValue.jsonValues.map(_.spaces2)
