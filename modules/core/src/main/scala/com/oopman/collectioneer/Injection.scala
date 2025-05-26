90e092864f00ec1406bd394386e3f1ca3604b6ef
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2025-05-25 21:29:29 -0400

    #34: Updated Injection to make use of ConfigManager to supply ModuleDef that, in turn, supplies bindings for Configs, SubConfigs, etc. Also introduced ability to configure a baseModuleDef for use by the Injection system. Together this allows management and usage of the application Configuration to be managed more dynamically using the Injection system

--- modules/core/src/main/scala/com/oopman/collectioneer/Injection.scala
+++ modules/core/src/main/scala/com/oopman/collectioneer/Injection.scala
@@ -13,25 +13,35 @@ object Injection:
   protected val pluginModules: ModuleBase = PluginLoader().load(pluginConfig).result.merge
   protected val injector: Injector[Identity] = Injector()
   private object emptyModule extends ModuleDef
-  private object dummyConfig extends Config:
-    val datasourceUri: Option[String] = None
-    val subConfigs: Map[String, SubConfig] = Map.empty
+  var baseModuleDef: ModuleDef = emptyModule
 
-  def getInjectorAndModule[F[_], A](configOption: Option[Config] = None, inputModule: ModuleDef = emptyModule): (Injector[Identity], ModuleDef) =
-    val config = configOption.getOrElse(dummyConfig)
-    val baseModule = new ModuleDef:
-      make[Config].from(config)
+  def getInjectorAndModule[F[_], A](inputModuleDef: ModuleDef = emptyModule, withConfig: Boolean = true): (Injector[Identity], ModuleDef) =
+    val bootstrapModuleDef = new ModuleDef:
+      include(baseModuleDef)
       include(pluginModules)
-    val databaseBackendPluginOption: Option[DatabaseBackendPlugin] = injector.produceRun(baseModule) {
-      (databaseBackendPlugins: Set[DatabaseBackendPlugin]) => databaseBackendPlugins.find(_.compatibleWithDatasourceUri)
-    }
-    val finalModule = new ModuleDef:
-      for (databaseBackendPlugin <- databaseBackendPluginOption) make[DatabaseBackendPlugin].from(databaseBackendPlugin)
-      include(baseModule)
-      for (databaseBackendPlugin <- databaseBackendPluginOption) include(databaseBackendPlugin.getDatabaseBackendModule)
-      include(inputModule)
-    (injector, finalModule)
+    val configModuleDef =
+      if withConfig
+      then injector.produceRun(bootstrapModuleDef) {
+        (configManager: ConfigManager) => configManager.getModuleDefForConfig
+      }
+      else emptyModule
+    val databaseBackendPluginOption =
+      try
+        injector.produceRun(bootstrapModuleDef ++ configModuleDef) {
+          (databaseBackendPlugins: Set[DatabaseBackendPlugin]) => databaseBackendPlugins.find(_.compatibleWithDatasourceUri)
+        }
+      catch
+        case _: Throwable => None
+    val outputModuleDef = new ModuleDef:
+      include(bootstrapModuleDef)
+      include(configModuleDef)
+      for (databaseBackendPlugin <- databaseBackendPluginOption)
+        make[DatabaseBackendPlugin].from(databaseBackendPlugin)
+        include(databaseBackendPlugin.getDatabaseBackendModule)
+      include(inputModuleDef)
+    (injector, outputModuleDef)
 
-  def produceRun[F[_], A](configOption: Option[Config] = None, inputModule: ModuleDef = emptyModule): Functoid[Identity[A]] => Identity[A] =
-    val (injector, module) = getInjectorAndModule(configOption, inputModule)
+
+  def produceRun[F[_], A](inputModuleDef: ModuleDef = emptyModule, withConfig: Boolean = true): Functoid[Identity[A]] => Identity[A] =
+    val (injector, module) = getInjectorAndModule(inputModuleDef, withConfig)
     injector.produceRun(module)
