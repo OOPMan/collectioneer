ac82f0fc805a8707c73f062ec4e1f0c6cef86470
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2025-05-25 21:37:14 -0400

    #34: SubConfig classes to implement getModuleDefForSubConfig method

--- modules/plugins/grand-archive-tcg-gui/src/main/scala/com/oopman/collectioneer/plugins/gatcg/gui/GATCGSubConfig.scala
+++ modules/plugins/grand-archive-tcg-gui/src/main/scala/com/oopman/collectioneer/plugins/gatcg/gui/GATCGSubConfig.scala
@@ -1,8 +1,17 @@
 package com.oopman.collectioneer.plugins.gatcg.gui
 
+import com.oopman.collectioneer.SubConfig
 import com.oopman.collectioneer.gui.GUISubConfig
+import distage.ModuleDef
 
 case class GATCGSubConfig
 (
   imagePath: String = (os.pwd / "gatcg"/ "images").toString
-) extends GUISubConfig
+) extends GUISubConfig:
+  def getModuleDefForSubConfig: ModuleDef =
+    val subConfig = this
+    new ModuleDef:
+      many[SubConfig].add(subConfig)
+      many[GUISubConfig].add(subConfig)
+      make[GATCGSubConfig].from(subConfig)
+  
--- modules/plugins/grand-archive-tcg/src/main/scala/com/oopman/collectioneer/plugins/gatcg/GATCGPluginConfig.scala
+++ modules/plugins/grand-archive-tcg/src/main/scala/com/oopman/collectioneer/plugins/gatcg/GATCGPluginConfig.scala
@@ -1,6 +1,8 @@
 package com.oopman.collectioneer.plugins.gatcg
 
+import com.oopman.collectioneer.SubConfig
 import com.oopman.collectioneer.cli.CLISubConfig
+import distage.ModuleDef
 
 import java.io.File
 
@@ -8,4 +10,12 @@ case class GATCGPluginConfig
 (
   grandArchiveTCGJSON: Option[File] = None,
   grandArchiveTCGImages: Option[File] = None
-) extends CLISubConfig
+) extends CLISubConfig:
+  def getModuleDefForSubConfig: ModuleDef = 
+    val subConfig = this
+    new ModuleDef:
+      many[SubConfig].add(subConfig)
+      many[CLISubConfig].add(subConfig)
+      make[GATCGPluginConfig].from(subConfig)
+  
+
