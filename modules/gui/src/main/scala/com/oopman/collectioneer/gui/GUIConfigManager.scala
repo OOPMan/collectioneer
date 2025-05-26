98ca0ea7c7abb22f053985db9a8f9c017e960db1
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2025-05-25 21:35:28 -0400

    #34: Added WIP GUIConfigManager

--- modules/gui/src/main/scala/com/oopman/collectioneer/gui/GUIConfigManager.scala
+++ modules/gui/src/main/scala/com/oopman/collectioneer/gui/GUIConfigManager.scala
@@ -1,14 +1,20 @@
 package com.oopman.collectioneer.gui
 
+import com.oopman.collectioneer.{Config, ConfigManager}
 import com.oopman.collectioneer.plugins.GUISubConfigCodecPlugin
+import distage.ModuleDef
 import io.circe.Decoder
 import io.circe.generic.semiauto.deriveDecoder
 
-object Configuration:
+// TODO: Make a Configuration trait that defines the configModuleProvider and maybe some other stuff. Implement versions for GUI and CLI
+// TODO: This probably needs to be moved to GUI Core?
+// TODO: Thread safety
+// TODO: Provide a Distage Module exposing the GUIConfig along with all sub-configs
+object GUIConfigManager extends ConfigManager:
   private val defaultRootPath = os.pwd
   private val defaultConfigFilePath = defaultRootPath / "collectioneer.yaml"
   private val defaultGUIConfig = GUIConfig(subConfigs = GUISubConfigCodecPlugin.getDefaultGUISubConfigs)
-  private var config: Option[GUIConfig] = None
+  private var configOption: Option[GUIConfig] = None
 
   private def loadAndParseConfiguration(path: os.Path = defaultConfigFilePath) =
     if !os.exists(path)
@@ -25,25 +31,30 @@ object Configuration:
       val config = json.flatMap(_.as[GUIConfig])
       config.getOrElse(defaultGUIConfig)
 
-  def getConfiguration: GUIConfig =
-    config match
-      case Some(config) => config
+  def getConfig: GUIConfig =
+    configOption match
+      case Some(guiConfig) => guiConfig
       case None =>
         val guiConfig = loadAndParseConfiguration()
-        config = Some(guiConfig)
+        configOption = Some(guiConfig)
         guiConfig
 
-  def updateConfiguration(newConfig: GUIConfig): GUIConfig =
-    config = Some(newConfig)
+  def getConfig_=(newConfig: GUIConfig): GUIConfig =
+    configOption = Some(newConfig)
     newConfig
 
-  def loadConfiguration(path: os.Path): GUIConfig =
+  def loadConfig(path: os.Path = defaultConfigFilePath): GUIConfig =
     val guiConfig = loadAndParseConfiguration(path)
-    config = Some(guiConfig)
+    configOption = Some(guiConfig)
     guiConfig
 
-  def saveConfiguration(path: os.Path): Boolean =
+  def saveConfig(path: os.Path): Boolean =
     // TODO: Implement
     ???
 
-
+  def getModuleDefForConfig: ModuleDef = 
+    val config = this.getConfig
+    new ModuleDef:
+      make[Config].from(config)
+      make[GUIConfig].from(config)
+      for (subConfig <- config.subConfigs.values) do include(subConfig.getModuleDefForSubConfig)
