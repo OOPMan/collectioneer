7d1cc7d7271e2f8d6f1b6cf59e8d7b66d2380673
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2025-05-31 17:06:51 -0400

    #34: Updated CollectioneerGUI configure base ModuleDef for Injection with a GUIConfigManager instance

--- modules/gui/src/main/scala/com/oopman/collectioneer/gui/CollectioneerGUI.scala
+++ modules/gui/src/main/scala/com/oopman/collectioneer/gui/CollectioneerGUI.scala
@@ -1,8 +1,6 @@
 package com.oopman.collectioneer.gui
 
-import com.oopman.collectioneer.Injection
-import com.oopman.collectioneer.db.DatabaseBackendPlugin
-import com.oopman.collectioneer.plugins.DatabaseBackendGUIPlugin
+import com.oopman.collectioneer.{ConfigManager, Injection}
 import scalafx.application.JFXApp3
 import scalafx.geometry.Insets
 import scalafx.scene.Scene
@@ -22,7 +20,7 @@ import java.util.concurrent.ExecutorService
 import scala.language.implicitConversions
 import scala.util.Try
 
-object CollectioneerGUI extends JFXApp3 :
+object CollectioneerGUI extends JFXApp3:
   // TODO: Create lazy vals for menu items
   // TODO: Implement handlers for menu items
   // TODO: Tie menu-bar width to stage width somehow
@@ -50,11 +48,14 @@ object CollectioneerGUI extends JFXApp3 :
     scene = new Scene(1024, 768):
       root = borderPane
 
-  lazy val collectioneerGUIModule = new ModuleDef:
-    make[Stage].named("com.oopman.collectioneer.plugins.GUIPlugin.stage").from(primaryStage)
-
   override def start(): Unit =
     stage = primaryStage
+    // TODO: Use parameters to determine if we need to load configuration from a separate file
+    val guiConfigManager = new GUIConfigManager
+    Injection.baseModuleDef = new ModuleDef:
+      make[ConfigManager].from(guiConfigManager)
+      make[GUIConfigManager].from(guiConfigManager)
+      make[Stage].named("com.oopman.collectioneer.plugins.GUIPlugin.stage").from(primaryStage)
     showDatabaseBackendPicker(false)
 
   def showDatabaseBackendPicker(backButtonVisible: Boolean): Unit =
