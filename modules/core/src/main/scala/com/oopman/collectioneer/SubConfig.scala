63a7d90da3d3937f324f466c76436fea894601b7
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2025-05-31 21:21:16 -0400

    #34: Final value for SubConfig.key should be lowercase

--- modules/core/src/main/scala/com/oopman/collectioneer/SubConfig.scala
+++ modules/core/src/main/scala/com/oopman/collectioneer/SubConfig.scala
@@ -5,7 +5,7 @@ import izumi.reflect.Tag
 import izumi.reflect.macrortti.LightTypeTag
 
 trait SubConfig:
-  private val key = getClass.getSimpleName.stripSuffix("SubConfig")
+  private val key = getClass.getSimpleName.stripSuffix("SubConfig").toLowerCase
   final def getKeyForSubConfig: String = key
   
   def getTag: LightTypeTag
