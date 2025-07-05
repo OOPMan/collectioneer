ba572e7fc58c55b1fe7e403ecc03d3f983ef35ff
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2025-07-04 20:42:59 -0400

    #34: Implemented dynamic rendering of card image based on edition selection

--- modules/plugins/grand-archive-tcg-gui/src/main/scala/com/oopman/collectioneer/plugins/gatcg/gui/GATCGCollectionRenderer.scala
+++ modules/plugins/grand-archive-tcg-gui/src/main/scala/com/oopman/collectioneer/plugins/gatcg/gui/GATCGCollectionRenderer.scala
@@ -5,9 +5,11 @@ import com.oopman.collectioneer.db.traits.dao.projected.CollectionDAO
 import com.oopman.collectioneer.db.traits.dao.raw.RelationshipDAO
 import com.oopman.collectioneer.plugins.gatcg.properties.{CommonProperties, EditionProperties, SetCardProperties}
 import com.oopman.collectioneer.db.traits.entity.raw.{HasTopLevelCollectionPKAndLevel, Relationship, given}
-import scalafx.scene.control.{Label, Tab, TabPane}
+import scalafx.collections.ObservableBuffer
+import scalafx.scene.control.{ChoiceBox, Label, Tab, TabPane}
 import scalafx.scene.image.{Image, ImageView}
 import scalafx.scene.layout.{HBox, VBox}
+import scalafx.util.StringConverter
 
 import java.util.UUID
 
@@ -85,28 +87,37 @@ class GATCGCollectionRenderer(gatcgSubConfig: GATCGSubConfig, collection: Collec
       )
 
   private val cardDataOption = generateCardDataCollection()
-  private val initialImageInputStreamOption =
-    for
-      cardData <- cardDataOption
-      propertyValue <- collection.propertyValues.get(SetCardProperties.primaryEditionUID)
-      primaryEditionUID <- propertyValue.textValues.headOption
-      primaryEdition <- cardData.editions.find(_.editionUID == primaryEditionUID)
-    yield
-      val imageUUID = UUID.nameUUIDFromBytes(primaryEdition.image.getBytes)
-      val imagePath = s"${gatcgSubConfig.imagePath}/$imageUUID.jpg"
-      os.Path(imagePath).getInputStream
 
   def render(collection: Collection): Option[Tab] =
-    for cardData <- cardDataOption yield new Tab:
+    for
+      cardData <- cardDataOption
+      primaryEditionPropertyValue <- collection.propertyValues.get(SetCardProperties.primaryEditionUID)
+      primaryEditionUID <- primaryEditionPropertyValue.textValues.headOption
+      primaryEdition <- cardData.editions.find(_.editionUID == primaryEditionUID)
+    yield new Tab:
+      private val imageView = new ImageView
+
+      private val editionChoiceBox = new ChoiceBox[Edition]:
+        items = ObservableBuffer.from(cardData.editions)
+        converter = StringConverter(
+          fromStringFunction = label => cardData.editions.find(edition => label.endsWith(s"(${edition.set.prefix})")).head,
+          toStringFunction = edition => s"${edition.set.name} (${edition.set.prefix})"
+        )
+        onAction = event =>
+          val edition = selectionModel().getSelectedItem
+          val imageUUID = UUID.nameUUIDFromBytes(edition.image.getBytes)
+          val imagePath = s"${gatcgSubConfig.imagePath}/$imageUUID.jpg"
+          imageView.image = new Image(os.Path(imagePath).getInputStream)
+        selectionModel().select(primaryEdition)
+
+
       text = "GATCG Card"
       content = new VBox:
         children = Seq(
+          editionChoiceBox,
           new HBox:
             children = Seq(
-              // Image
-              new ImageView:
-                initialImageInputStreamOption.foreach(inputStream => image = new Image(inputStream))
-              ,
+              imageView,
               // Main Card Content Area
               new TabPane:
                 tabs = Seq(
