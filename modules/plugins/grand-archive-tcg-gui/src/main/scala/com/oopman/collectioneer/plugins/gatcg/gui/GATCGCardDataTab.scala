256c2896784ba494510fc9f321d1fb9f13852341
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2025-07-05 18:42:57 -0400

    #34: Improved rendering of edition data to choicebox

--- modules/plugins/grand-archive-tcg-gui/src/main/scala/com/oopman/collectioneer/plugins/gatcg/gui/GATCGCardDataTab.scala
+++ modules/plugins/grand-archive-tcg-gui/src/main/scala/com/oopman/collectioneer/plugins/gatcg/gui/GATCGCardDataTab.scala
@@ -12,17 +12,26 @@ class GATCGCardDataTab(gatcgSubConfig: GATCGSubConfig, cardData: CardData, prima
   val imageView = new ImageView
 
   val editionChoiceBox = new ChoiceBox[Edition]:
+    def generateEditionLabel(edition: Edition): String =
+      val rarity = GATCGRarities.fromRarity(edition.rarity)
+      s"${edition.set.prefix} - ${edition.set.language} - ${edition.collectorNumber} - ${rarity.shortLabel}"
+
     items = ObservableBuffer.from(cardData.editions)
+    // TODO: Multiple editions with different rarities are possible, support this more cleanly...
     converter = StringConverter(
-      fromStringFunction = label => cardData.editions.find(edition => label.endsWith(s"(${edition.set.prefix})")).head,
-      toStringFunction = edition => s"${edition.set.name} (${edition.set.prefix})"
+      fromStringFunction = label => cardData.editions.find(edition => label == generateEditionLabel(edition)).head,
+      toStringFunction = edition => generateEditionLabel(edition)
     )
-    onAction = event =>
-      val edition = selectionModel().getSelectedItem
-      val imageUUID = UUID.nameUUIDFromBytes(edition.image.getBytes)
-      val imagePath = s"${gatcgSubConfig.imagePath}/$imageUUID.jpg"
-      imageView.image = new Image(os.Path(imagePath).getInputStream)
+    onAction = event => updateTab(selectionModel().getSelectedItem)
     selectionModel().select(primaryEdition)
+
+  def updateTab(edition: Edition): Unit =
+    // TODO: Update other data
+    // Update displayed image
+    // TODO: Handle cards with multiple images
+    val imageUUID = UUID.nameUUIDFromBytes(edition.image.getBytes)
+    val imagePath = s"${gatcgSubConfig.imagePath}/$imageUUID.jpg"
+    imageView.image = new Image(os.Path(imagePath).getInputStream)
 
   text = "GATCG Card"
   content = new VBox:
