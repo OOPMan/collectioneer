52204afa175fd2956ec31e2c113bfdc6640567b6
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2025-07-27 21:19:46 -0400

    #34: Completed implementation of CardDataVBox and added stylesheet for this element

--- modules/plugins/grand-archive-tcg-gui/src/main/scala/com/oopman/collectioneer/plugins/gatcg/gui/controls/CardDataVBox.scala
+++ modules/plugins/grand-archive-tcg-gui/src/main/scala/com/oopman/collectioneer/plugins/gatcg/gui/controls/CardDataVBox.scala
@@ -1,27 +1,56 @@
 package com.oopman.collectioneer.plugins.gatcg.gui.controls
 
-import com.oopman.collectioneer.plugins.gatcg.gui.controls.text.{Underline, Font}
+import com.oopman.collectioneer.gui.StyleClasses
 import com.oopman.collectioneer.plugins.gatcg.gui.{CardCommon, CardData, Edition, EditionCommon}
+import scalafx.geometry.Orientation
 import scalafx.scene.Node
-import scalafx.scene.control.TitledPane
-import scalafx.scene.layout.{HBox, VBox}
-import scalafx.scene.text.{FontWeight, Text, TextAlignment}
+import scalafx.scene.control.{Separator, TitledPane}
+import scalafx.scene.layout.VBox
+import scalafx.scene.text.{Text, TextFlow}
+
 
 class CardDataVBox(val cardData: CardData, val edition: Edition) extends VBox:
+  stylesheets.add("css/carddatavbox.css")
+  
+  val cardDataLabelStyleClass = "card-data-label"
+
+  def generateTextFlow(label: String, text: String): TextFlow =
+    new TextFlow(
+      new Text(label + ":") with StyleClasses(cardDataLabelStyleClass),
+      new Text(" " + text)
+    )
 
   def generateNode(cardData: CardCommon, edition: EditionCommon): Node =
-    val cardNameText = new Text(cardData.name) with Font(14, FontWeight.Bold) with Underline
-    val elementText = new Text(cardData.element) with Font(14, FontWeight.Bold)
-    val types = new Text(cardData.types.mkString(" ")) with Font(weight = FontWeight.Bold)
-    val subTypes = new Text(cardData.subTypes.mkString(" ")) with Font(weight = FontWeight.Bold)
+    val effects = edition.effect ++ cardData.effect
+    val effect = effects.headOption.map(effect => Separator(Orientation.Horizontal) :: new TextFlow(new Text(effect)) :: Nil).getOrElse(Nil)
+    val nodeOptions: List[Option[Node]] =
+      Some(generateTextFlow("Name", cardData.name)) ::
+      Some(generateTextFlow("Element", cardData.element)) ::
+      cardData.level.map(level => generateTextFlow("Level", level.toString)) ::
+      Some(generateTextFlow("Classes", cardData.classes.mkString(" "))) ::
+      Some(generateTextFlow("Types", cardData.types.mkString(" "))) ::
+      Some(generateTextFlow("Sub-types", cardData.subTypes.mkString(" "))) ::
+      cardData.reserveCost.map(cost => generateTextFlow("Reserve Cost", cost.toString)) ::
+      cardData.memoryCost.map(cost => generateTextFlow("Memory Cost", cost.toString)) ::
+      cardData.power.map(power => generateTextFlow("Power", power.toString)) ::
+      cardData.life.map(life => generateTextFlow("Life", life.toString)) ::
+      cardData.durability.map(durability => generateTextFlow("Durability", durability.toString)) ::
+      cardData.speed.map(speed => generateTextFlow("Speed", speed)) ::
+      Nil
+    val nodes = nodeOptions.flatten ++ effect
 
+    val cardNameLabelText = new Text("Name:") with StyleClasses(cardDataLabelStyleClass)
+    val cardNameText = new Text(" " + cardData.name)
+    val elementLabelText = new Text("Element:") with StyleClasses(cardDataLabelStyleClass)
+    val elementText = new Text(" " + cardData.element)
+    val typesLabelText = new Text("Types:") with StyleClasses(cardDataLabelStyleClass)
+    val typesText = new Text(" " + cardData.types.mkString(" "))
+    val subtypesLabelText = new Text("Subtypes:") with StyleClasses(cardDataLabelStyleClass)
+    val subTypesText = new Text(" " + cardData.subTypes.mkString(" "))
     new TitledPane:
-      text = edition.orientation.getOrElse("Front")
+      text = edition.orientation.getOrElse("front").capitalize
       expanded = true
-      content = new VBox(
-        new HBox(cardNameText, elementText),
-        new HBox(types, new Text(" - "), subTypes)
-      )
+      content = new VBox(nodes*)
 
   children = generateNode(cardData, edition) +: edition.innerCards.map(innerCard => generateNode(innerCard, innerCard.innerEdition))
 
