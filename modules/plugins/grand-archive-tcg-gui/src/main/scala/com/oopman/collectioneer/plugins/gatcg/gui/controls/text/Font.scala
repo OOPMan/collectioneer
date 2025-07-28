a409b24f75df528e870a7fd5bc1e57f4934e1d3c
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2025-07-27 21:17:23 -0400

    #34: Removed pointless Text node mixins

--- modules/plugins/grand-archive-tcg-gui/src/main/scala/com/oopman/collectioneer/plugins/gatcg/gui/controls/text/Font.scala
+++
@@ -1,9 +0,0 @@
-package com.oopman.collectioneer.plugins.gatcg.gui.controls.text
-
-import scalafx.scene.text.{Text, Font => SFXFont, FontWeight, FontPosture}
-
-trait Font(size: Double = 12,
-           weight: FontWeight=FontWeight.Normal,
-           posture: FontPosture = FontPosture.Regular,
-           family: String = "System") extends Text:
-  font = SFXFont(family, weight, posture, size)
--- modules/plugins/grand-archive-tcg-gui/src/main/scala/com/oopman/collectioneer/plugins/gatcg/gui/controls/text/Strikethrough.scala
+++
@@ -1,6 +0,0 @@
-package com.oopman.collectioneer.plugins.gatcg.gui.controls.text
-
-import scalafx.scene.text.Text
-
-trait Strikethrough extends Text:
-  strikethrough = true
