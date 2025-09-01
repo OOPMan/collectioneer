5514356be22d2bb5a53fa7dd23555e8a1f86ba34
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2025-09-01 19:31:32 -0400

    #53: Refactored contents of GATCG GUI plugin module

--- modules/plugins/grand-archive-tcg-gui/src/main/scala/com/oopman/collectioneer/plugins/gatcg/gui/GATCGUIPluginDef.scala
+++ modules/plugins/grand-archive-tcg-gui/src/main/scala/com/oopman/collectioneer/plugins/gatcg/gui/GATCGUIPluginDef.scala
@@ -1,7 +1,10 @@
 package com.oopman.collectioneer.plugins.gatcg.gui
 
 import com.oopman.collectioneer.Plugin
-import com.oopman.collectioneer.plugins.{DetailViewGUIPlugin, GUIPlugin, GUISubConfigCodecPlugin, MainViewGUIPlugin, PluginsMenuGUIPlugin}
+import com.oopman.collectioneer.plugins.gatcg.gui.detailview.GATCGDetailViewGUIPlugin
+import com.oopman.collectioneer.plugins.gatcg.gui.mainview.GATCGMainViewGUIPlugin
+import com.oopman.collectioneer.plugins.gatcg.gui.menu.GATCGPluginsMenuGUIPlugin
+import com.oopman.collectioneer.plugins.*
 import izumi.distage.plugins.PluginDef
 
 object GATCGUIPluginDef extends PluginDef:
@@ -9,7 +12,7 @@ object GATCGUIPluginDef extends PluginDef:
     .add[GATCGPluginsMenuGUIPlugin]
   many[GUISubConfigCodecPlugin]
     .add[GATCGGUISubConfigCodecPlugin]
-  many[DetailViewGUIPlugin]
+  many[GATCGDetailViewGUIPlugin]
     .add[GATCGDetailViewGUIPlugin]
   many[MainViewGUIPlugin]
     .add[GATCGMainViewGUIPlugin]
---
+++ modules/plugins/grand-archive-tcg-gui/src/main/scala/com/oopman/collectioneer/plugins/gatcg/gui/menu/GATCGPluginsMenuGUIPlugin.scala
@@ -0,0 +1,50 @@
+package com.oopman.collectioneer.plugins.gatcg.gui.menu
+
+import com.oopman.collectioneer.plugins.{GUIPlugin, PluginsMenuGUIPlugin}
+import distage.Id
+import scalafx.scene.control.{Menu, MenuItem}
+import scalafx.stage.Stage
+
+class GATCGPluginsMenuGUIPlugin(stage: Stage @Id("com.oopman.collectioneer.plugins.GUIPlugin.stage"))
+extends GUIPlugin(stage), PluginsMenuGUIPlugin:
+  private lazy val settingsMenuItem = new MenuItem("Setttings"):
+    onAction = event => {
+      // TODO: Handle click
+    }
+
+  private lazy val downloadDatasetMenuItem = new MenuItem("Download Dataset"):
+    onAction = event => {
+      // TODO: Handle click
+      /**
+       * Step 1: Show file chooser dialog (on cancel, do nothing further)
+       * Step 2: Show new window that downloads the dataset to the chosen file, displaying progress
+       */
+    }
+
+  private lazy val importDatasetMenuItem = new MenuItem("Import Dataset"):
+    onAction = event => {
+      // TODO: Handle click
+      /**
+       * Step 1: Show file chooser dialog (on cancel, do nothing further)
+       * Step 2: Show new window that imports the dataset from the chosen file, displaying progress
+       */
+    }
+
+  private lazy val downloadImagesMenuItem = new MenuItem("Download Images"):
+    onAction = event => {
+      // TODO: Handle click
+      /**
+       * Step 1: Show directory chooser dialog (on cancel, do nothing further)
+       * Step 2: Show new window that downloads images, displaying progress
+       */
+    }
+
+  def getMenu: Menu = new Menu("Grand Archive TCG"):
+    items = settingsMenuItem :: downloadDatasetMenuItem :: importDatasetMenuItem :: downloadImagesMenuItem :: Nil
+
+  override def getName: String = "GATCG Plugins Menu GUI Plugin"
+
+  override def getShortName: String = "GATCGPluginsMenuGUI"
+
+  override def getVersion: String = "master"
+
