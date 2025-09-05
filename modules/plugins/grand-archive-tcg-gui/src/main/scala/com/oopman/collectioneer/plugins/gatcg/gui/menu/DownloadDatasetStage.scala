283fe70f16dc671c57c688d37dc4869c66e4efa1
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2025-09-05 14:03:48 -0400

    #53: Implemented DownloadDatasetStage, allowing for the DownloadDataset action to be performed within a GUI window in a separate thread with progress and logger process output capture and displayed

--- modules/plugins/grand-archive-tcg-gui/src/main/scala/com/oopman/collectioneer/plugins/gatcg/gui/menu/DownloadDatasetStage.scala
+++ modules/plugins/grand-archive-tcg-gui/src/main/scala/com/oopman/collectioneer/plugins/gatcg/gui/menu/DownloadDatasetStage.scala
@@ -1,31 +1,69 @@
 package com.oopman.collectioneer.plugins.gatcg.gui.menu
 
+import com.oopman.collectioneer.gui.logback.TextAreaAppender
+import com.oopman.collectioneer.plugins.gatcg.actions.DownloadDataset
+import com.typesafe.scalalogging.LazyLogging
+import io.circe.Json
+import javafx.concurrent.Task
 import scalafx.scene.Scene
 import scalafx.scene.control.{Button, ProgressBar, ScrollPane, TextArea}
 import scalafx.scene.layout.VBox
 import scalafx.stage.{Modality, Stage}
 
 import java.io.File
+import scala.util.{Failure, Try}
 
-class DownloadDatasetStage(path: File, parent: Stage) extends Stage:
-  lazy val progressBar = new ProgressBar
-  lazy val logTextArea = new TextArea("Logging goes here...\nMore here\nWe might need a scrollbar\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nMore text"):
+class DownloadDatasetStage(path: File, parent: Stage) extends Stage with LazyLogging:
+  private val task = new Task[Try[Vector[Json]]]() with DownloadDataset(os.Path(path)) with LazyLogging:
+    override def succeeded(): Unit =
+      closeCancelButton.disable = false
+      closeCancelButton.text = "Close"
+      closeCancelButton.onAction = { event => close() }
+
+    override def cancelled(): Unit = close()
+
+    override def call(): Try[Vector[Json]] =
+      logger.underlying match
+        case logger: ch.qos.logback.classic.Logger =>
+          val appender = new TextAreaAppender(logTextArea)
+          appender.setContext(logger.getLoggerContext)
+          appender.start()
+          logger.addAppender(appender)
+        case _ => // Do Nothing
+      apply()
+
+    override def getData(page: Int): Try[Vector[Json]] =
+      for totalPages <- totalPages do updateProgress(page, totalPages)
+      if isCancelled then Failure(RuntimeException("Cancelled"))
+      else super.getData(page)
+
+  private val thread = new Thread(task)
+  thread.setDaemon(true)
+
+  private val logTextArea = new TextArea:
     editable = false
-  lazy val closeCancelButton = new Button("Cancel")
-  // TODO: Need on-click handler
+
+  private val progressBar = new ProgressBar:
+    maxWidth <== logTextArea.width
+    progress <== task.progressProperty()
+
+  private val closeCancelButton: Button = new Button("Cancel"):
+    onAction = { event =>
+      disable = true
+      task.cancel()
+    }
 
   title = "Download Dataset"
   initOwner(parent)
   initModality(Modality.None)
-  onCloseRequest = { event =>
-    // TODO: Stop download if in progress
-  }
+  onCloseRequest = { event => task.cancel() }
   scene = new Scene(640, 480):
     root = new ScrollPane:
       fitToWidth = true
       fitToHeight = true
       content = new VBox(progressBar, logTextArea, closeCancelButton)
+
   show()
-  // TODO: After showing the window we need to download the files. Needs to run in separate thread via Task system
+  thread.start()
 
 
