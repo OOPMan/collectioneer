4206423037609c6c53c86c23069dddd323ef6f6b
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2025-10-11 17:01:59 -0400

    #56: Cleaned up some names in CoreRelationships

--- modules/core/src/main/scala/com/oopman/collectioneer/CoreRelationships.scala
+++ modules/core/src/main/scala/com/oopman/collectioneer/CoreRelationships.scala
@@ -6,19 +6,19 @@ import com.oopman.collectioneer.db.traits.entity.raw.RelationshipType.ChildOf
 import com.oopman.collectioneer.given
 
 private object CoreRelationshipUUIDs:
-  val commonPropertiesChildOfRoot = "f3ff67b6-f1b9-43a3-99d9-961dc40b921b"
-  val commonPropertiesOfPropertiesChildOfRoot = "ec863b4c-3873-47d4-8af9-300b6fcf7110"
+  val commonPropertiesChildOfProperties = "f3ff67b6-f1b9-43a3-99d9-961dc40b921b"
+  val commonPropertiesOfPropertiesChildOfProperties = "ec863b4c-3873-47d4-8af9-300b6fcf7110"
 
 enum CoreRelationships(val relationship: Relationship):
-  case commonPropertiesChildOfRoot extends CoreRelationships(raw.Relationship(
-    pk = CoreRelationshipUUIDs.commonPropertiesChildOfRoot,
+  case commonPropertiesChildOfProperties extends CoreRelationships(raw.Relationship(
+    pk = CoreRelationshipUUIDs.commonPropertiesChildOfProperties,
     relatedCollectionPK = CoreCollections.commonProperties,
     relationshipType = ChildOf,
     collectionPK = CoreCollections.properties,
   ))
 
-  case commonPropertiesOfPropertiesChildOfRoot extends CoreRelationships(raw.Relationship(
-    pk = CoreRelationshipUUIDs.commonPropertiesOfPropertiesChildOfRoot,
+  case commonPropertiesOfPropertiesChildOfProperties extends CoreRelationships(raw.Relationship(
+    pk = CoreRelationshipUUIDs.commonPropertiesOfPropertiesChildOfProperties,
     relatedCollectionPK = CoreCollections.commonPropertiesOfProperties,
     relationshipType = ChildOf,
     collectionPK = CoreCollections.properties,
