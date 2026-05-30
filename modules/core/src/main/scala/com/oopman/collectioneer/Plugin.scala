bd8f59d435b466eb21f0001746d59dd333780dec
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2026-05-29 20:51:46 -0400

    #56: Added default implementation for Plugin.getShortName method and added getRank method

--- modules/core/src/main/scala/com/oopman/collectioneer/Plugin.scala
+++ modules/core/src/main/scala/com/oopman/collectioneer/Plugin.scala
@@ -2,5 +2,6 @@ package com.oopman.collectioneer
 
 trait Plugin:
   def getName: String
-  def getShortName: String
+  def getShortName: String = getClass.getName
   def getVersion: String
+  def getRank: Int = 0
