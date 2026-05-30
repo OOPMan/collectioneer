cff95ace3e5a65bf4f628644db82dfc4bb4235f3
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2026-05-29 20:47:44 -0400

    #56: Implemented TextPropertyValueEditorGUIPlugin

---
+++ modules/gui/src/main/scala/com/oopman/collectioneer/plugins/gui/controls/TextPropertyValueEditorGUIPlugin.scala
@@ -0,0 +1,62 @@
+package com.oopman.collectioneer.plugins.gui.controls
+
+import com.oopman.collectioneer.db.entity
+import com.oopman.collectioneer.db.traits.entity.projected.{Property, PropertyValue}
+import com.oopman.collectioneer.db.traits.entity.raw.PropertyType
+import com.oopman.collectioneer.plugins.{GUIPlugin, PropertyValueEditorGUIPlugin}
+import distage.Id
+import scalafx.scene.Node
+import scalafx.scene.control.{Button, TextArea, TextField}
+import scalafx.scene.layout.HBox
+import scalafx.stage.Stage
+
+class TextPropertyValueEditorGUIPlugin(stage: Stage @Id("com.oopman.collectioneer.plugins.GUIPlugin.stage"))
+extends GUIPlugin(stage), PropertyValueEditorGUIPlugin:
+  def canEditPropertyValuesForProperty(property: Property): Boolean =
+    property.propertyTypes.contains(PropertyType.text)
+
+  def generatePropertyValueEditor(property: Property, propertyValue: Option[PropertyValue]): PropertyValueEditorGUIPlugin.PropertyValueEditor =
+    new TextPropertyValueEditorGUIPlugin.TextPropertyValueEditor(property, propertyValue)
+
+  def getName: String = "Text PropertyValue Editor GUI Plugin"
+
+  override def getShortName: String = ???
+
+  def getVersion: String = "0.0.1"
+
+  override def getRank: Int = -1
+
+private object TextPropertyValueEditorGUIPlugin:
+  private class TextEditorHBox extends HBox:
+    private val textArea = new TextArea
+    private val textField = new TextField
+    private var activeTextEditor: Node = textField
+    private val switcher: Button = new Button("Switch"):
+      onAction = event =>
+        activeTextEditor = activeTextEditor match
+          case _: TextField => textArea
+          case _: TextArea => textField
+        children = activeTextEditor :: switcher :: Nil
+      
+
+    textArea.text <==> textField.text
+
+    def getText: String = textArea.text.value
+
+    children = activeTextEditor :: switcher :: Nil
+
+  private class TextPropertyValueEditor(property: Property, propertyValue: Option[PropertyValue])
+  extends PropertyValueEditorGUIPlugin.PropertyValueEditor(property, propertyValue), PropertyValueEditorGUIPlugin.MinValuesMaxValuesAwarePropertyValueEditor[TextEditorHBox]:
+
+    def getPropertyValue: Option[PropertyValue] =
+      val textValues =
+        for inputNode <- inputNodes.toSeq
+        text = inputNode.getText
+        if !text.isBlank
+        yield text
+      Some(entity.projected.PropertyValue(
+        textValues = textValues
+      ))
+
+    def generateInputNode: TextEditorHBox =
+      new TextEditorHBox
