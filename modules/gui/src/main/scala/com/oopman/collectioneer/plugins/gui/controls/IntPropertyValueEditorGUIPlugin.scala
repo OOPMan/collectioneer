67c6ecbb5cd44335ed455bbd533fab86e14dd3b9
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2026-07-18 21:09:14 -0400

    #56: Updated IntPropertyValueEditorGUIPlugin

--- modules/gui/src/main/scala/com/oopman/collectioneer/plugins/gui/controls/IntPropertyValueEditorGUIPlugin.scala
+++ modules/gui/src/main/scala/com/oopman/collectioneer/plugins/gui/controls/IntPropertyValueEditorGUIPlugin.scala
@@ -5,16 +5,13 @@ import com.oopman.collectioneer.db.traits.entity.projected.{Property, PropertyVa
 import com.oopman.collectioneer.db.traits.entity.raw.PropertyType
 import com.oopman.collectioneer.plugins.{GUIPlugin, PropertyValueEditorGUIPlugin}
 import distage.Id
-import net.synedra.validatorfx.Validator
 import scalafx.scene.control.TextField
 import scalafx.stage.Stage
-
-import scala.util.Try
 
 class IntPropertyValueEditorGUIPlugin(stage: Stage @Id("com.oopman.collectioneer.plugins.GUIPlugin.stage"))
 extends GUIPlugin(stage), PropertyValueEditorGUIPlugin:
   def canEditPropertyValuesForProperty(property: Property): Boolean =
-    property.propertyTypes.contains(PropertyType.int)
+    property.propertyTypes.contains(PropertyType.Int)
 
   def generatePropertyValueEditor(property: Property, propertyValue: Option[PropertyValue]): PropertyValueEditorGUIPlugin.PropertyValueEditor =
     new IntPropertyValueEditorGUIPlugin.IntPropertyValueEditor(property, propertyValue)
@@ -30,9 +27,14 @@ private object IntPropertyValueEditorGUIPlugin:
   extends PropertyValueEditorGUIPlugin.PropertyValueEditor(property, propertyValue), PropertyValueEditorGUIPlugin.MinValuesMaxValuesAwarePropertyValueEditor[TextField]:
 
     def getPropertyValue: Option[PropertyValue] =
-      val values = inputNodes.map(_.text.value).map(java.lang.Integer.parseInt).toSeq
+      val values =
+        for
+          inputNode <- inputNodes
+          value = inputNode.text.value
+          intValue <- value.toIntOption
+        yield intValue
       Some(entity.projected.PropertyValue(
-        intValues = values
+        intValues = values.toSeq
       ))
 
     override def generateInputNode: TextField =
@@ -42,8 +44,8 @@ private object IntPropertyValueEditorGUIPlugin:
         .dependsOn(key, textField.text)
         .withMethod { context =>
           val value = context.get[String](key)
-          try Integer.parseInt(value)
-          catch case _: NumberFormatException => context.error("Invalid integer")
+          try value.toInt
+          catch case _: NumberFormatException => context.error("Invalid 32-bit integer value")
         }
         .decorates(textField)
         .immediate()
